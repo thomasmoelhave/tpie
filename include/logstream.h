05a47fa964a93c9eb2d45a138826e5834e8e8814
Darren Vengroff <dev>
1994-05-12 14:46:00 +0000

    This file is going to become logstream.h, a gnereal package for C++ log files.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@4 669acd26-ddd2-4139-9e68-486997aa639d

--- include/logstream.h
+++ include/logstream.h
@@ -1,26 +1,46 @@
 // Copyright (c) 1994 Darren Erik Vengroff
 //
-// File: tp_log.h
+// File: logstream.h
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/12/94
 //
-// $Id: logstream.h,v 1.1 1994-05-12 14:05:23 dev Exp $
+// $Id: logstream.h,v 1.2 1994-05-12 14:46:00 dev Exp $
 //
-#ifndef _TP_LOG_H
-#define _TP_LOG_H
+#ifndef _LOGSTREAM_H
+#define _LOGSTREAM_H
 
-#include <iostream.h>
+#include <fstream.h>
+#include <iomanip.h>
 
 // A log is like a regular output stream, but it also supports messages
 // at different priorities.  If a message's priority is at least as high
 // as the current priority threshold, then it appears in the log.  
-// Otherwise, it does not.
+// Otherwise, it does not.  Lower numbers have higher priority; 0 is
+// the highest.  1 is the default if not 
 
-class tp_log : ostream {
-private:
-  
+class logstream : ofstream {
+  private:
+    unsigned int priority;
+    unsigned int thresh_p;
 
+  public:
+    logstream(const char *fname, unsigned int p = 0, unsigned int tp = 0);
+
+    void set_priority(unsigned int dp);
+    unsigned int get_default_priority(void);
+    void threshold_priority(unsigned int tp);
+    unsigned int get_threshold_priority(void);    
+}
+
+// Setting priority on the fly with manipulators.
+
+logstream& manip_priority(logstream& tpl, unsigned int p)
+{
+    tpl.priority = p;
+    return tpl;
 }
 
 
-#endif // _TP_LOG_H 
+
+
+#endif // _LOGSTREAM_H 
