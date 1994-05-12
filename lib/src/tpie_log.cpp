758977f63557fdbde15f66e4a797290ed9894353
Darren Vengroff <dev>
1994-05-12 21:02:52 +0000

    Initial revision
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@6 669acd26-ddd2-4139-9e68-486997aa639d

---
+++ lib/src/tpie_log.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 1994 Darren Erik Vengroff
+//
+// File: tpie_log.cpp
+// Author: Darren Erik Vengroff <dev@cs.duke.edu>
+// Created: 5/12/94
+//
+
+
+static char tpie_log_id[] = "$Id: tpie_log.cpp,v 1.1 1994-05-12 21:00:02 dev Exp $";
+
+#include <tpie_log.h>
+
+extern "C" char *mktemp(char *);
+
+#define TP_LOG_NAME	"/tmp/TPLOG_XXXXXX"
+
+// The name of the TPIE log
+static char tpl_name[] = TP_LOG_NAME;
+
+// The main TPIE log.  Unless changed, everything is logged.
+logstream *tpl;
+
+// The performance log
+logstream *tp_perfl;
+
+
+void init_tpie_logs(void)
+{
+    tpl = new logstream(mktemp(tpl_name), TP_LOG_INFO, TP_LOG_INFO);
+}
