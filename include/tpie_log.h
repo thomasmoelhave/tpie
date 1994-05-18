9f5187bbf649ed2672a50ff91fbb83759f325e34
Darren Vengroff <dev>
1994-05-18 18:50:16 +0000

    Added macros to do logging.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@8 669acd26-ddd2-4139-9e68-486997aa639d

--- include/tpie_log.h
+++ include/tpie_log.h
@@ -4,10 +4,14 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/12/94
 //
-// $Id: tpie_log.h,v 1.1 1994-05-12 21:00:30 dev Exp $
+// $Id: tpie_log.h,v 1.2 1994-05-18 18:50:16 dev Exp $
 //
 #ifndef _TPIE_LOG_H
 #define _TPIE_LOG_H
+
+#if TPL_LOGGING		
+
+// We are logging.
 
 #include <logstream.h>
 
@@ -31,5 +35,35 @@ extern logstream *tpl;
 extern logstream *tp_perfl;
 
 void init_tpie_logs(void);
+
+// Macros to simplify logging.  The argument to the macro can be any type
+// that log streams have an output operator for.
+
+#define LOG_FATAL(msg) (*tpl << setpriority(TP_LOG_FATAL) << msg)
+#define LOG_ERROR(msg) (*tpl << setpriority(TP_LOG_ERROR) << msg)
+#define LOG_WARNING(msg)  (*tpl << setpriority(TP_LOG_WARNING) << msg)
+#define LOG_ASSERT(msg)  (*tpl << setpriority(TP_LOG_ASSERT)  << msg)
+#define LOG_DEBUG_INFO(msg)  (*tpl << setpriority(TP_LOG_DEBUG)  << msg)
+#define LOG_OS_ERROR(msg)  (*tpl << setpriority(TP_LOG_OS_ERROR) << msg)
+#define LOG_DATA_ERROR(msg)  (*tpl << setpriority(TP_LOG_DATA_ERROR) << msg)
+#define LOG_INFO(msg)  (*tpl << setpriority(TP_LOG_INFO) << msg)
+
+#define LOG_FLUSH_LOG (tpl->ofstream::flush())
+
+#else // !TPL_LOGGING
+
+// We are not compiling logging in(msg)
+// the logging macros.
+
+#define LOG_FATAL(msg) 
+#define LOG_ERROR(msg) 
+#define LOG_WARNING(msg) 
+#define LOG_ASSERT(msg) 
+#define LOG_DEBUG_INFO(msg) 
+#define LOG_OS_ERROR(msg) 
+#define LOG_DATA_ERROR(msg) 
+#define LOG_INFO(msg) 
+
+#endif // TPL_LOGGING
 
 #endif // _TPIE_LOG_H 
