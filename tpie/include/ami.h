1609219a268a324dc57a677ea7e81b6962213f8f
Darren Vengroff <dev>
1994-05-25 20:08:06 +0000

    Initial revision

---
+++ tpie/include/ami.h
@@ -0,0 +1,21 @@
+// Copyright (c) 1994 Darren Erik Vengroff
+//
+// File: ami.h
+// Author: Darren Erik Vengroff <dev@cs.duke.edu>
+// Created: 5/19/94
+//
+// $Id: ami.h,v 1.1 1994-05-25 20:08:06 dev Exp $
+//
+#ifndef _AMI_H
+#define _AMI_H
+
+// Get an implementation definition
+#include <ami_imps.h>
+
+// Get the base class, enums, etc...
+#include <ami_base.h>
+
+// Get templates for ami_scan().
+#include <ami_scan.h>
+
+#endif // _AMI_H 
