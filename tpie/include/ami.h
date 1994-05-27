67afcc767e3237751b8f99776f330daff25a10b7
Darren Vengroff <dev>
1994-05-27 19:43:46 +0000

    General minor bug fixes to get the AMI working with the first test app.

--- tpie/include/ami.h
+++ tpie/include/ami.h
@@ -4,16 +4,16 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/19/94
 //
-// $Id: ami.h,v 1.1 1994-05-25 20:08:06 dev Exp $
+// $Id: ami.h,v 1.2 1994-05-27 19:43:46 dev Exp $
 //
 #ifndef _AMI_H
 #define _AMI_H
 
-// Get an implementation definition
-#include <ami_imps.h>
-
 // Get the base class, enums, etc...
 #include <ami_base.h>
+
+// Get an implementation definition
+#include <ami_imps.h>
 
 // Get templates for ami_scan().
 #include <ami_scan.h>
