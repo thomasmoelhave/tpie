e82633e8638075a74e613f5636f464826250e5a9
Darren Vengroff <dev>
1994-05-27 19:29:45 +0000

    Added the option of having assertion failure reports go to cerr as well as to the log stream.  Set DEBUG_CERR to enable this.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@14 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/tpie_assert.h
+++ tpie/include/tpie_assert.h
@@ -4,7 +4,7 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/12/94
 //
-// $Id: tpie_assert.h,v 1.1 1994-05-18 18:50:44 dev Exp $
+// $Id: tpie_assert.h,v 1.2 1994-05-27 19:29:45 dev Exp $
 //
 // This code is based on 
 // 	myassert.h
@@ -17,6 +17,12 @@
 
 #include <tpie_log.h>
 
+#if DEBUG_CERR
+#include <iostream.h>
+#define CERR_OUT(s) {cerr << s; cerr.flush();}
+#else
+#define CERR_OUT(s)
+#endif
 
 #define QUOTE(x) # x
 
@@ -31,10 +37,11 @@
 #define tp_assert(c, msg)						\
 {									\
     if (!(c)) {								\
-	LOG_ASSERT("Assertion failed (line "				\
-		   QUOTEMACRORESULT(__LINE__)				\
-		   " of " __FILE__ "): " msg "\n");			\
+	LOG_ASSERT(__FILE__ ":" QUOTEMACRORESULT(__LINE__)		\
+                   ": Assertion failed: " msg "\n");			\
 	LOG_FLUSH_LOG;							\
+	CERR_OUT(__FILE__ ":" QUOTEMACRORESULT(__LINE__)		\
+                 ": Assertion failed: " msg "\n");			\
     }									\
 }
 #else
@@ -42,10 +49,11 @@
 #endif
 
 #if DEBUG_STR
-#define TP_DEBUGSTR(s) LOG_DEBUG_INFO(s)
+#define TP_DEBUGSTR(s) LOG_DEBUG_INFO(s); LOG_FLUSH_LOG; CERR_OUT(s)
 #else
 #define TP_DEBUGSTR(s)
 #endif
 	
 
 #endif // _TPIE_ASSERT_H
+
