2b0fd048f8792d1a6a095351465815479ef80ec0
Darren Vengroff <dev>
1994-06-03 13:39:12 +0000

    Implementation of the log_init class to ensure that logs are initialized properly before thier use.  See ../include/tpie_log.h for details.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@28 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/lib/src/tpie_log.cpp
+++ tpie/lib/src/tpie_log.cpp
@@ -6,7 +6,7 @@
 //
 
 
-static char tpie_log_id[] = "$Id: tpie_log.cpp,v 1.2 1994-05-17 18:18:29 dev Exp $";
+static char tpie_log_id[] = "$Id: tpie_log.cpp,v 1.3 1994-06-03 13:39:12 dev Exp $";
 
 // We are logging
 #define TPL_LOGGING	1
@@ -16,18 +16,44 @@ static char tpie_log_id[] = "$Id: tpie_log.cpp,v 1.2 1994-05-17 18:18:29 dev Exp
 extern "C" char *mktemp(char *);
 
 #define TP_LOG_NAME	"/tmp/TPLOG_XXXXXX"
+#define TP_PLOG_NAME	"/tmp/TPPRF_XXXXXX"
 
-// The name of the TPIE log
+// The names of the TPIE logs
 static char tpl_name[] = TP_LOG_NAME;
+static char tp_perfl_name[] = TP_PLOG_NAME;
 
-// The main TPIE log.  Unless changed, everything is logged.
+// The main TPIE log. 
 logstream *tpl;
 
 // The performance log
 logstream *tp_perfl;
 
+// The counter of log_init instances.  It is implicity set to 0.
+unsigned int log_init::count;
 
-void init_tpie_logs(void)
+// The constructor and destructor that ensure that the log files are
+// created exactly once, and destroyed when appropriate.
+log_init::log_init(void)
 {
-    tpl = new logstream(mktemp(tpl_name), TP_LOG_INFO, TP_LOG_INFO);
+    if (count++ == 0) {
+        tpl = new logstream(mktemp(tpl_name), TP_LOG_INFO, TP_LOG_INFO);
+
+        // Why does this cause seg faults?
+        // LOG_INFO("Automatically created tpl.\n");
+        // LOG_FLUSH_LOG;
+
+        tp_perfl = new logstream(mktemp(tp_perfl_name), TP_LOG_INFO,
+                                 TP_LOG_INFO);
+    }
 }
+
+
+log_init::~log_init(void)
+{
+    if (--count == 0) {
+        delete tpl;
+        delete tp_perfl;
+    }
+}
+
+
