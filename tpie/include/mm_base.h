b44f067bf6e362dd3cfdd32401b13d9d616a542d
Darren Vengroff <dev>
1994-08-31 19:28:03 +0000

    Stole locks from dev.  We're on the way to full merging.

--- tpie/include/mm_base.h
+++ tpie/include/mm_base.h
@@ -4,7 +4,7 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/30/94
 //
-// $Id: mm_base.h,v 1.1 1994-06-03 13:27:30 dev Exp $
+// $Id: mm_base.h,v 1.2 1994-08-31 19:28:03 darrenv Exp $
 //
 #ifndef _MM_BASE_H
 #define _MM_BASE_H
@@ -15,6 +15,19 @@ enum MM_err {
     MM_ERROR_INSUFFICIENT_SPACE,
     MM_ERROR_UNDERFLOW
 };
+
+// types of memory usage queries we can make on streams (either BTE or MM)
+enum MM_stream_usage {
+    // Overhead of the object without the buffer
+    MM_STREAM_USAGE_OVERHEAD = 1,
+    // Max amount ever used by a buffer
+    MM_STREAM_USAGE_BUFFER,
+    // Amount currently in use.
+    MM_STREAM_USAGE_CURRENT,
+    // Max amount that will ever be used.
+    MM_STREAM_USAGE_MAXIMUM
+};
+
 
 // The base class for all memory management objects.
 class MM_base_manager
