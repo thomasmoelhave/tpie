ed0a792516eb0891b78338a4c3605e2a85bda4b7
Darren Vengroff <dev>
1994-09-16 13:25:54 +0000

    Checking in for transfer to and work at Duke.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@43 669acd26-ddd2-4139-9e68-486997aa639d

--- include/ami_merge.h
+++ include/ami_merge.h
@@ -8,7 +8,7 @@
 // lower level streams will use appropriate levels of buffering.  This
 // will be more critical for parallel disk implementations.
 //
-// $Id: ami_merge.h,v 1.2 1994-08-31 19:16:05 darrenv Exp $
+// $Id: ami_merge.h,v 1.3 1994-09-16 13:24:48 darrenv Exp $
 //
 #ifndef _AMI_MERGE_H
 #define _AMI_MERGE_H
@@ -210,15 +210,79 @@ template<class T, class M>
 AMI_err AMI_partition_and_merge(AMI_STREAM<T> *instream,
                                 AMI_STREAM<T> *outstream, M *mobj) 
 {
+    AMI_err ae;
+    off_t len;
+    size_t sz_avail, sz_stream;
+    
     // Figure out how much memory we've got to work with.
 
+    if (MM_manager.available(&sz_avail) != MM_ERROR_NO_ERROR) {
+        return AMI_ERROR_MM_ERROR;
+    }
+
+    // If the whole input stream can fit in main memory, then load it
+    // and call the bottoming out function.
+
+    len = instream->stream_len();
+
+    if ((len * sizeof(T)) <= sz_avail) {
+        MM_ptr<T> mm_stream;
+
+        if (!(mm_stream = MM_manager->alloc(len * sizeof(T)))) {
+            return AMI_ERROR_MM_ERROR;
+        }
+
+        if ((ae = instream->read_array(mm_stream, &len1)) !=
+            AMI_ERROR_NO_ERROR) {
+            return ae;
+        }
+        
+        tp_assert(len1 = len, "Did not read the right amount; "
+                  "Allocated space for " << len << ", read " << len1 << '.');
+
+        if ((ae = (*mm_operate)(mm_stream, len)) !=
+            AMI_ERROR_NO_ERROR) {
+            return ae;
+        }
+
+        if ((ae = outstream->write_array(mm_stream, len)) !=
+            AMI_ERROR_NO_ERROR) {
+            return ae;
+        }
+
+        MM_manager->free(mm_stream);
+
+        return AMI_ERROR_NO_ERROR;
+
+    } else {
+
+        // It won't all fit, so we have to recurse.
+
+        tp_assert(0, "Recursive part not implemented yet.");
+
+        return AMI_ERROR_INSUFFICIENT_MAIN_MEMORY;
+    }
+};
+
+
+#if 0
     // How much memory do we need for each substream?
 
+    if ((ae = instream->main_memory_usage(&sz_stream,
+                                          MM_STREAM_USAGE_SUBSTREAM)) !=
+                                          AMI_ERROR_NO_ERROR) {
+        return ae;
+    }                                     
+    
     // Determine how many substreams we can merge at a time.
+
+    substream_arity = sz_avail / sz_stream;
 
     // Determine the depth of recursion.  What is important is whether
     // it is even or odd, since that will determine whether we start
     // out going to the output stream or to the shadow stream.
+
+    
     
     // If the recursion depth exceeds 1 the create a shadow stream as
     // large as the input stream.
@@ -234,6 +298,7 @@ AMI_err AMI_partition_and_merge(AMI_STREAM<T> *instream,
     
     return AMI_ERROR_NO_ERROR;
 };
+#endif
 
 #endif // _AMI_MERGE_H 
 
