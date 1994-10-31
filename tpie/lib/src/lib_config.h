57f936d4870a57a67a9687f484dc27fdbd48e51d
Darren Vengroff <dev>
1994-10-31 21:46:06 +0000

    Initial revision

---
+++ tpie/lib/src/lib_config.h
@@ -0,0 +1,27 @@
+// Copyright (c) 1994 Darren Vengroff
+//
+// File: lib_config.h
+// Author: Darren Vengroff <darrenv@eecs.umich.edu>
+// Created: 10/31/94
+//
+// $Id: lib_config.h,v 1.1 1994-10-31 21:44:55 darrenv Exp $
+//
+#ifndef _LIB_CONFIG_H
+#define _LIB_CONFIG_H
+
+// Use logs if requested.
+#if TP_LOG_LIB
+#define TPL_LOGGING 1
+#endif
+#include <tpie_log.h>
+
+// Enable assertions if requested.
+#if TP_ASSERT_LIB
+#define DEBUG_ASSERTIONS 1
+#define DEBUG_CERR 1 
+#define DEBUG_STR 1
+#endif
+#include <tpie_assert.h>
+
+
+#endif // _LIB_CONFIG_H 
