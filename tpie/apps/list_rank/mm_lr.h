8db72afdadebbb85c77e0c61ca19fad8b5b1a416
Darren Vengroff <dev>
1994-10-31 21:25:58 +0000

    Initial revision

---
+++ tpie/apps/list_rank/mm_lr.h
@@ -0,0 +1,17 @@
+// Copyright (c) 1994 Darren Vengroff
+//
+// File: mm_lr.h
+// Author: Darren Vengroff <darrenv@eecs.umich.edu>
+// Created: 10/30/94
+//
+// $Id: mm_lr.h,v 1.1 1994-10-31 21:25:58 darrenv Exp $
+//
+#ifndef _MM_LR_H
+#define _MM_LR_H
+
+#include <sys/types.h>
+#include "list_edge.h"
+
+extern int main_mem_list_rank(edge *edges, size_t count);
+
+#endif // _MM_LR_H 
