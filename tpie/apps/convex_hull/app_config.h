a101e4522bf314a29d1e1b1e5aaf718d8502f2b8
Darren Vengroff <dev>
1994-10-31 20:04:04 +0000

    Fixed to set flags for debugging and logging based on the new method that uses the configuration script.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@104 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/apps/convex_hull/app_config.h
+++ tpie/apps/convex_hull/app_config.h
@@ -4,16 +4,32 @@
 // Author: Darren Erik Vengroff <darrenv@eecs.umich.edu>
 // Created: 10/6/94
 //
-// $Id: app_config.h,v 1.2 1994-10-11 12:53:34 dev Exp $
+// $Id: app_config.h,v 1.3 1994-10-31 20:04:04 darrenv Exp $
 //
 #ifndef _APP_CONFIG_H
 #define _APP_CONFIG_H
 
+// Get the configuration set up by the configure script.
+#include <config.h>
+
 // for size_t
 #include <sys/types.h>
 
-// Use logs.
+// Use logs if requested.
+#if TP_LOG_APPS
+#define TPL_LOGGING 1
+#endif
 #include <tpie_log.h>
+
+// Enable assertions if requested.
+#if TP_ASSERT_APPS
+#define DEBUG_ASSERTIONS 1
+#define DEBUG_CERR 1
+#define DEBUG_STR 1
+#endif
+#include <tpie_assert.h>
+
+// Set up some defaults for the apps.
 
 #define DEFAULT_TEST_SIZE (1024 * 1024 * 8)
 
@@ -27,6 +43,7 @@
 //#define BTE_IMP_CACHE
 //#define BTE_IMP_STDIO
 //#define BTE_IMP_UFS
+//#define BTE_IMP_BCS
 
 #ifdef BTE_IMP_MMB
 #define BTE_MMB_LOGICAL_BLOCKSIZE_FACTOR 1
