9e907e77e6db9690df63d1e15adb889cef4579a3
Darren Vengroff <dev>
1994-11-03 16:25:48 +0000

    Fixes for Solaris.

--- tpie/lib/src/lib_config.h
+++ tpie/lib/src/lib_config.h
@@ -4,10 +4,12 @@
 // Author: Darren Vengroff <darrenv@eecs.umich.edu>
 // Created: 10/31/94
 //
-// $Id: lib_config.h,v 1.1 1994-10-31 21:44:55 darrenv Exp $
+// $Id: lib_config.h,v 1.2 1994-11-03 16:25:48 dev Exp $
 //
 #ifndef _LIB_CONFIG_H
 #define _LIB_CONFIG_H
+
+#include <config.h>
 
 // Use logs if requested.
 #if TP_LOG_LIB
