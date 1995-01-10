73ba71842741bd575fed9538c7c57a2f54dc1533
Darren Vengroff <dev>
1995-01-10 16:48:20 +0000

    Added addition test.

--- tpie/apps/old/test_bit_matrix.cpp
+++ tpie/apps/old/test_bit_matrix.cpp
@@ -5,7 +5,7 @@
 // Created: 11/4/94
 //
 
-static char test_bit_matrix_id[] = "$Id: test_bit_matrix.cpp,v 1.1 1994-12-16 21:26:04 darrenv Exp $";
+static char test_bit_matrix_id[] = "$Id: test_bit_matrix.cpp,v 1.2 1995-01-10 16:48:20 darrenv Exp $";
 
 // This is just to avoid an error message since the string above is never
 // referenced.  Note that a self referential structure must be defined to
@@ -38,6 +38,26 @@ int main(int argc, char **argv)
     bm[0][0] = bm[1][1] = bm[3][3];
     
     cout << bm << '\n';
+
+    bit_matrix bv(16, 1);
+
+    cout << (bv = (off_t)(0xF0F0)) << '\n';
+
+    off_t ot = 1234;
+
+    bv = ot;
+
+    cout << bv << '\n';
+
+    ot = 4321;
+
+    ot = bv;
+
+    cout << ot << "\n\n";
+
+    bv = bv + bv;
+
+    cout << bv << '\n';
     
     return 0;
 }
