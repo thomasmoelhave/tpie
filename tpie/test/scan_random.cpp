506511d6a62442928edc3b37b352889d1c51db35
Darren Vengroff <dev>
1997-03-20 02:38:21 +0000

    Created (then fixed) bug in assigning *sf to tell when scan in done.

--- tpie/test/scan_random.cpp
+++ tpie/test/scan_random.cpp
@@ -7,7 +7,7 @@
 // A scan management object to write a stream of random integers.
 //
 
-static char scan_random_id[] = "$Id: scan_random.cpp,v 1.1 1994-10-07 15:47:40 darrenv Exp $";
+static char scan_random_id[] = "$Id: scan_random.cpp,v 1.2 1997-03-20 02:38:21 vengroff Exp $";
 
 // Get information on the configuration to test.
 #include "app_config.h"
@@ -41,7 +41,7 @@ AMI_err scan_random::initialize(void)
 
 AMI_err scan_random::operate(int *out1, AMI_SCAN_FLAG *sf)
 {
-    if (*sf = remaining--) {
+    if ((*sf = (remaining-- != 0))) {
         *out1 = random();
         return AMI_SCAN_CONTINUE;
     } else {
