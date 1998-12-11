eef9275aaffb96715c426f02eeee934b54c23df1
Octavian Procopiuc <tave>
1998-12-11 18:55:12 +0000

    Got rid of the performance log. Part of a restructuring of LOGS functionality.

--- tpie/lib/src/tpie_log.cpp
+++ tpie/lib/src/tpie_log.cpp
@@ -6,7 +6,7 @@
 //
 
 
-static char tpie_log_id[] = "$Id: tpie_log.cpp,v 1.3 1994-06-03 13:39:12 dev Exp $";
+static char tpie_log_id[] = "$Id: tpie_log.cpp,v 1.4 1998-12-11 18:55:12 tavi Exp $";
 
 // We are logging
 #define TPL_LOGGING	1
@@ -16,17 +16,12 @@ static char tpie_log_id[] = "$Id: tpie_log.cpp,v 1.3 1994-06-03 13:39:12 dev Exp
 extern "C" char *mktemp(char *);
 
 #define TP_LOG_NAME	"/tmp/TPLOG_XXXXXX"
-#define TP_PLOG_NAME	"/tmp/TPPRF_XXXXXX"
 
 // The names of the TPIE logs
 static char tpl_name[] = TP_LOG_NAME;
-static char tp_perfl_name[] = TP_PLOG_NAME;
 
 // The main TPIE log. 
 logstream *tpl;
-
-// The performance log
-logstream *tp_perfl;
 
 // The counter of log_init instances.  It is implicity set to 0.
 unsigned int log_init::count;
@@ -36,14 +31,8 @@ unsigned int log_init::count;
 log_init::log_init(void)
 {
     if (count++ == 0) {
-        tpl = new logstream(mktemp(tpl_name), TP_LOG_INFO, TP_LOG_INFO);
-
-        // Why does this cause seg faults?
-        // LOG_INFO("Automatically created tpl.\n");
-        // LOG_FLUSH_LOG;
-
-        tp_perfl = new logstream(mktemp(tp_perfl_name), TP_LOG_INFO,
-                                 TP_LOG_INFO);
+        tpl = new logstream(mktemp(tpl_name), 
+			    TP_LOG_DEBUG_INFO, TP_LOG_DEBUG_INFO);
     }
 }
 
@@ -52,8 +41,5 @@ log_init::~log_init(void)
 {
     if (--count == 0) {
         delete tpl;
-        delete tp_perfl;
     }
 }
-
-
