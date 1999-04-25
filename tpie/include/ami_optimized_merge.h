d1b87bf2fee883721d78afc8cb9d331a8b33f7c1
Rajiv Wickremesingh <rw>
1999-04-25 01:08:04 +0000

    fixed setting workdist from environment
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@364 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/ami_optimized_merge.h
+++ tpie/include/ami_optimized_merge.h
@@ -15,7 +15,7 @@
 // a quicksort using only keys of the items; there is a provision to 
 // to use templated heaps to implement the merge.
 
-// 	$Id: ami_optimized_merge.h,v 1.27 1999-04-24 20:08:50 rbarve Exp $	
+// 	$Id: ami_optimized_merge.h,v 1.28 1999-04-25 01:08:04 rajiv Exp $	
 //TO DO: substream_count setting; don't depend on current_stream_len
 
 
@@ -192,20 +192,13 @@ XXX
 
     char * temp_string;
     temp_string = getenv(AMI_SINGLE_DEVICE_ENV);
-     if (temp_string == NULL){
-	    temp_string = getenv(TMP_DIR_ENV);
-      if (temp_string == NULL)
-	    {
-            temp_string = getenv(TMP_DIR); 
-            working_disk = tempnam(temp_string,"Temp");
-	    }
-       else
-             working_disk = tempnam(temp_string,"Temp");
-    }
-    else
-	 { 
-        working_disk = tempnam(temp_string,"Temp");
-	 }
+	if (temp_string == NULL) {
+	  temp_string = getenv(TMP_DIR_ENV);
+      if (temp_string == NULL) {
+            temp_string = TMP_DIR;
+	  }
+	}
+	working_disk = tempnam(temp_string,"Temp");
 
 
 
