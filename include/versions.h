c20dd47dc58635183157958cbb8503aa701811e1
David Hutchinson <dh>
1999-11-15 23:23:41 +0000

    Initial revision
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@506 669acd26-ddd2-4139-9e68-486997aa639d

---
+++ include/versions.h
@@ -0,0 +1,16 @@
+// File: versions.h
+// Created: 99/11/15
+
+// This file defines a macro VERSION that creates a static variable __name whose
+// contents contain the given string __id. This is intended to be used for creating
+// RCS version identifiers as static data in object files and executables.
+
+// The "compiler_fooler stuff creates a (small) self-referential structure that 
+// prevents the compiler from warning that __name is never referenced.
+
+#ifndef _VERSIONS_H
+#define _VERSIONS_H
+// Be careful : keep on a single line
+#define VERSION(__name,__id)  static char __name[] = __id;static struct __name##_compiler_fooler { char *pc; __name##_compiler_fooler *next;} the##__name##_compiler_fooler = { __name, & the##__name##_compiler_fooler};
+
+#endif // _VERSIONS_H 
