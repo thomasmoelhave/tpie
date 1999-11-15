e5001447f5ef9b33895f9e64ad354244834c270f
David Hutchinson <dh>
1999-11-15 23:25:11 +0000

    *** empty log message ***
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@507 669acd26-ddd2-4139-9e68-486997aa639d

--- include/versions.h
+++ include/versions.h
@@ -7,9 +7,11 @@
 
 // The "compiler_fooler stuff creates a (small) self-referential structure that 
 // prevents the compiler from warning that __name is never referenced.
+// 	$Id: versions.h,v 1.2 1999-11-15 23:25:11 hutchins Exp $	
 
 #ifndef _VERSIONS_H
 #define _VERSIONS_H
+
 // Be careful : keep on a single line
 #define VERSION(__name,__id)  static char __name[] = __id;static struct __name##_compiler_fooler { char *pc; __name##_compiler_fooler *next;} the##__name##_compiler_fooler = { __name, & the##__name##_compiler_fooler};
 
