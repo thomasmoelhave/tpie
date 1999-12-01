5bfa4152c1b85396ad603d5775601ddc480d2a13
David Hutchinson <dh>
1999-12-01 16:31:41 +0000

    Changed tabs to spaces for manual's \verbatim_input

--- tpie/test/sample_pgm.cpp
+++ tpie/test/sample_pgm.cpp
@@ -10,10 +10,10 @@
 //stream and so on;
 #include "app_config.h"
 
-
 //Include the file that will allow us to use AMI_STREAMs.
 #include <ami.h>
-//include wall timer that will allow us to time
+
+//include "wall clock" timer that will allow us to time
 #include <wall_timer.h>
 
 //Include TPIE's internal memory sorting routines.
@@ -34,156 +34,149 @@ extern int register_new;
 
 void
 main(int argc, char *argv[]) {
-	
-	//parse arguments
-	if (argc < 3) {
-		cout << "Input the number of integers to be generated\n";
-		cout << " and the size of memory that can be used\n";
-		exit(1);
-	}
-	int Gen_Stream_Length = atoi(argv[1]);
-	long test_mm_size = atol(argv[2]);
-	
-	//Set the size of memory the application is allowed to use
-	MM_manager.resize_heap(test_mm_size);
-	//enable TPIE memory accounting
-	register_new = 1;
-	
-	//the source stream of ints
-	AMI_STREAM<int> source;
+   
+   //parse arguments
+   if (argc < 3) {
+      cout << "Input the number of integers to be generated\n";
+      cout << " and the size of memory that can be used\n";
+      exit(1);
+   }
+   int Gen_Stream_Length = atoi(argv[1]);
+   long test_mm_size = atol(argv[2]);
+   
+   //Set the size of memory the application is allowed to use
+   MM_manager.resize_heap(test_mm_size);
+   //enable TPIE memory accounting
+   register_new = 1;
+   
+   //the source stream of ints
+   AMI_STREAM<int> source;
 
-	//the 8 bucket streams of ints
-	AMI_STREAM<int> buckets[8];
-	
-	
-	//************************************************************
-	//generate the stream of randon integers
-	AMI_err ae;
-	int src_int;
-	for (int i = 0; i < Gen_Stream_Length; i++) {
-		
-		//generate a random int
-		src_int = random();
-		
-		//Now write out the integer into the AMI_STREAM source using 
-		//the AMI_STREAM member function write_item()
-		if ((ae = source.write_item(src_int)) != AMI_ERROR_NO_ERROR) {
-			cout << "AMI_ERROR " << ae << " during source.write_item()\n";
-			exit(1);
-		}
-		
-	}
-	//print stream length
-	cout << "source stream is of length " << source.stream_len() << endl;
-	
-	
+   //the 8 bucket streams of ints
+   AMI_STREAM<int> buckets[8];
+   
+   
+   //************************************************************
+   //generate the stream of randon integers
+   AMI_err ae;
+   int src_int;
+   for (int i = 0; i < Gen_Stream_Length; i++) {
+      
+      //generate a random int
+      src_int = random();
+      
+      //Now write out the integer into the AMI_STREAM source using 
+      //the AMI_STREAM member function write_item()
+      if ((ae = source.write_item(src_int)) != AMI_ERROR_NO_ERROR) {
+         cout << "AMI_ERROR " << ae << " during source.write_item()\n";
+         exit(1);
+      }
+      
+   }
+   //print stream length
+   cout << "source stream is of length " << source.stream_len() << endl;
+   
+   //************************************************************
+   //pick the first 7 integers in source stream as partitioning elements
+   //(pivots)
+   
+   //Seek to the beginning of the AMI_STREAM source.
+   if ((ae = source.seek(0))!= AMI_ERROR_NO_ERROR) {
+      cout << "AMI_ERROR " << ae << " during source.seek()\n";
+      exit(1);
+   }
+   
+   //read first 7 integers and fill in the partitioning array
+   int partitioning[8];
+   int *read_ptr;
+   
+   for (int i = 0; i < 7; i++) {
+      
+      //Obtain a pointer to the next integer in AMI_STREAM source
+      //using the member function read_item()
+      if ((ae = source.read_item(&read_ptr)) != AMI_ERROR_NO_ERROR) {
+         cout << "AMI_ERROR " << ae << " during source.read_item()\n";
+         exit(1);
+      }
+      
+      //Copy the current source integer into the partitioning element array.
+      partitioning[i]= *read_ptr;
+   }
+   cout << "Loaded partitioning array\n";
 
-	//************************************************************
-	//pick the first 7 integers in source stream as partitioning elements
-	//(pivots)
-	
-	//Seek to the beginning of the AMI_STREAM source.
-	if ((ae = source.seek(0))!= AMI_ERROR_NO_ERROR) {
-		cout << "AMI_ERROR " << ae << " during source.seek()\n";
-		exit(1);
-	}
-	
-	//read first 7 integers and fill in the partitioning array
-	int partitioning[8];
-	int *read_ptr;
-	
-	for (int i = 0; i < 7; i++) {
-		
-		//Obtain a pointer to the next integer in AMI_STREAM source
-		//using the member function read_item()
-		if ((ae = source.read_item(&read_ptr)) != AMI_ERROR_NO_ERROR) {
-			cout << "AMI_ERROR " << ae << " during source.read_item()\n";
-			exit(1);
-		}
-		
-		//Copy the current source integer into the partitioning element array.
-		partitioning[i]= *read_ptr;
-	}
-	cout << "Loaded partitioning array\n";
+   //************************************************************
+   //sort partitioning array
+   
+   quicker_sort_op((int *)partitioning,7);
+   cout << "sorted partitioning array\n";
+   partitioning[7] = INT_MAX;
 
+   //************************************************************
+   //PARTITION INTS OF source INTO THE buckets USING partitioning ELEMENTS
+   
+   struct timeval tp1, tp2;
 
+   //binary search variables.
+   int u,v,l,j;
+   
+   //start timer
+   wall_timer wt;
+   wt.start();
+   
+   //seek to the beginning of the AMI_STREAM source.
+   if ((ae = source.seek(0))!= AMI_ERROR_NO_ERROR) {
+      cout << "AMI_ERROR " << ae << " during source.seek()\n";
+      exit(1);
+   }
+   
+   //scan source stream distributing the integers in the approriate
+   //buckets
+   for (int i = 0; i < Gen_Stream_Length; i++) {
+      
+      //Obtain a pointer to the next integer in AMI_STREAM source
+      //using the member function read_item()
+      if ((ae =   source.read_item(&read_ptr)) != AMI_ERROR_NO_ERROR) {
+         cout << "AMI_ERROR " << ae << " during source.read_item()\n";
+         exit(1);
+      }
+      v = *read_ptr;
 
-	//************************************************************
-	//sort partitioning array
-	
-	quicker_sort_op((int *)partitioning,7);
-	cout << "sorted partitioning array\n";
-	partitioning[7] = INT_MAX;
+      // using a binary search, find the stream index l to which v 
+      // should be assigned
+      l = 0;
+      u = 7;
+      while (u >= l) {
+         j = (l+u)>>1; 
+         if (v < partitioning[j]) {
+            u = j-1;
+         } else {
+            l = j+1;
+         }
+      }
 
-
-
-	//************************************************************
-	//PARTITION INTS OF source INTO THE buckets USING partitioning ELEMENTS
-	
-	struct timeval tp1, tp2;
-
-	//binary search variables.
-	int u,v,l,j;
-	
-	//start timer
-	wall_timer wt;
-	wt.start();
-	
-	//seek to the beginning of the AMI_STREAM source.
-	if ((ae = source.seek(0))!= AMI_ERROR_NO_ERROR) {
-		cout << "AMI_ERROR " << ae << " during source.seek()\n";
-		exit(1);
-	}
-	
-	//scan source stream distributing the integers in the approriate
-	//buckets
-	for (int i = 0; i < Gen_Stream_Length; i++) {
-		
-		//Obtain a pointer to the next integer in AMI_STREAM source
-		//using the member function read_item()
-		if ((ae =	source.read_item(&read_ptr)) != AMI_ERROR_NO_ERROR) {
-			cout << "AMI_ERROR " << ae << " during source.read_item()\n";
-			exit(1);
-		}
-		v = *read_ptr;
-
-		//find out l such that v is assigned to buckets[l] using binary
-		//search
-		l = 0;
-		u = 7;
-		while (u >= l) {
-			j = (l+u)>>1; 
-			if (v < partitioning[j]) {
-				u = j-1;
-			} else {
-				l = j+1;
-			}
-		}
-
-		//now write out the int into the AMI_STREAM buckets[l] using 
-		//the AMI_STREAM member function write_item().
-		if ((ae = buckets[l].write_item(v)) != AMI_ERROR_NO_ERROR) {
-			cout << "AMI_ERROR " << ae << " during buckets[" << l 
-					 << "].write_item()\n";
-			exit(1);
-		}
-	}
-	
-	//stop timer
-	wt.stop();
-	cout << "Time taken to partition is " << wt.seconds() << " seconds" << endl;
-	
-  //delete the file corresponding to the source stream when source
-  //stream gets destructed (this is the default, so this call is not
-  //needed)
-	source.persist(PERSIST_DELETE);
-	
-	//let the file corresp to buckets[i] persist on disk when the
-	//buckets[i] stream gets destructed
-	for (int i = 0; i < 8; i++) {
-		buckets[i].persist(PERSIST_PERSISTENT);
-		cout << "Length of bucket " << i << " is " 
-				 << buckets[i].stream_len() << endl;
-	}
-	
+      // now write out the int into the AMI_STREAM buckets[l] using 
+      // the AMI_STREAM member function write_item().
+      if ((ae = buckets[l].write_item(v)) != AMI_ERROR_NO_ERROR) {
+         cout << "AMI_ERROR " << ae << " during buckets[" << l 
+                << "].write_item()\n";
+         exit(1);
+      }
+   }
+   
+   //stop timer
+   wt.stop();
+   cout << "Time taken to partition is " << wt.seconds() << " seconds" << endl;
+   
+   //delete the file corresponding to the source stream when source
+   //stream gets destructed (this is the default, so this call is not
+   //needed)
+   source.persist(PERSIST_DELETE);
+   
+   //let the file corresponding to buckets[i] persist on disk when the
+   //buckets[i] stream gets destructed
+   for (int i = 0; i < 8; i++) {
+      buckets[i].persist(PERSIST_PERSISTENT);
+      cout << "Length of bucket " << i << " is " 
+             << buckets[i].stream_len() << endl;
+   }  
 }
