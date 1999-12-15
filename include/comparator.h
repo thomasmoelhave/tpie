ca114a25c6315622de59bd77943c3fd9834555a8
David Hutchinson <dh>
1999-12-15 22:08:40 +0000

    Changes to allow *inlining* of user comparison function given as 'compare' member function of a user-specified class. (1) for efficiency, the virtual member function 'compare' is undefined if AMI_VIRTUAL_BASE==0 (default)
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@527 669acd26-ddd2-4139-9e68-486997aa639d

--- include/comparator.h
+++ include/comparator.h
@@ -4,7 +4,7 @@
 // Author: Darren Vengroff <darrenv@eecs.umich.edu>
 // Created: 3/6/95
 //
-// $Id: comparator.h,v 1.1 1995-03-07 15:00:46 darrenv Exp $
+// $Id: comparator.h,v 1.2 1999-12-15 22:08:40 hutchins Exp $
 //
 #ifndef _COMPARATOR_H
 #define _COMPARATOR_H
@@ -14,7 +14,9 @@ template<class T>
 class comparator
 {
 public:
-    virtual int compare(const T &t1, const T &t2) = 0;
+#if AMI_VIRTUAL_BASE
+  virtual int compare(const T &t1, const T &t2) = 0;
+#endif
 };
 
 #endif // _COMPARATOR_H 
