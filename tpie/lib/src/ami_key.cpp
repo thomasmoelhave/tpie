298cca18ae6e28a5225d6a3d00a2c77be1237719
David Hutchinson <dh>
2000-01-10 21:38:58 +0000

    add VERSION macro

--- tpie/lib/src/ami_key.cpp
+++ tpie/lib/src/ami_key.cpp
@@ -5,10 +5,11 @@
 // Created: 3/12/95
 //
 
-
-static char ami_radix_dist_id[] = "$Id: ami_key.cpp,v 1.1 1995-06-20 18:39:26 darrenv Exp $";
-
+#include <versions.h>
 #include <ami_key.h>
+
+VERSION(ami_key_cpp,"$Id: ami_key.cpp,v 1.2 2000-01-10 21:38:58 hutchins Exp $");
+
 
 key_range::key_range(kb_key min_key, kb_key max_key) :
         min(min_key), max(max_key)
