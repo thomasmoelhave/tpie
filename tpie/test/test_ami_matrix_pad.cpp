663d71f235ecaa648b17ce52d885fef4e302211f
David Hutchinson <dh>
2000-01-11 01:16:45 +0000

    fixed typo

--- tpie/test/test_ami_matrix_pad.cpp
+++ tpie/test/test_ami_matrix_pad.cpp
@@ -6,7 +6,7 @@
 //
 
 #include <versions.h>
-VERSION(test_ami_matrix_cpp,"$Id: test_ami_matrix_pad.cpp,v 1.4 2000-01-11 01:15:45 hutchins Exp $");
+VERSION(test_ami_matrix_cpp,"$Id: test_ami_matrix_pad.cpp,v 1.5 2000-01-11 01:16:45 hutchins Exp $");
 
 #include <iostream.h>
 #include <fstream.h>
@@ -61,8 +61,6 @@ void parse_app_opt(char c, char *optarg)
             break;
     }
 }
-
-extern int register_new;
 
 int main(int argc, char **argv)
 {
