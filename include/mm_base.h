30deca9331e038da974c18827dd49ef941fb9845
David Hutchinson <dh>
2000-01-14 19:12:58 +0000

    initial version of new memory manager mm_base.h mm_base.cpp mm_register.h mm_register.cpp
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@558 669acd26-ddd2-4139-9e68-486997aa639d

--- include/mm_base.h
+++ include/mm_base.h
@@ -4,12 +4,17 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/30/94
 //
-// $Id: mm_base.h,v 1.4 1994-09-22 14:56:32 darrenv Exp $
+// $Id: mm_base.h,v 1.5 2000-01-14 19:09:18 hutchins Exp $
 //
 #ifndef _MM_BASE_H
 #define _MM_BASE_H
 
 #include <sys/types.h>
+
+// dh. MM accounting modes
+#define MM_IGNORE_MEMORY_EXCEEDED   0
+#define MM_ABORT_ON_MEMORY_EXCEEDED 1
+#define MM_WARN_ON_MEMORY_EXCEEDED  2
 
 // MM Error codes
 enum MM_err {
@@ -37,41 +42,25 @@ enum MM_stream_usage {
 // managers.  In a uniprocessor, these objects will simply contain
 // pointers.  In multiprocessors, they will be more complicated
 // descriptions of the layout of memory.
+
 class MM_ptr_base
 {
 public:
     // This should return 1 to indicate a valid pointer and 0 to
-    // indicate an invalid one.  It is usefull for tests and
+    // indicate an invalid one.  It is useful for tests and
     // assertions.
     virtual operator int (void) = 0;
 };
 
 // The base class for all memory management objects.
+
 class MM_manager_base
 {
 public:
+
     // How much is currently available.
-    virtual MM_err available(size_t *sz_a) = 0;
-#if 0
-    // Allocate some space.
-    virtual MM_err alloc(size_t req, MM_ptr_base *p) = 0;
-    // Free space.
-    virtual MM_err free(MM_ptr_base *p) = 0;
+    virtual MM_err available (size_t *sz_a) = 0;
 
-    // Registration for main memory usage that cannot be allocated
-    // directly through the memory manager.  Use of these is
-    // discouraged, but sometimes unavoidable, such as in accounting
-    // for space used in the buffer cache.
-    
-    virtual MM_err register_allocation(size_t sz) = 0;
-    virtual MM_err register_deallocation(size_t sz) = 0;
-#endif
-    
 };
-
-#if 0
-// A pointer to the one and only memory manager.
-extern MM_manager_base *mm_manager;
-#endif
 
 #endif // _MM_BASE_H 
