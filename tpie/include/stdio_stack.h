5eb9f82153d9b4512b602590eecea11aa9df2e99
Octavian Procopiuc <tave>
2000-08-06 15:13:26 +0000

    *** empty log message ***
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@592 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/stdio_stack.h
+++ tpie/include/stdio_stack.h
@@ -1,15 +1,18 @@
-//File: stdio_stack.h
-//Author: Rakesh Barve
-//Defining a stack based on bte_stdio separately specifically
-//for use in block collection class and related apps.
-//The reason we don't want to use ami_stack is because 
-//then the stack would be implemented as a BTE_STREAM, which
-//may have large block size etc. which is undesirable for 
-//stacks related to block collections since such a stack is only
+//
+// File:	stdio_stack.h
+// Authors:	Rakesh Barve
+//
+// Defining a stack based on bte_stdio separately specifically
+// for use in block collection class and related apps.
+// The reason we don't want to use ami_stack is because 
+// then the stack would be implemented as a BTE_STREAM, which
+// may have large block size etc. which is undesirable for 
+// stacks related to block collections since such a stack is only
 // a meta data structure accessed no more than once every block
 // is created or destroyed. 
-//// 	$Id: stdio_stack.h,v 1.3 1999-09-30 16:09:52 tavi Exp $	
-
+//
+// 	$Id: stdio_stack.h,v 1.4 2000-08-06 15:13:26 tavi Exp $	
+//
 
 #ifndef _STDIO_STACK_H
 #define _STDIO_STACK_H
@@ -19,16 +22,22 @@
 template<class T>
 class stdio_stack : public BTE_stream_stdio<T> {
 public:
-    stdio_stack(char *path); 
-    ~stdio_stack(void);
-    BTE_err push(const T &t);
-    BTE_err pop(T **t);
+
+  stdio_stack(char *path, BTE_stream_type type = BTE_WRITE_STREAM); 
+
+  ~stdio_stack(void);
+
+  BTE_err push(const T &t);
+
+  BTE_err pop(T **t);
+
 };
 
 
 template<class T>
-stdio_stack<T>::stdio_stack(char *path) :
-        BTE_stream_stdio<T>(path, BTE_WRITE_STREAM)
+stdio_stack<T>::stdio_stack(char *path, 
+			    BTE_stream_type type = BTE_WRITE_STREAM) :
+  BTE_stream_stdio<T>(path, type)
 {
 }
 
@@ -40,41 +49,41 @@ stdio_stack<T>::~stdio_stack(void)
 template<class T>
 BTE_err stdio_stack<T>::push(const T &t)
 {
-    BTE_err ae;
-    off_t slen;
+  BTE_err ae;
+  off_t slen;
     
-    ae = truncate((slen = stream_len())+1);
-    if (ae != BTE_ERROR_NO_ERROR) {
-        return ae;
-    }
+  ae = truncate((slen = stream_len())+1);
+  if (ae != BTE_ERROR_NO_ERROR) {
+    return ae;
+  }
 
-    ae = seek(slen);
-    if (ae != BTE_ERROR_NO_ERROR) {
-        return ae;
-    }
+  ae = seek(slen);
+  if (ae != BTE_ERROR_NO_ERROR) {
+    return ae;
+  }
 
-    return write_item(t);
+  return write_item(t);
 }
 
 
 template<class T>
 BTE_err stdio_stack<T>::pop(T **t)
 {
-    BTE_err ae;
-    off_t slen;
+  BTE_err ae;
+  off_t slen;
+  
+  slen = stream_len();
+  ae = seek(slen-1);
+  if (ae != BTE_ERROR_NO_ERROR) {
+    return ae;
+  }
+  
+  ae = read_item(t);
+  if (ae != BTE_ERROR_NO_ERROR) {
+    return ae;
+  }
 
-    slen = stream_len();
-    ae = seek(slen-1);
-    if (ae != BTE_ERROR_NO_ERROR) {
-      return ae;
-    }
-
-    ae = read_item(t);
-    if (ae != BTE_ERROR_NO_ERROR) {
-      return ae;
-    }
-
-    return truncate(slen-1);
+  return truncate(slen-1);
 }
 
 #endif // _stdio_stack_H 
