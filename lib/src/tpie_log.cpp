0c24965e83b607fde15b4a37c1b2a75f69a0aed0
Octavian Procopiuc <tave>
2001-02-19 20:52:24 +0000

    Got rid of Darren's init_log class and replaced it with function theLog, which creates the log once, and returns the same log each time. This solves problems appearing when other initialization needed the log, but the log was not initialized. (used idea from Item 47 of Scott Meyer's Effective C++).
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@633 669acd26-ddd2-4139-9e68-486997aa639d

--- lib/src/tpie_log.cpp
+++ lib/src/tpie_log.cpp
@@ -6,7 +6,7 @@
 //
 
 #include <versions.h>
-VERSION(tpie_log_cpp,"$Id: tpie_log.cpp,v 1.6 2000-11-05 19:51:48 tavi Exp $");
+VERSION(tpie_log_cpp,"$Id: tpie_log.cpp,v 1.7 2001-02-19 20:52:24 tavi Exp $");
 
 // We are logging
 #define TPL_LOGGING	1
@@ -19,35 +19,9 @@ extern "C" char *mktemp(char *);
 
 #define TP_LOG_NAME	"/tmp/TPLOG_XXXXXX"
 
-// The names of the TPIE logs
-static char tpl_name[] = TP_LOG_NAME;
-
-// The main TPIE log.  tavi: added this instead of the log_init
-// initialization. Should we get rid of the log_init class and put
-// tpl=&__tpl?
-logstream __tpl(mktemp(tpl_name),TP_LOG_DEBUG_INFO, TP_LOG_DEBUG_INFO);
-logstream *tpl;
-
-// The counter of log_init instances.  It is implicity set to 0.
-unsigned int log_init::count;
-
-// The constructor and destructor that ensure that the log files are
-// created exactly once, and destroyed when appropriate.
-log_init::log_init(void)
-{
-    if (count++ == 0) {
-      // tavi: removed the new initialization due to errors (the log
-      // was used before it was initialized).
-      tpl = &__tpl;
-      //tpl = new logstream(mktemp(tpl_name), 
-      //		  TP_LOG_DEBUG_INFO, TP_LOG_DEBUG_INFO);
-     }
-}
-
-
-log_init::~log_init(void)
-{
-    if (--count == 0) {
-      //delete tpl;
-    }
+// tavi: 02/19/2001 deleted all of Darren's stuff b/c was giving seg faults.
+logstream &theLog() {
+  static char tpl_name[] = TP_LOG_NAME;
+  static logstream log(mktemp(tpl_name),TP_LOG_DEBUG_INFO, TP_LOG_DEBUG_INFO);
+  return log;
 }
