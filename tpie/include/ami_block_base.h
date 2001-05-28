4d941ab0a06df720ef08a2fe5c14ec54e71e9edd
Octavian Procopiuc <tave>
2001-05-28 18:56:19 +0000

    Added BTECOLL template to block and collection classes.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@658 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/ami_block_base.h
+++ tpie/include/ami_block_base.h
@@ -3,7 +3,7 @@
 // File:    ami_block_base.h
 // Author:  Octavian Procopiuc <tavi@cs.duke.edu>
 //
-// $Id: ami_block_base.h,v 1.1 2001-05-17 19:45:02 tavi Exp $
+// $Id: ami_block_base.h,v 1.2 2001-05-28 18:56:19 tavi Exp $
 //
 // Definition of the AMI_block_base class.
 //
@@ -16,6 +16,9 @@
 // The Block_stats class.
 #include <ami_block_stats.h>
 
+// AMI block id type.
+typedef off_t AMI_bid;
+
 //
 // Block status type
 //
@@ -24,14 +27,16 @@ enum AMI_block_status {
   AMI_BLOCK_STATUS_INVALID = 1,
 };
 
+
+template<class BTECOLL>
 class AMI_block_base {
 protected:
 
   // Pointer to the block collection.
-  BTE_COLLECTION * pcoll_;
+  BTECOLL * pcoll_;
 
   // Unique ID. Represents the offset of the block in the blocks file.
-  AMI_block_id bid_;
+  AMI_bid bid_;
 
   // Dirty bit. If set, the block needs to be written back.
   char dirty_;
@@ -42,31 +47,22 @@ protected:
   // Persistence flag.
   persistence per_;
 
-  // Statistics object, one for all blocks.
-  static AMI_block_stats stats_;
-
-  friend AMI_block_stats& AMI_block_statistics();
-
 public:
 
   // Constructor.
   // Read and initialize a block with a given ID.
   // When bid is missing or 0, a new block is created.
-  AMI_block_base(AMI_COLLECTION* pacoll, AMI_block_id bid = 0)
+  AMI_block_base(AMI_COLLECTION_NT<BTECOLL>* pacoll, AMI_bid bid = 0)
     : bid_(bid), dirty_(0), per_(PERSIST_PERSISTENT) {
     pcoll_ = pacoll->bte();
     if (bid != 0) {
       // Get an existing block from disk.
       if (pcoll_->get_block(bid_, pdata_) != BTE_ERROR_NO_ERROR)
 	pdata_ = NULL;
-      else
-	stats_.record(BLOCK_READ);
     } else {
       // Create a new block in the collection.
       if (pcoll_->new_block(bid_, pdata_) != BTE_ERROR_NO_ERROR)
 	pdata_ = NULL;
-      else
-	stats_.record(BLOCK_CREATE);
     }
   }
 
@@ -78,14 +74,14 @@ public:
   }
 
   // Get the block id.
-  AMI_block_id bid() const { return bid_; }
+  AMI_bid bid() const { return bid_; }
 
   // Get a reference to the dirty bit.
   char& dirty() { return dirty_; };
   char dirty() const { return dirty_; }
 
   // Copy block rhs into this block.
-  AMI_block_base& operator=(const AMI_block_base& rhs) { 
+  AMI_block_base<BTECOLL>& operator=(const AMI_block_base<BTECOLL>& rhs) { 
     if (pcoll_ == rhs.pcoll_) {
       memcpy(pdata_, rhs.pdata_, pcoll_->block_size());
       dirty_ = 1;
@@ -113,21 +109,14 @@ public:
       if (per_ == PERSIST_PERSISTENT) {
 	// Write back the block.
 	if (pcoll_->put_block(bid_, pdata_) == BTE_ERROR_NO_ERROR)
-	  stats_.record(BLOCK_WRITE);
+	  ;
       } else {
 	// Delete the block from the collection.
 	if (pcoll_->delete_block(bid_, pdata_) == BTE_ERROR_NO_ERROR)
-	  stats_.record(BLOCK_DELETE);
+	  ;
       }
   }
 };
 
-// *AMI_block_base::stats_*
-AMI_block_stats AMI_block_base::stats_;
-
-
-AMI_block_stats& AMI_block_statistics() {
-  return AMI_block_base::stats_;
-}
 
 #endif _AMI_BLOCK_BASE_H
