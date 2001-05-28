5f748a333d58b117ca28169353ee73d53dcf02f3
Octavian Procopiuc <tave>
2001-05-28 18:54:30 +0000

    Added BTECOLL template to the block and collection classes.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@657 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/ami_block.h
+++ tpie/include/ami_block.h
@@ -5,7 +5,7 @@
 //
 // Definition and implementation of the AMI_block class.
 //
-// $Id: ami_block.h,v 1.1 2001-05-17 19:49:42 tavi Exp $
+// $Id: ami_block.h,v 1.2 2001-05-28 18:54:30 tavi Exp $
 //
 
 #ifndef _AMI_BLOCK_H
@@ -17,25 +17,25 @@
 // The b_vector class.
 #include <b_vector.h>
  
-template<class E, class I>
-class AMI_block: public AMI_block_base {
+template<class E, class I, class BTECOLL=BTE_COLLECTION>
+class AMI_block: public AMI_block_base<BTECOLL> {
 
 public:
   
   // The array of links.
-  b_vector<AMI_block_id> lk;
+  b_vector<AMI_bid> lk;
 
   // The array of elements.
   b_vector<E> el;
 
-  typedef b_vector<AMI_block_id>::iterator  lk_iterator;
+  typedef b_vector<AMI_bid>::iterator  lk_iterator;
   typedef b_vector<E>::iterator  el_iterator;
 
 public:
 
   // Constructor.  Read and initialize a block with a given ID. If the
   // ID is missing or 0, a new block is created.
-  AMI_block(AMI_COLLECTION* pacoll, size_t links, AMI_block_id bid = 0);
+  AMI_block(AMI_COLLECTION_NT<BTECOLL>* pacoll, size_t links, AMI_bid bid = 0);
 
   // Get a reference to the info field.
   I* info();
@@ -51,24 +51,24 @@ public:
 ////////// **AMI_block** ///////////
 ////////////////////////////////
 
-template<class E, class I>
-AMI_block<E,I>::AMI_block(AMI_COLLECTION* pacoll, 
-		  size_t links, AMI_block_id bid):
-  AMI_block_base(pacoll, bid), lk((AMI_block_id*)pdata_, links),
-  el((E*) ((char*) pdata_ + links * sizeof(AMI_block_id)),
-     (size_t) ((pcoll_->block_size()-sizeof(I)-links*sizeof(AMI_block_id))/sizeof(E)))
+template<class E, class I, class BTECOLL>
+AMI_block<E,I,BTECOLL>::AMI_block(AMI_COLLECTION_NT<BTECOLL>* pacoll, 
+		  size_t links, AMI_bid bid):
+  AMI_block_base<BTECOLL>(pacoll, bid), lk((AMI_bid*)pdata_, links),
+  el((E*) ((char*) pdata_ + links * sizeof(AMI_bid)),
+     (size_t) ((pcoll_->block_size()-sizeof(I)-links*sizeof(AMI_bid))/sizeof(E)))
 {
 }
 
-template<class E, class I>
-I* AMI_block<E,I>::info() {
-  return (I*) (((char*) pdata_ + (lk.capacity()*sizeof(AMI_block_id) + 
+template<class E, class I, class BTECOLL>
+I* AMI_block<E,I,BTECOLL>::info() {
+  return (I*) (((char*) pdata_ + (lk.capacity()*sizeof(AMI_bid) + 
 				  el.capacity()*sizeof(E))));
 }
 
-template<class E, class I>
-const I*  AMI_block<E,I>::info() const {
-  return (I*) (((char*) pdata_ + (lk.capacity()*sizeof(AMI_block_id) + 
+template<class E, class I, class BTECOLL>
+const I*  AMI_block<E,I,BTECOLL>::info() const {
+  return (I*) (((char*) pdata_ + (lk.capacity()*sizeof(AMI_bid) + 
 				  el.capacity()*sizeof(E))));
 }
 
