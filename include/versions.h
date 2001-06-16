f445b970201672f37c8f30d04ec44235af2202cd
Octavian Procopiuc <tave>
2001-06-16 20:03:12 +0000

    Aesthetic changes. Nothing of consequence.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@677 669acd26-ddd2-4139-9e68-486997aa639d

--- include/versions.h
+++ include/versions.h
@@ -10,20 +10,18 @@
 // structure that prevents the compiler from warning that __name is
 // never referenced.
 
-// 	$Id: versions.h,v 1.3 2000-03-14 01:40:38 rajiv Exp $	
+// $Id: versions.h,v 1.4 2001-06-16 20:03:12 tavi Exp $
 
 #ifndef _VERSIONS_H
 #define _VERSIONS_H
 
-
-#define VERSION(__name,__id)											\
-  static char __name[] = __id;											\
-  static struct __name##_compiler_fooler {								\
-	char *pc;															\
-	__name##_compiler_fooler *next;										\
-  }																		\
-  the##__name##_compiler_fooler = { __name, 							\
-									& the##__name##_compiler_fooler};
-
+#define VERSION(__name,__id)                    \
+  static char __name[] = __id;                  \
+  static struct __name##_compiler_fooler {	\
+	char *pc;				\
+	__name##_compiler_fooler *next;		\
+  }						\
+  the##__name##_compiler_fooler = { __name, 	\
+       & the##__name##_compiler_fooler};
 
 #endif // _VERSIONS_H 
