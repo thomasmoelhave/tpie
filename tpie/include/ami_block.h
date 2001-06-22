d9d419e9343f375192f5a04d036ed576fbf974b2
Octavian Procopiuc <tave>
2001-06-22 02:36:34 +0000

    Added the static el_capacity method.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@684 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/ami_block.h
+++ tpie/include/ami_block.h
@@ -5,7 +5,7 @@
 //
 // Definition and implementation of the AMI_block class.
 //
-// $Id: ami_block.h,v 1.2 2001-05-28 18:54:30 tavi Exp $
+// $Id: ami_block.h,v 1.3 2001-06-22 02:36:34 tavi Exp $
 //
 
 #ifndef _AMI_BLOCK_H
@@ -19,7 +19,6 @@
  
 template<class E, class I, class BTECOLL=BTE_COLLECTION>
 class AMI_block: public AMI_block_base<BTECOLL> {
-
 public:
   
   // The array of links.
@@ -32,6 +31,11 @@ public:
   typedef b_vector<E>::iterator  el_iterator;
 
 public:
+  // Compute the capacity of the el vector statically (but you have to
+  // give the correct block size and number of links!). It can also be
+  // used to figure out how many elements would fit into a block with
+  // a given size and a given number of links.
+  static size_t el_capacity(size_t block_size, size_t links);
 
   // Constructor.  Read and initialize a block with a given ID. If the
   // ID is missing or 0, a new block is created.
@@ -47,16 +51,22 @@ public:
 
 
 
-////////////////////////////////
+////////////////////////////////////
 ////////// **AMI_block** ///////////
-////////////////////////////////
+////////////////////////////////////
+
+template<class E, class I, class BTECOLL>
+size_t AMI_block<E,I,BTECOLL>::el_capacity(size_t block_size, size_t links) {
+  return (size_t) ((block_size - sizeof(I) - links * sizeof(AMI_bid)) / sizeof(E));
+}
 
 template<class E, class I, class BTECOLL>
 AMI_block<E,I,BTECOLL>::AMI_block(AMI_COLLECTION_NT<BTECOLL>* pacoll, 
 		  size_t links, AMI_bid bid):
-  AMI_block_base<BTECOLL>(pacoll, bid), lk((AMI_bid*)pdata_, links),
-  el((E*) ((char*) pdata_ + links * sizeof(AMI_bid)),
-     (size_t) ((pcoll_->block_size()-sizeof(I)-links*sizeof(AMI_bid))/sizeof(E)))
+  AMI_block_base<BTECOLL>(pacoll, bid), 
+  lk((AMI_bid*)pdata_, links),
+  el((E*) ((char*) pdata_ + links * sizeof(AMI_bid)), 
+     el_capacity(pcoll_->block_size(), links))
 {
 }
 
