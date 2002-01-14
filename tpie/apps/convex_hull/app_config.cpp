7416ecc8769ab10fa315a80ca5eb9923c81434d2
Octavian Procopiuc <tave>
2002-01-14 17:39:56 +0000

    verbose is now true by default.

--- tpie/apps/convex_hull/app_config.cpp
+++ tpie/apps/convex_hull/app_config.cpp
@@ -5,15 +5,11 @@
 // Created: 10/7/94
 //
 
-
-
-static char app_config_id[] = "$Id: app_config.cpp,v 1.3 1999-06-28 22:07:58 laura Exp $";
-
+static char app_config_id[] = "$Id: app_config.cpp,v 1.4 2002-01-14 17:39:56 tavi Exp $";
 
 #include "app_config.h"
 
-bool verbose = false;
-
+bool verbose = true;
 size_t test_mm_size = DEFAULT_TEST_MM_SIZE;
 size_t test_size = DEFAULT_TEST_SIZE;
 int random_seed = 17;
