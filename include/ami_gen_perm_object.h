587699294367360266e0eaaf124caef58e43f54e
Octavian Procopiuc <tave>
2002-01-14 15:53:05 +0000

    UPdated for new naming convention.?\027[D?\027[C?\127
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@725 669acd26-ddd2-4139-9e68-486997aa639d

--- include/ami_gen_perm_object.h
+++ include/ami_gen_perm_object.h
@@ -1,16 +1,15 @@
-// Copyright (c) 1994 Darren Vengroff
 //
 // File: ami_gen_perm_object.h
 // Author: Darren Vengroff <darrenv@eecs.umich.edu>
 // Created: 12/15/94
 //
-// $Id: ami_gen_perm_object.h,v 1.2 1998-12-11 18:38:20 tavi Exp $
+// $Id: ami_gen_perm_object.h,v 1.3 2002-01-14 15:53:05 tavi Exp $
 //
 #ifndef _AMI_GEN_PERM_OBJECT_H
 #define _AMI_GEN_PERM_OBJECT_H
 
 // For AMI_err.
-#include <ami_base.h>
+#include <ami_err.h>
 
 // A class of object that computes permutation destinations.
 class AMI_gen_perm_object {
