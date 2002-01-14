ce851596f030f987dc02736fe59e06bba4f01daa
Octavian Procopiuc <tave>
2002-01-14 17:45:33 +0000

    *** empty log message ***
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@756 669acd26-ddd2-4139-9e68-486997aa639d

--- apps/convex_hull/parse_args.cpp
+++ apps/convex_hull/parse_args.cpp
@@ -1,4 +1,3 @@
-// Copyright (c) 1994 Darren Erik Vengroff
 //
 // File: parse_args.cpp
 // Author: Darren Erik Vengroff <darrenv@eecs.umich.edu>
@@ -6,7 +5,7 @@
 //
 
 
-static char parse_args_id[] = "$Id: parse_args.cpp,v 1.9 2001-02-12 16:27:11 tavi Exp $";
+static char parse_args_id[] = "$Id: parse_args.cpp,v 1.10 2002-01-14 17:42:37 tavi Exp $";
 
 #include <stdlib.h>
 #include <strings.h>
@@ -18,10 +17,10 @@ static char parse_args_id[] = "$Id: parse_args.cpp,v 1.9 2001-02-12 16:27:11 tav
 #include "app_config.h"
 #include "parse_args.h"
 
-static size_t
-parse_number(char *s) {
-  size_t n, mult=1;
-  int len = strlen(s);
+static size_t parse_number(char *s) {
+  size_t n; 
+  size_t mult = 1;
+  size_t len = strlen(s);
   if(isalpha(s[len-1])) {
     switch(s[len-1]) {
     case 'M':
@@ -41,45 +40,46 @@ parse_number(char *s) {
   return n * mult;
 }
 
+
 void parse_args(int argc, char **argv, const char *as_opts,
                 void (*parse_app_opt)(char opt, char *optarg))
 {
-    static char standard_opts[] = "m:t:z:v";
+  static char standard_opts[] = "m:t:z:v";
+  
+  // All options, standard and application specific.
+  char *all_opts;
 
-    // All options, stnadard and application specific.
-    char *all_opts;
-
-    if (as_opts != NULL) {
-      unsigned int l_aso;
-      
-      all_opts = new char[sizeof(standard_opts) + (l_aso = strlen(as_opts))]; 
-      strncpy(all_opts, standard_opts, sizeof(standard_opts));
-      strncat(all_opts, as_opts, l_aso);
-    } else {
-      all_opts = standard_opts;
+  if (as_opts != NULL) {
+    unsigned int l_aso;
+    
+    all_opts = new char[sizeof(standard_opts) + (l_aso = strlen(as_opts))]; 
+    strncpy(all_opts, standard_opts, sizeof(standard_opts));
+    strncat(all_opts, as_opts, l_aso);
+  } else {
+    all_opts = standard_opts;
+  }
+  
+  char c;
+  
+  optarg = NULL;
+  while((c = getopt(argc, argv, all_opts)) != -1) {
+    switch (c) {
+    case 'v':
+      verbose = true;
+      break;
+    case 'm':
+      test_mm_size = parse_number(optarg);
+      break;                
+    case 't':
+      test_size = parse_number(optarg);
+      break;
+    case 'z':
+      random_seed = atol(optarg);
+      break;
+    default:
+      parse_app_opt(c, optarg);
+      break;
     }
-
-    char c;
-
-    optarg = NULL;
-    while((c = getopt(argc, argv, all_opts)) != -1) {
-      switch (c) {
-      case 'v':
-	verbose = 1;
-	break;
-      case 'm':
-	test_mm_size = parse_number(optarg);
-	break;                
-      case 't':
-	test_size = parse_number(optarg);
-	break;
-      case 'z':
-	random_seed = atol(optarg);
-	break;
-      default:
-	parse_app_opt(c, optarg);
-	break;
-      }
-    }
+  }
 }
 
