20d7e635655db7cabf3cb05a0e90eec8d3707186
Octavian Procopiuc <tave>
2002-01-14 16:04:13 +0000

    Changed AMI_IMP_SINGLE to AMI_STREAM_IMP_SINGLE to reflect new naming convention.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@729 669acd26-ddd2-4139-9e68-486997aa639d

--- include/ami_sort.h
+++ include/ami_sort.h
@@ -1,17 +1,16 @@
-// Copyright (c) 1994 Darren Erik Vengroff
 //
 // File: ami_sort.h
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 6/10/94
 //
-// $Id: ami_sort.h,v 1.8 2000-04-17 02:03:31 hutchins Exp $
+// $Id: ami_sort.h,v 1.9 2002-01-14 16:04:13 tavi Exp $
 //
 #ifndef _AMI_SORT_H
 #define _AMI_SORT_H
 
 #define CONST const
 
-#ifdef AMI_IMP_SINGLE
+#ifdef AMI_STREAM_IMP_SINGLE
 #include <ami_sort_single.h>
 #include <ami_optimized_sort.h>
 #include <ami_sort_single_dh.h>
