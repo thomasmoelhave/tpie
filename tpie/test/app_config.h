6b196848fa96b26f6a4f6bb9ddd0175d679a254d
Octavian Procopiuc <tave>
2002-01-25 23:26:50 +0000

    Cleanup.

--- tpie/test/app_config.h
+++ tpie/test/app_config.h
@@ -3,7 +3,7 @@
 // Author: Darren Erik Vengroff <darrenv@eecs.umich.edu>
 // Created: 10/6/94
 //
-// $Id: app_config.h,v 1.22 2002-01-15 03:53:03 tavi Exp $
+// $Id: app_config.h,v 1.23 2002-01-25 23:26:50 tavi Exp $
 //
 #ifndef _APP_CONFIG_H
 #define _APP_CONFIG_H
@@ -11,78 +11,59 @@
 // Get the configuration as set up by the TPIE configure script.
 #include <config.h>
 
-/* ********************************************************************** */
-/*                      developer use                                     */
-/* ********************************************************************** */
+
+//// ****Developer use**** ////
 
 
-/* ********************************************************************** */
-/*                  choose default BTE collection implementation          */
-/* ********************************************************************** */
 
-/* Pick a version of BTE collections; default is BTE_COLLECTION_IMP_MMAP */
+//// ****Choose default BTE collection implementation**** ////
+
+// Define only one (default is BTE_COLLECTION_IMP_MMAP)
 #define BTE_COLLECTION_IMP_MMAP
 //#define BTE_COLLECTION_IMP_UFS
+//#define BTE_COLLECTION_IMP_USER_DEFINED
 
-/* ********************************************************************** */
-/*                       choose BTE stream implementation                 */
-/* ********************************************************************** */
+//// ****Choose BTE stream implementation**** ////
 
-/* Pick a version of BTE streams; default is BTE_STREAM_IMP_UFS */
+// Define only one (default is BTE_STREAM_IMP_UFS)
+#define BTE_STREAM_IMP_UFS
 //#define BTE_STREAM_IMP_MMAP
 //#define BTE_STREAM_IMP_STDIO
-#define BTE_STREAM_IMP_UFS
+//#define BTE_STREAM_IMP_USER_DEFINED
 
 
-/* ********************************************************************** */
-/*                      configure BTE                                     */
-/* ********************************************************************** */
+//// ****BTE_STREAM_MMAP configuration options**** ////
 
-
-/* ********************************************************************** */
-/*                      BTE_MMB configuration options                     */
-/* ********************************************************************** */
 #ifdef BTE_STREAM_IMP_MMAP
-
-/* define logical blocksize; default is 32 * operating system blocksize */
-#define BTE_STREAM_MMAP_BLOCK_FACTOR 32
-
-/* enable/disable TPIE read ahead; default is enabled (set to 1) */
-#define BTE_MMB_READ_AHEAD 1
-
-/* read ahead method, ignored unless BTE_MMB_READ_AHEAD is set to 1;
+   // Define logical blocksize factor (default is 32)
+#  define BTE_STREAM_MMAP_BLOCK_FACTOR 32
+   // Enable/disable TPIE read ahead; default is enabled (set to 1)
+#  define BTE_MMB_READ_AHEAD 1
+   /* read ahead method, ignored unless BTE_MMB_READ_AHEAD is set to 1;
    if USE_LIBAIO is enabled, use asynchronous IO read ahead; otherwise
    use use mmap-based read ahead; default is mmap-based read ahead
    (USE_LIBAIO not defined) */
-//#define USE_LIBAIO
-
+   //#define USE_LIBAIO
 #endif
 
 
-/* ********************************************************************** */
-/*                      BTE_UFS configuration options                     */
-/* ********************************************************************** */
+//// ****BTE_UFS configuration options**** ////
 #ifdef BTE_STREAM_IMP_UFS
-
-/* define logical blocksize; default is 32 * operating system blocksize */
-#define BTE_STREAM_UFS_BLOCK_FACTOR 32
-
-/* enable/disable TPIE read ahead; default is disabled (set to 0) */
-#define BTE_UFS_READ_AHEAD 0
-
-/* read ahead method, ignored unless BTE_UFS_READ_AHEAD is set to 1;
+   // Define logical blocksize factor (default is 32)
+#  define BTE_STREAM_UFS_BLOCK_FACTOR 32
+   // Enable/disable TPIE read ahead; default is disabled (set to 0)
+#  define BTE_UFS_READ_AHEAD 0
+   /* read ahead method, ignored unless BTE_UFS_READ_AHEAD is set to 1;
    if USE_LIBAIO is set to 1, use asynchronous IO read ahead;
    otherwise no TPIE read ahead is done; default is disabled (set to
    0) */
-#define USE_LIBAIO 0
-
+#  define USE_LIBAIO 0
 #endif
-/********************************************************************/
 
 
-/********************************************************************/
-/*              Set up some defaults for the apps                   */
-/********************************************************************/
+
+//// ****Set up some defaults for the apps**** ////
+
 #include <sys/types.h> // for size_t
 #include <stdlib.h> // for random
 
@@ -93,11 +74,11 @@ extern bool verbose;
 extern size_t test_mm_size;
 extern size_t test_size;
 extern int random_seed;
-/********************************************************************/
 
 
+
 /********************************************************************/
-/*                            logging;                              */
+/*                     logging and assertions;                      */
 /*              this should NOT be modified by user!!!              */
 /*       in order to enable/disable library/application logging,    */
 /*     run tpie configure script with appropriate options           */
@@ -114,6 +95,5 @@ extern int random_seed;
 #define DEBUG_ASSERTIONS 1
 #endif
 #include <tpie_assert.h>
-/********************************************************************/
 
 #endif
