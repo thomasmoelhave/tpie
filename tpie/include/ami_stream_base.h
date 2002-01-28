c9929361a724e28faf3596195e4eff7d8fd3c89d
Octavian Procopiuc <tave>
2002-01-28 16:50:03 +0000

    Introduced an is_valid() method to avoid remebering all these STATUS constants.

--- tpie/include/ami_stream_base.h
+++ tpie/include/ami_stream_base.h
@@ -3,7 +3,7 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/19/94
 //
-// $Id: ami_stream_base.h,v 1.1 2002-01-14 16:09:45 tavi Exp $
+// $Id: ami_stream_base.h,v 1.2 2002-01-28 16:50:03 tavi Exp $
 //
 #ifndef _AMI_STREAM_BASE_H
 #define _AMI_STREAM_BASE_H
@@ -43,6 +43,8 @@ public:
 
   AMI_stream_base(void) { status_ = AMI_STREAM_STATUS_INVALID; }
   AMI_stream_status status() const { return status_; }
+  bool is_valid() const { return status_ == AMI_STREAM_STATUS_VALID; }
+  // TODO: Does this need to be virtual?
   virtual ~AMI_stream_base(void) {}
   
 #if AMI_VIRTUAL_BASE
