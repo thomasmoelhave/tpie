5f938d79a0ae95b91a416b3b99e4beb95514293c
Octavian Procopiuc <tave>
2002-02-04 06:20:20 +0000

    Changed mktemp() to tempnam() to avoid some warnings on Linux. Cleanup.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@797 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/lib/src/tpie_log.cpp
+++ tpie/lib/src/tpie_log.cpp
@@ -1,4 +1,3 @@
-// Copyright (c) 1994 Darren Erik Vengroff
 //
 // File: tpie_log.cpp
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
@@ -6,22 +5,21 @@
 //
 
 #include <versions.h>
-VERSION(tpie_log_cpp,"$Id: tpie_log.cpp,v 1.8 2001-06-16 19:43:16 tavi Exp $");
+VERSION(tpie_log_cpp,"$Id: tpie_log.cpp,v 1.9 2002-02-04 06:20:20 tavi Exp $");
 
 // We are logging
 #define TPL_LOGGING	1
 
+// For tempnam().
+#include <stdio.h>
+
 #include <tpie_log.h>
-#include <string.h>
-#include <stdlib.h>
 
-extern "C" char *mktemp(char *);
+#define TP_LOG_DIR "/tmp"
+#define TP_LOG_PFX "TPLOG"
 
-#define TP_LOG_NAME	"/tmp/TPLOG_XXXXXX"
-
-// tavi (02/19/2001): deleted all of Darren's stuff b/c it was giving seg faults.
 logstream &theLog() {
-  static char tpl_name[] = TP_LOG_NAME;
-  static logstream log(mktemp(tpl_name),TP_LOG_DEBUG, TP_LOG_DEBUG);
+  static logstream log(tempnam(TP_LOG_DIR, TP_LOG_PFX), 
+		       TP_LOG_DEBUG, TP_LOG_DEBUG);
   return log;
 }
