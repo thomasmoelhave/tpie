ce8248824a7e4e79a9cc597c7972456ca04bfa3c
Octavian Procopiuc <tave>
2002-06-22 18:10:22 +0000

    Cosmetic changes.

--- tpie/apps/list_rank/app_config.h
+++ tpie/apps/list_rank/app_config.h
@@ -3,7 +3,7 @@
 // Author: Darren Erik Vengroff <darrenv@eecs.umich.edu>
 // Created: 10/6/94
 //
-// $Id: app_config.h,v 1.23 2002-01-25 23:26:50 tavi Exp $
+// $Id: app_config.h,v 1.24 2002-06-22 18:10:22 tavi Exp $
 //
 #ifndef _APP_CONFIG_H
 #define _APP_CONFIG_H
@@ -12,18 +12,37 @@
 #include <config.h>
 
 
-//// ****Developer use**** ////
+// <><><><><><><><><><><><><><><><><><><><><><> //
+// <><><><><><><> Developer use  <><><><><><><> //
+// <><><><><><><><><><><><><><><><><><><><><><> //
+
+// Set up some defaults for the test applications
+
+#include <sys/types.h> // for size_t
+#include <stdlib.h> // for random()
+
+#define DEFAULT_TEST_SIZE (20000000)
+#define DEFAULT_TEST_MM_SIZE (1024 * 1024 * 32)
+
+extern bool verbose;
+extern size_t test_mm_size;
+extern size_t test_size;
+extern int random_seed;
 
 
-
-//// ****Choose default BTE collection implementation**** ////
+// <><><><><><><><><><><><><><><><><><><><><><> //
+// <><><> Choose default BTE COLLECTION  <><><> //
+// <><><><><><><><><><><><><><><><><><><><><><> //
 
 // Define only one (default is BTE_COLLECTION_IMP_MMAP)
-#define BTE_COLLECTION_IMP_MMAP
-//#define BTE_COLLECTION_IMP_UFS
+//#define BTE_COLLECTION_IMP_MMAP
+#define BTE_COLLECTION_IMP_UFS
 //#define BTE_COLLECTION_IMP_USER_DEFINED
 
-//// ****Choose BTE stream implementation**** ////
+
+// <><><><><><><><><><><><><><><><><><><><><><> //
+// <><><><><><> Choose BTE STREAM  <><><><><><> //
+// <><><><><><><><><><><><><><><><><><><><><><> //
 
 // Define only one (default is BTE_STREAM_IMP_UFS)
 #define BTE_STREAM_IMP_UFS
@@ -32,7 +51,9 @@
 //#define BTE_STREAM_IMP_USER_DEFINED
 
 
-//// ****BTE_STREAM_MMAP configuration options**** ////
+// <><><><><><><><><><><><><><><><><><><><><><><><> //
+// <><> BTE_STREAM_MMAP configuration options  <><> //
+// <><><><><><><><><><><><><><><><><><><><><><><><> //
 
 #ifdef BTE_STREAM_IMP_MMAP
    // Define logical blocksize factor (default is 32)
@@ -47,7 +68,10 @@
 #endif
 
 
-//// ****BTE_UFS configuration options**** ////
+// <><><><><><><><><><><><><><><><><><><><><><><><> //
+// <><> BTE_STREAM_UFS configuration options <><><> //
+// <><><><><><><><><><><><><><><><><><><><><><><><> //
+
 #ifdef BTE_STREAM_IMP_UFS
    // Define logical blocksize factor (default is 32)
 #  define BTE_STREAM_UFS_BLOCK_FACTOR 32
@@ -59,21 +83,6 @@
    0) */
 #  define USE_LIBAIO 0
 #endif
-
-
-
-//// ****Set up some defaults for the apps**** ////
-
-#include <sys/types.h> // for size_t
-#include <stdlib.h> // for random
-
-#define DEFAULT_TEST_SIZE (20000000)
-#define DEFAULT_TEST_MM_SIZE (1024 * 1024 * 32)
-
-extern bool verbose;
-extern size_t test_mm_size;
-extern size_t test_size;
-extern int random_seed;
 
 
 
