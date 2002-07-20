49ba7caad9ac29a804fdf15804b07ccccf6448f7
Octavian Procopiuc <tave>
2002-07-20 21:28:44 +0000

    Using tpie_tempnam instead of tempnam, to avoid warnings. Changed prefix of log file name from "TPLOG" to "tpielog".
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@834 669acd26-ddd2-4139-9e68-486997aa639d

--- lib/src/tpie_log.cpp
+++ lib/src/tpie_log.cpp
@@ -1,25 +1,26 @@
 //
 // File: tpie_log.cpp
-// Author: Darren Erik Vengroff <dev@cs.duke.edu>
+// Authors: Darren Erik Vengroff <dev@cs.duke.edu>
+//          Octavian Procopiuc <tavi@cs.duke.edu>
 // Created: 5/12/94
 //
 
 #include <versions.h>
-VERSION(tpie_log_cpp,"$Id: tpie_log.cpp,v 1.9 2002-02-04 06:20:20 tavi Exp $");
+VERSION(tpie_log_cpp,"$Id: tpie_log.cpp,v 1.10 2002-07-20 21:28:44 tavi Exp $");
 
 // We are logging
 #define TPL_LOGGING	1
 
-// For tempnam().
-#include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <tpie_tempnam.h>
 #include <tpie_log.h>
 
-#define TP_LOG_DIR "/tmp"
-#define TP_LOG_PFX "TPLOG"
+#define TPLOGDIR "/tmp"
+#define TPLOGPFX "tpielog"
 
 logstream &theLog() {
-  static logstream log(tempnam(TP_LOG_DIR, TP_LOG_PFX), 
-		       TP_LOG_DEBUG, TP_LOG_DEBUG);
+  static logstream log((srandom(time(NULL)), tpie_tempnam(TPLOGPFX, TPLOGDIR)),
+                       TP_LOG_DEBUG, TP_LOG_DEBUG);
   return log;
 }
