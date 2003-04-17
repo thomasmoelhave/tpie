a2c02cccaff6de3601d558e74adb2cb1daa54276
Jan Vahrenhold <jan.vahrenhold@cs.tu-dortmund.de>
2003-04-17 19:43:16 +0000

    Added portability code and changed typedefs and/or function names as necessary.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@907 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/mm.h
+++ tpie/include/mm.h
@@ -4,10 +4,13 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/30/94
 //
-// $Id: mm.h,v 1.2 2002-01-14 16:29:10 tavi Exp $
+// $Id: mm.h,v 1.3 2003-04-17 19:38:28 jan Exp $
 //
 #ifndef _MM_H
 #define _MM_H
+
+// Get definitions for working with Unix and Windows
+#include <portability.h>
 
 // Get the base class, enums, etc...
 #include <mm_base.h>
