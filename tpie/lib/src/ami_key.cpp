51cb58a170fe721411deb4f132ef2233069e9877
Jan Vahrenhold <jan.vahrenhold@cs.tu-dortmund.de>
2003-04-17 20:42:24 +0000

    Added portability code and changed typedefs and/or function names as necessary.
    Added "this->" in front of "min" and "max" to avoid conflicts
    with STL methods.
    
    
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@918 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/lib/src/ami_key.cpp
+++ tpie/lib/src/ami_key.cpp
@@ -8,16 +8,16 @@
 #include <versions.h>
 #include <ami_key.h>
 
-VERSION(ami_key_cpp,"$Id: ami_key.cpp,v 1.2 2000-01-10 21:38:58 hutchins Exp $");
+VERSION(ami_key_cpp,"$Id: ami_key.cpp,v 1.3 2003-04-17 20:42:24 jan Exp $");
 
 
-key_range::key_range(kb_key min_key, kb_key max_key) :
-        min(min_key), max(max_key)
-{
+key_range::key_range(kb_key min_key, kb_key max_key) {
+    this->min = min_key;
+    this->max = max_key;
 }
 
-key_range::key_range(void) :
-        min(0), max(0)
-{
+key_range::key_range(void) {
+    this->min = 0;
+    this->max = 0;
 }
 
