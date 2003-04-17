5cc541d72face7c627cff31e379e9919a6a314ef
Jan Vahrenhold <jan.vahrenhold@cs.tu-dortmund.de>
2003-04-17 20:45:15 +0000

    Added portability code and changed typedefs and/or function names as necessary.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@919 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/lib/src/ami_matrix_blocks.cpp
+++ tpie/lib/src/ami_matrix_blocks.cpp
@@ -5,7 +5,7 @@
 //
 
 #include <versions.h>
-VERSION(ami_matrix_blocks_cpp,"$Id: ami_matrix_blocks.cpp,v 1.4 2002-01-14 17:25:35 tavi Exp $");
+VERSION(ami_matrix_blocks_cpp,"$Id: ami_matrix_blocks.cpp,v 1.5 2003-04-17 20:45:15 jan Exp $");
 #include "lib_config.h"
 
 #include <sys/types.h>
@@ -27,41 +27,41 @@ perm_matrix_into_blocks::~perm_matrix_into_blocks()
 {
 }
 
-AMI_err perm_matrix_into_blocks::initialize(off_t len)
+AMI_err perm_matrix_into_blocks::initialize(TPIE_OS_OFFSET len)
 {
-    return ((off_t) (r * c) == len) ? AMI_ERROR_NO_ERROR : AMI_MATRIX_BOUNDS;
+    return ((TPIE_OS_OFFSET) (r * c) == len) ? AMI_ERROR_NO_ERROR : AMI_MATRIX_BOUNDS;
 }
 
-off_t perm_matrix_into_blocks::destination(off_t source)
+TPIE_OS_OFFSET perm_matrix_into_blocks::destination(TPIE_OS_OFFSET source)
 {
     tp_assert(r % be == 0, "Rows not a multiple of block extent.");
     tp_assert(c % be == 0, "Cols not a multiple of block extent.");
 
     // What row and column are the source in?
 
-    off_t src_row = source / c;
-    off_t src_col = source % c;
+    TPIE_OS_OFFSET src_row = source / c;
+    TPIE_OS_OFFSET src_col = source % c;
 
     // How many rows of blocks come before the one the source is in?
 
-    off_t src_brow = src_row / be;
+    TPIE_OS_OFFSET src_brow = src_row / be;
 
     // How many blocks in the row of blocks that the source is in come
     // before the block the source is in?
 
-    off_t src_bcol = src_col / be;
+    TPIE_OS_OFFSET src_bcol = src_col / be;
 
     // Number of objects in block rows above.
 
-    off_t obj_b_above = src_brow * be * c;
+    TPIE_OS_OFFSET obj_b_above = src_brow * be * c;
 
     // Number of objects in blocks in the same block row before it.
 
-    off_t obj_b_left = src_bcol * be * be;
+    TPIE_OS_OFFSET obj_b_left = src_bcol * be * be;
     
     // Position in block
 
-    off_t bpos = (src_row - be * src_brow) * be +
+    TPIE_OS_OFFSET bpos = (src_row - be * src_brow) * be +
         (src_col - be * src_bcol);
     
     return obj_b_above + obj_b_left + bpos;
@@ -81,51 +81,51 @@ perm_matrix_outof_blocks::~perm_matrix_outof_blocks()
 {
 }
 
-AMI_err perm_matrix_outof_blocks::initialize(off_t len)
+AMI_err perm_matrix_outof_blocks::initialize(TPIE_OS_OFFSET len)
 {
-    return ((off_t) (r * c) == len) ? AMI_ERROR_NO_ERROR : AMI_MATRIX_BOUNDS;
+    return ((TPIE_OS_OFFSET) (r * c) == len) ? AMI_ERROR_NO_ERROR : AMI_MATRIX_BOUNDS;
 }
 
-off_t perm_matrix_outof_blocks::destination(off_t source)
+TPIE_OS_OFFSET perm_matrix_outof_blocks::destination(TPIE_OS_OFFSET source)
 {
     tp_assert(r % be == 0, "Rows not a multiple of block extent.");
     tp_assert(c % be == 0, "Cols not a multiple of block extent.");
 
     // How many full blocks come before source?
 
-    off_t src_blocks_before = source / (be * be);
+    TPIE_OS_OFFSET src_blocks_before = source / (be * be);
 
     // How many rows of blocks are above the block source is in?
 
-    off_t src_brow = src_blocks_before / (c / be);
+    TPIE_OS_OFFSET src_brow = src_blocks_before / (c / be);
 
     // How many blocks in the current row are before the block src is in?
 
-    off_t src_bleft = src_blocks_before % (c / be); 
+    TPIE_OS_OFFSET src_bleft = src_blocks_before % (c / be); 
     
     // What is the position of source in its block?
 
-    off_t src_pos_in_block = source % (be * be);
+    TPIE_OS_OFFSET src_pos_in_block = source % (be * be);
 
     // What is the row of the source in its block?
 
-    off_t src_row_in_block = src_pos_in_block / be;
+    TPIE_OS_OFFSET src_row_in_block = src_pos_in_block / be;
 
     // What is the col of the source in its block?
 
-    off_t src_col_in_block = src_pos_in_block % be;
+    TPIE_OS_OFFSET src_col_in_block = src_pos_in_block % be;
 
     // Number of items in block rows above src.
 
-    off_t items_brow_above = src_brow * c * be;
+    TPIE_OS_OFFSET items_brow_above = src_brow * c * be;
 
     // Number of items in the current block row above src.
 
-    off_t items_curr_brow_above = src_row_in_block * c;
+    TPIE_OS_OFFSET items_curr_brow_above = src_row_in_block * c;
 
     // Number of items in item row to left of source.
 
-    off_t items_left_in_row = (src_bleft * be) + src_col_in_block;
+    TPIE_OS_OFFSET items_left_in_row = (src_bleft * be) + src_col_in_block;
 
     // Add up everything before it.
     
