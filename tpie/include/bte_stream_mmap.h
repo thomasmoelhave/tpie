893e3dd2201db8d50a47a0bc9a6f4aeb36f17256
Octavian Procopiuc <tave>
2003-04-21 04:01:44 +0000

    Added include <iostream> and prepended std:: to cout and cerr (to avoid errors in gcc 3.2).

--- tpie/include/bte_stream_mmap.h
+++ tpie/include/bte_stream_mmap.h
@@ -3,7 +3,7 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 5/13/94
 //
-// $Id: bte_stream_mmap.h,v 1.8 2003-04-20 23:55:11 tavi Exp $
+// $Id: bte_stream_mmap.h,v 1.9 2003-04-21 04:01:44 tavi Exp $
 //
 // Memory mapped streams.  This particular implementation explicitly manages
 // blocks, and only ever maps in one block at a time.
@@ -17,6 +17,10 @@
 
 #ifndef _BTE_STREAM_MMAP_H
 #define _BTE_STREAM_MMAP_H
+
+#ifdef VERBOSE
+#  include <iostream>
+#endif
 
 // Get definitions for working with Unix and Windows
 #include <portability.h>
@@ -399,7 +403,7 @@ BTE_stream_mmap < T >::BTE_stream_mmap (const char *dev_path,
 
 #ifdef VERBOSE
 	    if (verbose)
-		cout << "CONS created file: " << path << endl;
+		std::cout << "CONS created file: " << path << "\n";
 #endif
 
 	    // what does this do??? Rajiv
@@ -454,7 +458,7 @@ BTE_stream_mmap < T >::BTE_stream_mmap (const char *dev_path,
 #ifdef VERBOSE
     // Rajiv
     if (verbose)
-	cout << "CONS logical eof=" << header->item_logical_eof << "\n";
+	std::cout << "CONS logical eof=" << header->item_logical_eof << "\n";
 #endif
 
     // By default, all streams are deleted at destruction time.
@@ -473,13 +477,13 @@ BTE_stream_mmap < T >::BTE_stream_mmap (const char *dev_path,
     if (verbose) {
 	switch (st) {
 	case BTE_READ_STREAM:
-	    cout << "CONS read stream\n";
+	    std::cout << "CONS read stream\n";
 	    break;
 	case BTE_WRITE_STREAM:
-	    cout << "CONS read/write stream\n";
+	    std::cout << "CONS read/write stream\n";
 	    break;
 	default:
-	    cout << "CONS someother stream\n";
+	    std::cout << "CONS someother stream\n";
 	    break;
 	}
 	print ("CONS ");
@@ -613,7 +617,7 @@ template < class T > BTE_stream_mmap < T >::~BTE_stream_mmap (void) {
 #ifdef VERBOSE
     // Rajiv
     if (verbose) {
-	cout << "DELE logical eof=" << header->item_logical_eof << "\n";
+	std::cout << "DELE logical eof=" << header->item_logical_eof << "\n";
     }
 #endif
     assert (substream_level ||
@@ -683,9 +687,9 @@ template < class T > BTE_stream_mmap < T >::~BTE_stream_mmap (void) {
 #ifdef VERBOSE
     if (verbose) {
 	if (per == PERSIST_DELETE)
-	    cout << "DELE unlinked file\n";
+	    std::cout << "DELE unlinked file\n";
 	if (substream_level) {
-	    cout << "DELE substream destructor\n";
+	    std::cout << "DELE substream destructor\n";
 	}
 	print ("DELE ");
     }
@@ -700,7 +704,7 @@ template < class T > void BTE_stream_mmap < T >::print (char *pref) {
     fprintf (stdout, "%sPFSTATS %d %d %d %d (fd=%d)\n",
 	     pref, stats_hits, stats_misses,
 	     stats_compulsory - stats_misses, stats_eos, fd);
-    cout << pref << stats << " RWMUSCD\n";
+    std::cout << pref << stats << " RWMUSCD\n";
 #endif
 #endif
     fprintf (stdout, "%sfile=%s", pref, path);
@@ -721,7 +725,7 @@ B_INLINE BTE_err BTE_stream_mmap < T >::read_item (T ** elt) {
     if (w_only) {
 #ifdef VERBOSE
 	if (verbose)
-	    cerr << "ERROR read on a write-only stream\n";
+	    std::cerr << "ERROR read on a write-only stream\n";
 #endif
 	return BTE_ERROR_WRITE_ONLY;
     }
@@ -1156,7 +1160,7 @@ inline BTE_err BTE_stream_mmap < T >::validate_current (void)
 #ifdef VERBOSE
     // Rajiv
     if (verbose && bte_err != BTE_ERROR_NO_ERROR)
-	cerr << "validate_current failed\n";
+	std::cerr << "validate_current failed\n";
 #endif
 
     // Rajiv
@@ -1373,7 +1377,7 @@ BTE_stream_mmap < T >::grow_file (TPIE_OS_OFFSET block_offset)
 	LOG_FATAL ('\n');
 	LOG_FLUSH_LOG;
 	// Rajiv
-	//cerr << "map_current: ftruncate\n";
+	//std::cerr << "map_current: ftruncate\n";
 	return BTE_ERROR_END_OF_STREAM;	// generate an error Rajiv
     }
      assert (TPIE_OS_LSEEK(fd, 0, TPIE_OS_FLAG_SEEK_END) == f_filelen);
@@ -1464,7 +1468,7 @@ template < class T > void BTE_stream_mmap < T >::read_ahead (void)
 	    (f_curr_block + 2 * header->block_size > f_filelen)) {
 #ifdef VERBOSE
 	    if (verbose)
-		cout << "growing file (fd" << fd << ") in advance\n";
+		std::cout << "growing file (fd" << fd << ") in advance\n";
 #endif
 	    grow_file (f_curr_block);
 	}
