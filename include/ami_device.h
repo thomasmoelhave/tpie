41a62de3ccec4e2ba311c4e754514b795740130e
Jan Vahrenhold <jan.vahrenhold@cs.tu-dortmund.de>
2003-09-12 01:44:29 +0000

    Removed std:: descriptor from std::ostream
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@1049 669acd26-ddd2-4139-9e68-486997aa639d

--- include/ami_device.h
+++ include/ami_device.h
@@ -4,7 +4,7 @@
 // Author: Darren Erik Vengroff <darrenv@eecs.umich.edu>
 // Created: 8/22/93
 //
-// $Id: ami_device.h,v 1.3 2003-04-20 06:38:39 tavi Exp $
+// $Id: ami_device.h,v 1.4 2003-09-12 01:44:29 jan Exp $
 //
 #ifndef _AMI_DEVICE_H
 #define _AMI_DEVICE_H
@@ -15,7 +15,7 @@
 #include <iostream>
 
 class AMI_device {
-    friend std::ostream &operator<<(std::ostream &os, const AMI_device &dev);
+    friend ostream &operator<<(ostream &os, const AMI_device &dev);
 private:
     void dispose_contents(void);
 protected:
@@ -32,7 +32,7 @@ public:
 };
 
 // Output operator
-std::ostream &operator<<(std::ostream &os, const AMI_device &dev);
+ostream &operator<<(ostream &os, const AMI_device &dev);
 
 #endif // _AMI_DEVICE_H 
 
