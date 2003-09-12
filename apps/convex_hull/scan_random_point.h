e337056a6fba85a3961284f9d6273593d0cb0f4e
Octavian Procopiuc <tave>
2003-09-12 14:59:06 +0000

    Removed random() and srandom() declarations.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@1069 669acd26-ddd2-4139-9e68-486997aa639d

--- apps/convex_hull/scan_random_point.h
+++ apps/convex_hull/scan_random_point.h
@@ -4,17 +4,12 @@
 // Author: Darren Vengroff <darrenv@eecs.umich.edu>
 // Created: 12/19/94
 //
-// $Id: scan_random_point.h,v 1.4 1999-02-03 22:15:44 tavi Exp $
+// $Id: scan_random_point.h,v 1.5 2003-09-12 14:58:23 tavi Exp $
 //
 #ifndef _SCAN_RANDOM_POINT_H
 #define _SCAN_RANDOM_POINT_H
 
 #include "point.h"
-
-#if 0
-extern "C" int srandom(int);
-extern "C" int random(void);
-#endif
 
 // A scan object to generate random integers.
 class scan_random_point : AMI_scan_object {
