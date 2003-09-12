8b46077254c4ea9df9020fb993d75f2ea8c7d6d4
Octavian Procopiuc <tave>
2003-09-12 15:38:38 +0000

    Added a warning fooler for win32.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@1074 669acd26-ddd2-4139-9e68-486997aa639d

--- include/ami_block_base.h
+++ include/ami_block_base.h
@@ -3,7 +3,7 @@
 // File:    ami_block_base.h
 // Author:  Octavian Procopiuc <tavi@cs.duke.edu>
 //
-// $Id: ami_block_base.h,v 1.11 2003-09-12 15:35:43 tavi Exp $
+// $Id: ami_block_base.h,v 1.12 2003-09-12 15:38:38 tavi Exp $
 //
 // Definition of the AMI_block_base class and supporting types:
 // AMI_bid, AMI_block_status.
@@ -124,11 +124,11 @@ public:
       if (per_ == PERSIST_PERSISTENT) {
 	// Write back the block.
 	if (pcoll_->put_block(bid_, pdata_) == BTE_ERROR_NO_ERROR)
-	  ; //do nothing
+	  int warning_fooler = 0; //do nothing
       } else {
 	// Delete the block from the collection.
 	if (pcoll_->delete_block(bid_, pdata_) == BTE_ERROR_NO_ERROR)
-	  ; //do nothing 
+	  int warning_fooler = 0; //do nothing 
       }
   }
 };
