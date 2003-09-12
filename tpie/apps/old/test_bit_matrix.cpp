9231bc3c932cdfc758b677cadf37cc0a07b55e5e
Octavian Procopiuc <tave>
2003-09-12 01:34:41 +0000

    Updates for portability:  - added portability.h  - removed using declarations  - removed directory names from data files  - changed \n to endl
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@1045 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/apps/old/test_bit_matrix.cpp
+++ tpie/apps/old/test_bit_matrix.cpp
@@ -5,36 +5,36 @@
 // Created: 11/4/94
 //
 
-#include <versions.h>
-VERSION(test_bit_matrix_cpp,"$Id: test_bit_matrix.cpp,v 1.6 2003-04-20 23:51:40 tavi Exp $");
+#include <portability.h>
 
-#include <iostream>
+#include <versions.h>
+VERSION(test_bit_matrix_cpp,"$Id: test_bit_matrix.cpp,v 1.7 2003-09-12 01:34:41 tavi Exp $");
+
 #include <bit_matrix.h>
 
-using std::cout;
 
 int main(int argc, char **argv)
 {
     bit_matrix bm(5,5);
-    cout << bm << '\n';
+    cout << bm << endl;
 
     bm[3][3] = 3;
     bm[2][2] = 2;
     bm[0][0] = bm[1][1] = bm[3][3];
-    cout << bm << '\n';
+    cout << bm << endl;
 
     bit_matrix bv(16, 1), bbv(16, 1);
-    cout << (bv = (off_t)(0xF0F0)) << '\n';
+    cout << (bv = (off_t)(0xF0F0)) << endl;
     off_t ot = 1234;
     bv = ot;
-    cout << bv << '\n';
+    cout << bv << endl;
 
     ot = 4321;
     ot = bv;
-    cout << ot << "\n\n";
+    cout << ot << endl << endl;
 
     bbv = bv + bv;
-    cout << bbv << '\n';
+    cout << bbv << endl;
     
     return 0;
 }
