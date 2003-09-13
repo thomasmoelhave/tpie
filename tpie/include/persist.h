6211735b916851bf0ea33e2cf408b33855eea0e3
Jan Vahrenhold <jan.vahrenhold@cs.tu-dortmund.de>
2003-09-13 17:42:27 +0000

    Added a fix for OSs that do not support READ_ONCE access.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@1085 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/persist.h
+++ tpie/include/persist.h
@@ -4,7 +4,7 @@
 // Author: Darren Erik Vengroff <darrenv@eecs.umich.edu>
 // Created: 4/7/95
 //
-// $Id: persist.h,v 1.2 2003-04-17 19:43:16 jan Exp $
+// $Id: persist.h,v 1.3 2003-09-13 17:42:27 jan Exp $
 //
 // Persistence flags for TPIE streams.
 //
@@ -18,9 +18,11 @@ enum persistence {
     // Delete the stream from the disk when it is destructed.
     PERSIST_DELETE = 0,
     // Do not delete the stream from the disk when it is destructed.
-    PERSIST_PERSISTENT,
+    PERSIST_PERSISTENT = 1,
     // Delete each block of data from the disk as it is read.
-    PERSIST_READ_ONCE
+    // If not supported by the OS (see portability.h), delete
+    // the stream when it is destructed (see PERSIST_DELETE).
+    PERSIST_READ_ONCE = TPIE_OS_PERSIST_READ_ONCE
 };
 
 #endif // _PERSIST_H 
