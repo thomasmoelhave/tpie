776b3a7bb8b263114010d076cbb473ad324dc5f7
Jan Vahrenhold <jan.vahrenhold@cs.tu-dortmund.de>
2004-02-05 17:49:10 +0000

    no message
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@1176 669acd26-ddd2-4139-9e68-486997aa639d

--- test/app_config.cpp
+++ test/app_config.cpp
@@ -5,11 +5,16 @@
 // Created: 10/7/94
 //
 
-static char app_config_id[] = "$Id: app_config.cpp,v 1.5 2003-11-18 17:56:52 tavi Exp $";
+static char app_config_id[] = "$Id: app_config.cpp,v 1.6 2004-02-05 17:49:10 jan Exp $";
 
 #include "app_config.h"
 
+#ifdef NDEBUG
 bool verbose = false;
+#else
+bool verbose = true;
+#endif
+
 size_t test_mm_size = DEFAULT_TEST_MM_SIZE;
 size_t test_size = DEFAULT_TEST_SIZE;
 int random_seed = 17;
