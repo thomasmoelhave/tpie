c12cb2685f84cb0c5afcf91069e94e6efcb36905
Octavian Procopiuc <tave>
2005-02-15 00:20:03 +0000

    Removed getopt.h dependency
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@1256 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/test/parse_args.h
+++ tpie/test/parse_args.h
@@ -3,23 +3,20 @@
 // Author: Darren Erik Vengroff <darrenv@eecs.umich.edu>
 // Created: 10/7/94
 //
-// $Id: parse_args.h,v 1.4 2003-11-18 18:09:06 tavi Exp $
+// $Id: parse_args.h,v 1.5 2005-02-15 00:20:03 tavi Exp $
 //
 #ifndef _PARSE_ARGS_H
 #define _PARSE_ARGS_H
 
 #include "app_config.h"
-#include "getopt.h"
 #include "getopts.h"
 
-void usage(void (*usage_app)(void) = NULL);
 
 // Parse arguments for flags common to all test apps, as well as those
 // specific to this particular app.  aso points to a string of app
 // specific options, and parse_app is a pointer to a function, tpyically
 // just a big switch, to handle them.
-void parse_args(int argc, char **argv, const char *aso = NULL,
-                void (*parse_app_opt)(char opt, char *optarg) = NULL);
 void parse_args(int argc, char **argv, struct options *application_opts,
-		void (*parse_app_opts)(int idx, char *opt_arg));              
+		void (*parse_app_opts)(int idx, char *opt_arg), 
+		bool stop_if_no_args = true);              
 #endif // _PARSE_ARGS_H 
