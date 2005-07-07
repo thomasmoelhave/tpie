f5f12c38407dd0ff6a2af28e334a4b60a0ee5fae
Andrew Danner <adanner@cs.swarthmore.edu>
2005-07-07 20:39:22 +0000

    was going to get rid of it but instead added some helper classes that convert between different comparison types (TPIE, STL, <).
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk@1269 669acd26-ddd2-4139-9e68-486997aa639d

--- tpie/include/comparator.h
+++ tpie/include/comparator.h
@@ -1,10 +1,12 @@
-// Copyright (c) 1995 Darren Vengroff
+// Copyright (c) 2005 Andrew Danner
 //
 // File: comparator.h
-// Author: Darren Vengroff <darrenv@eecs.umich.edu>
-// Created: 3/6/95
+// Author: Andrew Danner
+// Created: 28 Jun 2005
 //
-// $Id: comparator.h,v 1.3 2003-04-17 18:48:39 jan Exp $
+// Mappings/Wrappers for converting between different comparison types
+//
+// $Id: comparator.h,v 1.4 2005-07-07 20:39:22 adanner Exp $
 //
 #ifndef _COMPARATOR_H
 #define _COMPARATOR_H
@@ -12,14 +14,60 @@
 // Get definitions for working with Unix and Windows
 #include <portability.h>
 
-// First we define a comparison object.
-template<class T>
-class comparator
-{
-public:
-#if AMI_VIRTUAL_BASE
-  virtual int compare(const T &t1, const T &t2) = 0;
-#endif
+
+// In is unlikely that users will need to directly need these classes
+// except to maybe upgrade old code with minimal changes. In the future it
+// may be better to make TPIE's compare() method match the syntax of the
+// STL operator ()
+
+//Convert STL comparison object with operator() to a TPIE comparison
+//object with a compare() function. 
+template<class T, class STLCMP>
+class STL2TPIE_cmp{
+  private:
+    STLCMP *isLess; //Class with STL comparison operator()
+
+  public:
+    STL2TPIE_cmp(STLCMP* _cmp) {isLess=_cmp; }
+    //Do not use with applications that test if compare returns +1
+    //Because it never does so.
+    inline int compare(const T& left, const T& right){
+      if( (*isLess)(left, right) ){ return -1; }
+      else { return 0; }
+    }
 };
+  
+ 
+//Convert a TPIE comparison object with a compare() function. 
+//to STL comparison object with operator() 
+template<class T, class TPCMP>
+class TPIE2STL_cmp{
+  private:
+    TPCMP* cmpobj; //Class with TPIE comparison compare()
+
+  public:
+    TPIE2STL_cmp(TPCMP* cmp) {cmpobj=cmp;}
+    inline bool operator()(const T& left, const T& right) const{
+      return (cmpobj->compare(left, right) < 0);
+    }
+};
+
+//Convert a class with a comparison operator <
+//to a TPIE comparison object with a compare() function. 
+template<class T>
+class op2TPIE_cmp{
+  public:
+    op2TPIE_cmp(){};
+    //Do not use with applications that test if compare returns +1
+    //Because it never does so.
+    inline int compare(const T& left, const T& right){
+      if( left < right ){ return -1; }
+      else { return 0; }
+    }
+};
+
+//Convert a class with a comparison operator <
+//to an STL comparison object with a comparison operator ().
+//Not implemented here. It is called less in <functional>, part of STL
 
 #endif // _COMPARATOR_H 
