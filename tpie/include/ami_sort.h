61e1cb60193c63e2d23d6d1423960ac48df17ec5
Andrew Danner <adanner@cs.swarthmore.edu>
2005-11-09 11:39:03 +0000

    Part of DistPIE/CleanPIE initiative and sort cleanup. Removed Darren's sorting version (formerly ami_sort_single.h) and moved Rakesh's version to a temporary "dumpster" directory. Only sort implementation is now a rewrite of ami_sort_single_dh.h, but is handled by the sort_manager class in sort_manager.h. ami_sort.h simply includes one sort implementation and defines the AMI_sort wrappers that call the sort_manager.

--- tpie/include/ami_sort.h
+++ tpie/include/ami_sort.h
@@ -3,7 +3,7 @@
 // Author: Darren Erik Vengroff <dev@cs.duke.edu>
 // Created: 6/10/94
 //
-// $Id: ami_sort.h,v 1.11 2003-09-27 07:00:48 tavi Exp $
+// $Id: ami_sort.h,v 1.12 2005-11-09 11:39:03 adanner Exp $
 //
 #ifndef _AMI_SORT_H
 #define _AMI_SORT_H
@@ -11,14 +11,178 @@
 // Get definitions for working with Unix and Windows
 #include <portability.h>
 
-#define CONST const
+// The class that actually does the sorting
+#include <sort_manager.h>
+#include <mergeheap.h>
+#include <internal_sort.h>
 
-#include <ami_stream.h>
+// *******************************************************************
+// *                                                                 *
+// *           The actual AMI_sort calls                             *
+// *                                                                 *
+// *******************************************************************
 
-#ifdef AMI_STREAM_IMP_SINGLE
-#include <ami_sort_single.h>
-#include <ami_optimized_sort.h>
-#include <ami_sort_single_dh.h>
-#endif
+// ********************************************************************
+// *                                                                  *
+// *  These first two versions use a mergeheap on actual objecet      *
+// *                                                                  *
+// ********************************************************************
+
+// A version of AMI_sort that takes an input stream of elements of type
+// T, and an output stream, and and uses the < operator to sort
+template<class T>
+AMI_err AMI_sort(AMI_STREAM<T> *instream, AMI_STREAM<T> *outstream,
+                 bool progress=false)
+{
+  return sort_manager< T, Internal_Sorter_Op<T>, merge_heap_op<T> >
+    (Internal_Sorter_Op<T>(), merge_heap_op<T>() ).sort
+    (instream, outstream, progress);
+}
+
+// A version of AMI_sort that takes an input stream of elements of
+// type T, an output stream, and a user-specified comparison
+// object. The comparison object "cmp", of (user-defined) class
+// represented by CMPR, must have a member function called "compare"
+// which is used for sorting the input stream.
+template<class T, class CMPR>
+AMI_err AMI_sort(AMI_STREAM<T> *instream, AMI_STREAM<T> *outstream,
+    CMPR *cmp, bool progress=false)
+{
+  return sort_manager<T, Internal_Sorter_Obj<T, CMPR>,
+         merge_heap_obj<T,CMPR> >( Internal_Sorter_Obj<T, CMPR>(cmp),
+             merge_heap_obj<T,CMPR>(cmp) ).sort
+              (instream, outstream, progress);
+}
+
+// ********************************************************************
+// *                                                                  *
+// *  These versions build  a heap on pointers to objects             *
+// *                                                                  *
+// ********************************************************************
+
+// A version of AMI_sort that takes an input stream of elements of type
+// T, and an output stream, and and uses the < operator to sort
+template<class T>
+AMI_err AMI_ptr_sort(AMI_STREAM<T> *instream, AMI_STREAM<T> *outstream,
+                 bool progress=false)
+{
+  return sort_manager< T, Internal_Sorter_Op<T>, merge_heap_ptr_op<T> >
+    (Internal_Sorter_Op<T>(), merge_heap_ptr_op<T>()).sort
+    (instream, outstream, progress);
+}
+
+// A version of AMI_sort that takes an input stream of elements of
+// type T, an output stream, and a user-specified comparison
+// object. The comparison object "cmp", of (user-defined) class
+// represented by CMPR, must have a member function called "compare"
+// which is used for sorting the input stream.
+template<class T, class CMPR>
+AMI_err AMI_ptr_sort(AMI_STREAM<T> *instream, AMI_STREAM<T> *outstream,
+    CMPR *cmp, bool progress=false)
+{
+  return sort_manager<T, Internal_Sorter_Obj<T, CMPR>,
+         merge_heap_ptr_obj<T,CMPR> >( Internal_Sorter_Obj<T, CMPR>(cmp),
+             merge_heap_ptr_obj<T,CMPR>(cmp) ).sort
+             (instream, outstream, progress);
+}
+
+// ********************************************************************
+// *                                                                  *
+// *  This version keeps a heap of keys to records, separating small  *
+// *  keys from large records and reducing data movement in the heap  *
+// *  when objects are very large but the sort key is small           *
+// *                                                                  *
+// ********************************************************************
+// A version of AMI_sort that takes an input stream of elements of
+// type T, an output stream, a key specification, and a user-specified
+// comparison object.
+
+// The key specification consists of an example key, which is used to
+// infer the type of the key field. The comparison object "cmp", of
+// (user-defined) class represented by CMPR, must have a member
+// function called "compare" which is used for sorting the input
+// stream, and a member function called "copy" which is used for
+// copying the key of type KEY from a record of type T (the type to be
+// sorted).
+template<class T, class KEY, class CMPR>
+AMI_err  AMI_key_sort(AMI_STREAM<T> *instream, AMI_STREAM<T> *outstream,
+    KEY dummykey, CMPR *cmp, bool progress=false)
+{
+  return sort_manager<T, Internal_Sorter_KObj<T, KEY, CMPR>,
+        merge_heap_kobj<T,KEY,CMPR> >( Internal_Sorter_KObj<T,KEY,CMPR>(cmp),             merge_heap_kobj<T,KEY,CMPR>(cmp) ).sort
+          (instream, outstream, progress);
+}
+
+// ********************************************************************
+// *                                                                  *
+// * Duplicates of the above versions that only use 2x space and      *
+// * overwrite the original input stream                              *
+// *                                                                  *
+// ********************************************************************
+
+// object heaps, < operator comparisons
+template<class T>
+AMI_err AMI_sort(AMI_STREAM<T> *instream, bool progress=false)
+{
+  return sort_manager< T, Internal_Sorter_Op<T>, merge_heap_op<T> >
+  (Internal_Sorter_Op<T>(), merge_heap_op<T>() ).sort
+  (instream, progress);
+}
+
+// object heaps, comparison object comparisions
+template<class T, class CMPR>
+AMI_err AMI_sort(AMI_STREAM<T> *instream, CMPR *cmp, bool progress=false)
+{
+  return sort_manager<T, Internal_Sorter_Obj<T, CMPR>,
+         merge_heap_obj<T,CMPR> >( Internal_Sorter_Obj<T, CMPR>(cmp),
+             merge_heap_obj<T,CMPR>(cmp) ).sort
+           (instream, progress);
+}
+
+// ptr heaps, < operator comparisons
+template<class T>
+AMI_err AMI_ptr_sort(AMI_STREAM<T> *instream, bool progress=false)
+{
+  return sort_manager< T, Internal_Sorter_Op<T>, merge_heap_ptr_op<T> >
+    (Internal_Sorter_Op<T>(), merge_heap_ptr_op<T>()).sort
+    (instream, progress);
+}
+
+// ptr heaps, comparison object comparisions
+template<class T, class CMPR>
+AMI_err AMI_ptr_sort(AMI_STREAM<T> *instream, CMPR *cmp, bool progress=false)
+{
+  return sort_manager<T, Internal_Sorter_Obj<T, CMPR>,
+         merge_heap_ptr_obj<T,CMPR> >( Internal_Sorter_Obj<T, CMPR>(cmp),
+             merge_heap_ptr_obj<T,CMPR>(cmp) ).sort
+           (instream, progress);
+}
+
+// key/object heaps, key/object comparisons 
+template<class T, class KEY, class CMPR>
+AMI_err  AMI_key_sort(AMI_STREAM<T> *instream, KEY dummykey, CMPR *cmp,
+    bool progress=false)
+{
+  return sort_manager<T, Internal_Sorter_KObj<T, KEY, CMPR>,
+        merge_heap_kobj<T,KEY,CMPR> >( Internal_Sorter_KObj<T,KEY,CMPR>(cmp),             merge_heap_kobj<T,KEY,CMPR>(cmp) ).sort
+          (instream, progress);
+}
+
+/*
+DEPRECATED: comparison function sorting
+Earlier TPIE versions allowed a sort that used a C-style
+comparison function to sort. However, comparison functions cannot be
+inlined, so each comparison requires one function call. Given that the
+comparison operator < and comparison object classes can be inlined and
+have better performance while providing the exact same functionality,
+comparison functions have been removed from TPIE. If you can provide us
+with a compelling argument on why they should be in here, we may consider
+adding them again, but you must demonstrate that comparision functions
+can outperform other methods in at least some cases or give an example
+were it is impossible to use a comparison operator or comparison object
+
+Sincerely,
+the management
+*/
 
 #endif // _AMI_SORT_H 
