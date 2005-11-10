2a84a87a688704247119ab56aac84d820140e603
Jan Vahrenhold <jan.vahrenhold@cs.tu-dortmund.de>
2005-11-10 21:30:19 +0000

    Made the class a struct. Also renamed the members according to the new naming scheme.
    git-svn-id: http://svn.daimi.au.dk/svn/tpie/trunk/tpie@1318 669acd26-ddd2-4139-9e68-486997aa639d

--- include/bte_stream_header.h
+++ include/bte_stream_header.h
@@ -2,31 +2,37 @@
 #define _BTE_STREAM_HEADER_H
 
 // BTE stream header info.
-class BTE_stream_header {
-public:
-  
+struct BTE_stream_header {
+
     // Unique header identifier. Set to BTE_STREAM_HEADER_MAGIC_NUMBER.
-    unsigned int magic_number;
+    unsigned int m_magicNumber;
+
     // Should be 2 for current version (version 1 has been deprecated).
-    unsigned int version;
+    unsigned int m_version;
+
     // The type of BTE_STREAM that created this header. Not all types of
     // BTE's are readable by all BTE implementations. For example,
     // BTE_STREAM_STDIO streams are not readable by either
     // BTE_STREAM_UFS or BTE_STREAM_MMAP implementations. The value 0 is
     // reserved for the base class. Use numbers bigger than 0 for the
     // various implementations.
-    unsigned int type;
+    unsigned int m_type;
+
     // The number of bytes in this structure.
-    TPIE_OS_SIZE_T header_length;
+    TPIE_OS_SIZE_T m_headerLength;
+
     // The size of each item in the stream.
-    TPIE_OS_SIZE_T item_size;
+    TPIE_OS_SIZE_T m_itemSize;
+
     // The size of a physical block on the device this stream resides.
-    TPIE_OS_SIZE_T os_block_size;
+    TPIE_OS_SIZE_T m_osBlockSize;
+
     // Size in bytes of each logical block, if applicable.
-    TPIE_OS_SIZE_T block_size;
+    TPIE_OS_SIZE_T m_blockSize;
+
     // For all intents and purposes, the length of the stream in number
     // of items.
-    TPIE_OS_OFFSET item_logical_eof;
+    TPIE_OS_OFFSET m_itemLogicalEOF;
 };
 
 #endif // _BTE_STREAM_HEADER_H
