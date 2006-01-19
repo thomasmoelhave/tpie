d48d5a8ca8a092e4482c914612bf6c2102351047
Jan Vahrenhold <jan.vahrenhold@cs.tu-dortmund.de>
2006-01-19 18:25:11 +0000

    Major design change: class AMI_stack<T> no longer inherits from AMI_STREAM<T> but instead aggregates an AMI_STREAM<T>.
    Added a size() method (but still provide stream_len() for
    backwards compatibility).

--- tpie/include/ami_stack.h
+++ tpie/include/ami_stack.h
@@ -4,7 +4,7 @@
 // Author: Darren Vengroff <darrenv@eecs.umich.edu>
 // Created: 12/15/94
 //
-// $Id: ami_stack.h,v 1.9 2005-01-21 16:52:39 tavi Exp $
+// $Id: ami_stack.h,v 1.10 2006-01-19 18:25:11 jan Exp $
 //
 #ifndef _AMI_STACK_H
 #define _AMI_STACK_H
@@ -15,77 +15,102 @@
 #include <ami_stream.h>
 
 template<class T>
-class AMI_stack : public AMI_STREAM<T> {
+class AMI_stack {
   public:
-    using AMI_STREAM<T>::seek;
-    using AMI_STREAM<T>::truncate;
-    using AMI_STREAM<T>::stream_len;
-
     AMI_stack(); 
     AMI_stack(const char* path, 
         AMI_stream_type type = AMI_READ_WRITE_STREAM);
     ~AMI_stack(void);
     AMI_err push(const T &t);
     AMI_err pop(T **t);
+
+    TPIE_OS_OFFSET size() const {
+	return m_size;
+    }
+
+    //  This should go as soon as all old code has been migrated.
+    TPIE_OS_OFFSET stream_len() const {
+	cerr << "Using AMI_stack<T>::stream_len() is deprecated." << endl;
+	return m_size;
+    }
+
+protected:
+    AMI_STREAM<T>* m_amiStream;
+    TPIE_OS_OFFSET m_size;
 };
 
 
 template<class T>
-AMI_stack<T>::AMI_stack() :
-        AMI_STREAM<T>()
-{
+AMI_stack<T>::AMI_stack() : 
+    m_amiStream(NULL), 
+    m_size(0) {
+
+    // No error checking done for the time being.
+    m_amiStream = new AMI_STREAM<T>();
 }
 
 template<class T>
-AMI_stack<T>::AMI_stack(const char* path, AMI_stream_type type):
-        AMI_STREAM<T>(path, type)
-{
+AMI_stack<T>::AMI_stack(const char* path, AMI_stream_type type) :
+    m_amiStream(NULL), 
+    m_size(0) {
+
+    // No error checking done for the time being.
+    m_amiStream = new AMI_STREAM<T>(path, type);
+
+    // Set the size of the stack to be the number of items present 
+    // in the underlying stream file. 
+    m_size = m_amiStream->stream_len();
 }
 
 template<class T>
-AMI_stack<T>::~AMI_stack(void)
-{
+AMI_stack<T>::~AMI_stack() {
+
+    // Make sure there are no left-overs.
+    m_amiStream->truncate(m_size);
+
+    delete m_amiStream;
 }
 
 template<class T>
-AMI_err AMI_stack<T>::push(const T &t)
-{
-    AMI_err ae;
-    TPIE_OS_OFFSET slen;
-    
-    ae = truncate((slen = stream_len())+1);
-    if (ae != AMI_ERROR_NO_ERROR) {
-        return ae;
+AMI_err AMI_stack<T>::push(const T &t) {
+
+    AMI_err ae = m_amiStream->write_item(t);
+
+    if (ae == AMI_ERROR_NO_ERROR) {
+	m_size++;
     }
 
-    ae = seek(slen);
-    if (ae != AMI_ERROR_NO_ERROR) {
-        return ae;
-    }
-
-    return write_item(t);
+    return ae;
 }
 
 
 template<class T>
 AMI_err AMI_stack<T>::pop(T **t)
 {
-    AMI_err ae;
-    TPIE_OS_OFFSET slen;
+    AMI_err ae = AMI_ERROR_END_OF_STREAM;
 
-    slen = stream_len();
+    if (m_size) {
 
-    ae = seek(slen-1);
-    if (ae != AMI_ERROR_NO_ERROR) {
-        return ae;
+	ae = m_amiStream->seek(m_size-1);
+	
+	if (ae != AMI_ERROR_NO_ERROR) {
+	    return ae;
+	}
+	
+	ae = m_amiStream->read_item(t);
+	
+	if (ae != AMI_ERROR_NO_ERROR) {
+	    return ae;
+	}
+	
+	ae = m_amiStream->seek(m_size-1);
+	
+	if (ae == AMI_ERROR_NO_ERROR) {
+	    m_size--;
+	}
     }
 
-    ae = read_item(t);
-    if (ae != AMI_ERROR_NO_ERROR) {
-        return ae;
-    }
-
-    return truncate(slen-1);
+    return ae;
 }
 
 #endif // _AMI_STACK_H 
