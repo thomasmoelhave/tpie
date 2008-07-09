19e567c42344c78f62660087ec8a8f437b9aeb24
Thomas Moelhave <thomas@moelhave.com>
2008-07-09 21:15:42 +0000

    libtpie can now be built using tpie, with the following limitations: 1) config.h cannot be generated (thus, I commit a version that should work on modern linux'es. 2) ami_scan can not be expanded (this was not the default anyways) 3) the rest of the tpie distibution (apps and tests) cannot be built at this time.

--- tpie/include/config.h
+++ tpie/include/config.h
@@ -1,40 +1,41 @@
+/* include/config.h.  Generated from config.h.in by configure.  */
 // Copyright (c) 1994 Darren Erik Vengroff
 //
 // File: config.h.in
 // Author: Darren Erik Vengroff <darrenv@eecs.umich.edu>
 // Created: 10/13/94
 //
-// $Id: config.h,v 1.1 2004-02-05 17:30:37 jan Exp $
+// $Id: config.h.in,v 1.8 2003-04-17 18:51:20 jan Exp $
 //
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
 // Define if you have getrusage()
-#define HAVE_GETRUSAGE 0
+#define HAVE_GETRUSAGE 1
 
 // Define if you have mmap()
-#define HAVE_MMAP 0
+#define HAVE_MMAP 1
 
 // Define if asyncronous I/O is avaialable.
 #define HAVE_LIBAIO 0
 
 // Define if you have a prototype for mmap()
-#define HAVE_PROTOTYPE_MMAP 0
+#define HAVE_PROTOTYPE_MMAP 1
 
 // Define if you have a prototype for munmap()
-#define HAVE_PROTOTYPE_MUNMAP 0
+#define HAVE_PROTOTYPE_MUNMAP 1
 
 // Define if you have a prototype for ftruncate()
-#define HAVE_PROTOTYPE_FTRUNCATE 0
+#define HAVE_PROTOTYPE_FTRUNCATE 1
 
 // Do we have string.h or strings.h?
-#define HAVE_STRING_H 0
-#define HAVE_STRINGS_H 0
+#define HAVE_STRING_H 1
+#define HAVE_STRINGS_H 1
 
 // Where is unistd.h?
 #ifdef _WIN32
-#define HAVE_UNISTD_H 0
-#define HAVE_SYS_UNISTD_H 0
+#define HAVE_UNISTD_H 1
+#define HAVE_SYS_UNISTD_H 1
 #else
 #define HAVE_UNISTD_H 1
 #define HAVE_SYS_UNISTD_H 1
