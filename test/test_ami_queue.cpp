8840242d058456d7d5bd581c40c715f339b92ac2
Jeffrey Vitter <jv>
2008-10-16 19:41:05 +0000

    Made "queue test" work with namespaces.

--- test/test_ami_queue.cpp
+++ test/test_ami_queue.cpp
@@ -17,7 +17,7 @@
 using namespace tpie;
 
 struct options app_opts[] = {
-  { 0, NULL, NULL, NULL, 0 }
+    { 0, NULL, NULL, NULL, 0 }
 };
 
 void parse_app_opts(int idx, char *opt_arg) {
@@ -30,36 +30,37 @@ int main(int argc, char **argv)
     parse_args(argc, argv, app_opts, parse_app_opts);
 
     if (verbose) {
-      std::cout << "test_size = " << test_size << "." << std::endl;
-      std::cout << "test_mm_size = " << static_cast<TPIE_OS_OUTPUT_SIZE_T>(test_mm_size) << "." << std::endl;
-      std::cout << "random_seed = " << random_seed << "." << std::endl;
+	std::cout << "test_size = " << test_size << "." << std::endl;
+	std::cout << "test_mm_size = " << static_cast<TPIE_OS_OUTPUT_SIZE_T>(test_mm_size) << "." << std::endl;
+	std::cout << "random_seed = " << random_seed << "." << std::endl;
     } else {
-      std::cout << test_size << ' ' << static_cast<TPIE_OS_OUTPUT_SIZE_T>(test_mm_size) << ' ' << random_seed;
+	std::cout << test_size << ' ' << static_cast<TPIE_OS_OUTPUT_SIZE_T>(test_mm_size) << ' ' << random_seed;
     }
     
     // Set the amount of main memory:
     MM_manager.set_memory_limit(test_mm_size);
 
-    AMI_queue<TPIE_OS_OFFSET> queue;
-
+    ami::queue<TPIE_OS_OFFSET> queue;
+    
     pi->set_percentage_range(0,test_size);
     pi->set_description("Enqueue");
-
+    
     // Push values.
     TPIE_OS_OFFSET ii;
     for (ii = 0; ii < test_size; ii++) {
-      pi->step_percentage();
-      queue.enqueue(ii);
+	pi->step_percentage();
+	queue.enqueue(ii);
     }
     pi->done("Done");
-
+    
     if (verbose) {
-      std::cout << "Queue size = " << queue.size() << std::endl;
+	std::cout << "Queue size = " << queue.size() << std::endl;
     }
     
     TPIE_OS_OFFSET *jj;
     TPIE_OS_OFFSET last;
     TPIE_OS_OFFSET read = 0;
+
     queue.dequeue(&jj);
     last = *jj;
     
@@ -67,24 +68,28 @@ int main(int argc, char **argv)
     pi->reset();
     read++;
     pi->step_percentage();
+
     while(!queue.is_empty()) {
-      AMI_err ae = queue.dequeue(&jj);
-      if(ae != AMI_ERROR_NO_ERROR) {
-        std::cout << "Error from queue received" << std::endl;
-      }
-      read++;
-      pi->step_percentage();
-      if(*jj != ++last) {
-        std::cout << std::endl << "Error in output: " << *jj << "!=" << last  << std::endl;
-      }
+
+	ami::err ae = queue.dequeue(&jj);
+	if(ae != ami::NO_ERROR) {
+	    std::cout << "Error from queue received" << std::endl;
+	}
+	read++;
+	pi->step_percentage();
+	if(*jj != ++last) {
+	    std::cout << std::endl << "Error in output: " << *jj << "!=" << last  << std::endl;
+	}
     }
+
     pi->done("Done");
+
     if(read != test_size) {
-      std::cout << "Error: Wrong amount of elements read, got: " << read << " expected: "<<test_size << std::endl;
+	std::cout << "Error: Wrong amount of elements read, got: " << read << " expected: "<<test_size << std::endl;
     }
 
     if(verbose) {
-      std::cout << "Queue size = " << queue.size() << std::endl;
+	std::cout << "Queue size = " << queue.size() << std::endl;
     }
     
     return 0;
