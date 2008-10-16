4fec3f08025c7dc2bd14e288ab0d1fbb8964e49e
Jeffrey Vitter <jv>
2008-10-16 14:09:37 +0000

    Hybrid version of error codes. Will go.

--- tpie/err.h
+++ tpie/err.h
@@ -8,48 +8,101 @@
 // AMI error codes, moved here from ami_base.h
 //
 
-#ifndef _AMI_ERR_H
-#define _AMI_ERR_H
+#ifndef _TPIE_AMI_ERR_H
+#define _TPIE_AMI_ERR_H
 
-// AMI error codes are returned using the AMI_err type.
-enum AMI_err {
-  AMI_ERROR_NO_ERROR = 0,
-  AMI_ERROR_IO_ERROR,
-  AMI_ERROR_END_OF_STREAM,
-  AMI_ERROR_READ_ONLY,
-  AMI_ERROR_OS_ERROR,
-  AMI_ERROR_BASE_METHOD,
-  AMI_ERROR_BTE_ERROR,
-  AMI_ERROR_MM_ERROR,
-  AMI_ERROR_OBJECT_INITIALIZATION,
-  AMI_ERROR_OBJECT_INVALID,
-  AMI_ERROR_PERMISSION_DENIED,
-  AMI_ERROR_INSUFFICIENT_MAIN_MEMORY,
-  AMI_ERROR_INSUFFICIENT_AVAILABLE_STREAMS,
-  AMI_ERROR_ENV_UNDEFINED,
-  AMI_ERROR_NO_MAIN_MEMORY_OPERATION,
-  AMI_ERROR_BIT_MATRIX_BOUNDS,
-  AMI_ERROR_NOT_POWER_OF_2,
-  AMI_ERROR_NULL_POINTER,
+namespace tpie {
 
-  AMI_ERROR_GENERIC_ERROR = 0xfff,
+    namespace ami {
+	
+// AMI error codes.
 
-  // Values returned by scan objects.
-  AMI_SCAN_DONE = 0x1000,
-  AMI_SCAN_CONTINUE,
+	enum err {
+	    NO_ERROR = 0,
+	    IO_ERROR,
+	    END_OF_STREAM,
+	    READ_ONLY,
+	    OS_ERROR,
+	    BASE_METHOD,
+	    BTE_ERROR,
+	    MM_ERROR,
+	    OBJECT_INITIALIZATION,
+	    OBJECT_INVALID,
+	    PERMISSION_DENIED,
+	    INSUFFICIENT_MAIN_MEMORY,
+	    INSUFFICIENT_AVAILABLE_STREAMS,
+	    ENV_UNDEFINED,
+	    NO_MAIN_MEMORY_OPERATION,
+	    BIT_MATRIX_BOUNDS,
+	    NOT_POWER_OF_2,
+	    NULL_POINTER,
 
-  // Values returned by merge objects.
-  AMI_MERGE_DONE = 0x2000,
-  AMI_MERGE_CONTINUE,
-  AMI_MERGE_OUTPUT,
-  AMI_MERGE_READ_MULTIPLE,
+	    GENERIC_ERROR = 0xfff,
 
-  // Matrix related errors
-  AMI_MATRIX_BOUNDS = 0x3000,
+	    // Values returned by scan objects.
+	    SCAN_DONE = 0x1000,
+	    SCAN_CONTINUE,
 
-  // Values returned by sort routines.
-  AMI_SORT_ALREADY_SORTED = 0x4000
+	    // Values returned by merge objects.
+	    MERGE_DONE = 0x2000,
+	    MERGE_CONTINUE,
+	    MERGE_OUTPUT,
+	    MERGE_READ_MULTIPLE,
+
+	    // Matrix related errors
+	    MATRIX_BOUNDS = 0x3000,
+
+	    // Values returned by sort routines.
+	    SORT_ALREADY_SORTED = 0x4000
   
-};
+	};
 
-#endif // _AMI_ERR_H
+    }  //  ami namespace
+
+}  //  tpie namespace
+
+#define USE_OLD_ERROR_CODES 1
+
+#if USE_OLD_ERROR_CODES
+	enum AMI_err {
+	    AMI_ERROR_NO_ERROR = 0,
+	    AMI_ERROR_IO_ERROR,
+	    AMI_ERROR_END_OF_STREAM,
+	    AMI_ERROR_READ_ONLY,
+	    AMI_ERROR_OS_ERROR,
+	    AMI_ERROR_BASE_METHOD,
+	    AMI_ERROR_BTE_ERROR,
+	    AMI_ERROR_MM_ERROR,
+	    AMI_ERROR_OBJECT_INITIALIZATION,
+	    AMI_ERROR_OBJECT_INVALID,
+	    AMI_ERROR_PERMISSION_DENIED,
+	    AMI_ERROR_INSUFFICIENT_MAIN_MEMORY,
+	    AMI_ERROR_INSUFFICIENT_AVAILABLE_STREAMS,
+	    AMI_ERROR_ENV_UNDEFINED,
+	    AMI_ERROR_NO_MAIN_MEMORY_OPERATION,
+	    AMI_ERROR_BIT_MATRIX_BOUNDS,
+	    AMI_ERROR_NOT_POWER_OF_2,
+	    AMI_ERROR_NULL_POINTER,
+
+	    AMI_ERROR_GENERIC_ERROR = 0xfff,
+
+	    // Values returned by scan objects.
+	    AMI_SCAN_DONE = 0x1000,
+	    AMI_SCAN_CONTINUE,
+
+	    // Values returned by merge objects.
+	    AMI_MERGE_DONE = 0x2000,
+	    AMI_MERGE_CONTINUE,
+	    AMI_MERGE_OUTPUT,
+	    AMI_MERGE_READ_MULTIPLE,
+
+	    // Matrix related errors
+	    AMI_MATRIX_BOUNDS = 0x3000,
+
+	    // Values returned by sort routines.
+	    AMI_SORT_ALREADY_SORTED = 0x4000
+  
+	};
+#endif
+
+#endif // _TPIE_AMI_ERR_H
