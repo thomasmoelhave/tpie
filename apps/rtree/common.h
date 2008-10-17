bdcb8357335c43273ba3733436b7c807f619792f
Thomas Moelhave <thomas@moelhave.com>
2008-10-17 09:54:12 +0000

    Another missing file.

---
+++ apps/rtree/common.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include "app_config.h"
+#include <tpie/portability.h>
+#include <tpie/stream.h>
+
+using namespace tpie;
+using namespace std;
+using namespace tpie::ami;
+
+
+#endif
