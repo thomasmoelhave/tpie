f71b7d7a14a4e72a150f9192f73729f06352825b
Jeffrey Vitter <jv>
2008-10-17 11:47:09 +0000

    Added tpie::ami namespace. Proceed with caution, not tested.

--- tpie/coll.h
+++ tpie/coll.h
@@ -6,14 +6,18 @@
 //
 // Front end for the AMI_COLLECTION implementations.
 //
-#ifndef _AMI_COLL_H
-#define _AMI_COLL_H
+#ifndef _TPIE_AMI_COLL_H
+#define _TPIE_AMI_COLL_H
 
 // Get definitions for working with Unix and Windows
 #include <tpie/portability.h>
 
 #include <tpie/coll_base.h>
 #include <tpie/coll_single.h>
+
+namespace tpie {
+
+    namespace ami {
 
 ///////////////////////////////////////////////////////////////////////////
 /// \file tpie/coll.h Provides means to choose and set a specific collection type.
@@ -25,10 +29,14 @@
 /// As \ref tpie::ami::collection_single is for now the only collection implementation,
 /// define collection to point to collection_single.
 ///////////////////////////////////////////////////////////////////////////
-#define AMI_collection AMI_collection_single
-
+#define AMI_collection collection_single
+	
 #ifdef BTE_COLLECTION
-#  define AMI_COLLECTION AMI_collection_single< BTE_COLLECTION >
+#  define AMI_COLLECTION collection_single< BTE_COLLECTION >
 #endif
 
-#endif // _AMI_COLL_H
+    }  //  ami namespace
+
+}  //  tpie namespace
+
+#endif // _TPIE_AMI_COLL_H
