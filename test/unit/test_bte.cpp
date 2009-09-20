3f46f26a132ba88cf3a4cdf049fbeaa7383b999c
Thomas Moelhave <thomas@moelhave.com>
2009-09-20 02:37:15 +0000

    Get rid of a bunch of warnings. The problem was that tpie::bte::NO_ERROR was used, even when the stream from tpie::ami::stream was used (which used tpie::ami::err). This could have been fixed a lot easier of tpie::bte::err was an enum inside tpie::bte::stream_base, and similarly for tpie::ami::stream_base and tpie::ami::err.

--- test/unit/test_bte.cpp
+++ test/unit/test_bte.cpp
@@ -37,28 +37,28 @@ using namespace std;
 #define ERR(x) {cerr << x << endl; return 1;}
 const TPIE_OS_OFFSET size = 1024*1024*10;
 
-template <class T> 
-int test_bte(T & bte, char * test) {
+template <typename T, typename ERROR_ENUM> 
+int test_bte(T & bte, char * test, ERROR_ENUM errorval) {
 
 	if(!strcmp(test,"basic")) {
 		srand(42);
 		for(TPIE_OS_OFFSET i= 0; i < size; ++i) 
-			if(bte.write_item(rand()) != NO_ERROR) ERR("Write failed");
+			if(bte.write_item(rand()) != errorval) ERR("Write failed");
 		if(bte.stream_len() != size) ERR("Stream size wrong");
 		if(bte.tell() != size) ERR("Tell failed");
-		if(bte.seek(0) != NO_ERROR) ERR("Seek failed");
+		if(bte.seek(0) != errorval) ERR("Seek failed");
 		if(bte.tell() != 0) ERR("Tell failed");
 		srand(42);
 		for(TPIE_OS_OFFSET i= 0; i < size; ++i) {
 			int * x;
-			if(bte.read_item(&x) != NO_ERROR) ERR("Read failed");
+			if(bte.read_item(&x) != errorval) ERR("Read failed");
 			if(*x != rand()) ERR("Wrong value returned");
 		}
 		return 0;
 	} else if(!strcmp(test,"randomread")) {
 		int * buf = new int[size];
 		srand(42);
-		if(bte.seek(0) != NO_ERROR) ERR("Seek failed (0)");
+		if(bte.seek(0) != errorval) ERR("Seek failed (0)");
 		for(TPIE_OS_OFFSET i =0; i < size; ++i) {
 			int r = rand();
 			buf[i] = r;
@@ -66,10 +66,10 @@ int test_bte(T & bte, char * test) {
 			if(i % (size/200) == 1) {
 				r = rand();
 				int * x;
-				if(bte.seek(r%(i+1)) != NO_ERROR) ERR("Seek failed (1)");
-				if(bte.read_item(&x) != NO_ERROR) ERR("Read failed");
+				if(bte.seek(r%(i+1)) != errorval) ERR("Seek failed (1)");
+				if(bte.read_item(&x) != errorval) ERR("Read failed");
 				if(*x != buf[r%(i+1)]) ERR("Wrong value returned");
-				if(bte.seek(i+1) != NO_ERROR) ERR("Seek failed (2)");
+				if(bte.seek(i+1) != errorval) ERR("Seek failed (2)");
 			}
 		}
 		delete[] buf;
@@ -79,16 +79,16 @@ int test_bte(T & bte, char * test) {
 		int buf[1024];
 		for(TPIE_OS_OFFSET i= 0; i < size; i += 1024) {
 			for(TPIE_OS_OFFSET j=0; j < 1024; ++j) buf[j] = rand();
-			if(bte.write_array(buf, 1024) != NO_ERROR) ERR("Write failed");
+			if(bte.write_array(buf, 1024) != errorval) ERR("Write failed");
 		}
 		if(bte.stream_len() != size) ERR("Stream size wrong");
 		if(bte.tell() != size) ERR("Tell failed");
-		if(bte.seek(0) != NO_ERROR) ERR("Seek failed");
+		if(bte.seek(0) != errorval) ERR("Seek failed");
 		if(bte.tell() != 0) ERR("Tell failed");
 		srand(42);
 		for(TPIE_OS_OFFSET i= 0; i < size; i += 1024) {
 			size_t x=1024;
-			if(bte.read_array(buf, x) != NO_ERROR || x != 1024) ERR("Write failed");
+			if(bte.read_array(buf, x) != errorval || x != 1024) ERR("Write failed");
 			for(TPIE_OS_OFFSET j=0; j < 1024; ++j) 	if(buf[j] != rand()) ERR("Wrong value returned");
 		}
 		return 0;
@@ -101,20 +101,26 @@ int main(int argc, char **argv) {
 	unlink("/tmp/stream");
 	if(!strcmp(argv[1],"stdio")) {
 		stream_stdio<int> stream("/tmp/stream", WRITE_STREAM);
-		return test_bte(stream, argv[2]);
+		return test_bte<stream_stdio<int>,tpie::bte::err>
+				(stream, argv[2], tpie::bte::NO_ERROR);
+
 	} else if(!strcmp(argv[1],"cache")) {
 		stream_cache<int> stream("/tmp/stream", WRITE_STREAM, size*sizeof(int));
-		return test_bte(stream, argv[2]);
+		return test_bte<stream_cache<int>,tpie::bte::err>
+				(stream, argv[2], tpie::bte::NO_ERROR);
 	} else if(!strcmp(argv[1],"ami_stream")) {
 		tpie::ami::stream<int> stream("/tmp/stream", tpie::ami::WRITE_STREAM);
-		return test_bte(stream, argv[2]);
+		return test_bte<tpie::ami::stream<int>,tpie::ami::err>
+				(stream, argv[2], tpie::ami::NO_ERROR);
 #ifndef WIN32
 	} else if(!strcmp(argv[1],"mmap")) {
 		stream_mmap<int> stream("/tmp/stream", WRITE_STREAM);
-		return test_bte(stream, argv[2]);
+		return test_bte<stream_mmap<int>,tpie::bte::err>
+				(stream, argv[2], tpie::bte::NO_ERROR);
 	} else if(!strcmp(argv[1],"ufs")) {
 		stream_ufs<int> stream("/tmp/stream", WRITE_STREAM);
-		return test_bte(stream, argv[2]);
+		return test_bte<stream_ufs<int>,tpie::bte::err>
+				(stream, argv[2], tpie::bte::NO_ERROR);
 #endif 
 	}
 	return 1;
