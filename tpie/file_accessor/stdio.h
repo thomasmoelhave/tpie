ba44d56a605d30007b405d7df0e37ccc488a7647
Antialize <antialize@gmail.com>
2010-04-08 12:38:24 +0200

    Fix some windows issues

--- tpie/file_accessor/stdio.h
+++ tpie/file_accessor/stdio.h
@@ -1,28 +1,32 @@
-// -*- mode: c++; tab-width: 4; indent-tabs-mode: t; eval: (progn (c-set-style "stroustrup") (c-set-offset 'innamespace 0)); -*-
+// -*- mode: c++; tab-width: 4; indent-tabs-mode: t; c-file-style: "stroustrup"; -*-
 // vi:set ts=4 sts=4 sw=4 noet :
-// Copyright 2009, The TPIE development team
-// 
+// Copyright 2009, 2010, The TPIE development team
+//
 // This file is part of TPIE.
-// 
+//
 // TPIE is free software: you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the
 // Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
-// 
+//
 // TPIE is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or
 // FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 // License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with TPIE.  If not, see <http://www.gnu.org/licenses/>
-#ifndef __tpie_file_accessor_stdio_h__
-#define __tpie_file_accessor_stdio_h__
+#ifndef _TPIE_FILE_ACCESSOR_STDIO_H
+#define _TPIE_FILE_ACCESSOR_STDIO_H
 
 #include <tpie/file_accessor/file_accessor.h>
 
 namespace tpie {
 namespace file_accessor {
+
+#ifdef _WIN32
+#define _fseeki64 fseeko
+#endif
 
 class stdio: public file_accessor {
 private:
@@ -38,14 +42,14 @@ private:
 
 public:
 	stdio();
-	virtual void open(const std::string & path, 
-					  bool read, 
-					  bool write, 
+	virtual void open(const std::string & path,
+					  bool read,
+					  bool write,
 					  memory_size_type itemSize,
 					  memory_size_type userDataSize);
 	virtual void close();
 	virtual memory_size_type read(void * data, stream_size_type offset, memory_size_type size);
-	virtual void write(const void * data, stream_size_type offset, memory_size_type size); 
+	virtual void write(const void * data, stream_size_type offset, memory_size_type size);
 	virtual void read_user_data(void * data);
 	virtual void write_user_data(const void * data);
 	virtual void truncate(stream_size_type size);
@@ -57,4 +61,4 @@ public:
 
 }
 }
-#endif //__tpie_file_accessor_stdio_h__
+#endif //_TPIE_FILE_ACCESSOR_STDIO_H
--- tpie/types.h
+++ tpie/types.h
@@ -1,23 +1,23 @@
-// -*- mode: c++; tab-width: 4; indent-tabs-mode: t; eval: (progn (c-set-style "stroustrup") (c-set-offset 'innamespace 0)); -*-
+// -*- mode: c++; tab-width: 4; indent-tabs-mode: t; c-file-style: "stroustrup"; -*-
 // vi:set ts=4 sts=4 sw=4 noet :
-// Copyright 2009, The TPIE development team
-// 
+// Copyright 2009, 2010, The TPIE development team
+//
 // This file is part of TPIE.
-// 
+//
 // TPIE is free software: you can redistribute it and/or modify it under
 // the terms of the GNU Lesser General Public License as published by the
 // Free Software Foundation, either version 3 of the License, or (at your
 // option) any later version.
-// 
+//
 // TPIE is distributed in the hope that it will be useful, but WITHOUT ANY
 // WARRANTY; without even the implied warranty of MERCHANTABILITY or
 // FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
 // License for more details.
-// 
+//
 // You should have received a copy of the GNU Lesser General Public License
 // along with TPIE.  If not, see <http://www.gnu.org/licenses/>
-#ifndef __TPIE_TYPES_H__
-#define __TPIE_TYPES_H__
+#ifndef _TPIE_TYPES_H
+#define _TPIE_TYPES_H
 
 #include <boost/cstdint.hpp>
 
@@ -33,8 +33,12 @@ typedef boost::uint64_t stream_size_type;
 typedef boost::int64_t stream_offset_type;
 
 typedef ::size_t memory_size_type;
+#ifdef _WIN32
+typedef SSIZE_T memory_offset_type;
+#else
 typedef ::ssize_t memory_offset_type;
+#endif
 
 }
 
-#endif //__TPIE_TYPES_H__
+#endif //_TPIE_TYPES_H
