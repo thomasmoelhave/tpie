2fc5cc1d48c32a3c3221c2f9d37ce003f896c75d
Jakob Truelsen <jakob@scalgo.com>
2010-09-08 10:52:19 +0200

    Recreate external priority queue test as a unit test

--- test/unit/test_external_priority_queue.cpp
+++ test/unit/test_external_priority_queue.cpp
@@ -19,8 +19,11 @@
 #include "common.h"
 #include <tpie/priority_queue.h>
 #include <vector>
+#include <vector>
 #include <stdint.h>
+#include <tpie/progress_indicator_arrow.h>
 using namespace tpie;
+using namespace std;
 
 bool basic_test() {
 	uint64_t size=350003; //Must be a prime
@@ -45,10 +48,106 @@ bool basic_test() {
 	return true;
 }
 
+bool medium_instance() {
+	TPIE_OS_OFFSET iterations = 10000;
+    MM_manager.set_memory_limit(16*1024*1024);
+	progress_indicator_arrow progress("Running test","Running test:",1100,iterations,1);
+    for(TPIE_OS_OFFSET it = 1100; it < iterations; it++)  {
+		progress.step();
+		ami::priority_queue<int, std::greater<int> > pq(0.75);
+		std::priority_queue<int, vector<int>,std::less<int> > pq2;
+
+		TPIE_OS_OFFSET elements = 71;
+		TPIE_OS_SRANDOM(static_cast<unsigned int>(it));
+		for(TPIE_OS_OFFSET i=0;i<elements;i++) {
+			int src_int = TPIE_OS_RANDOM()%220;
+			pq.push(src_int);
+			pq2.push(src_int);
+      }
+		pq.pop();
+		pq2.pop();
+		pq.pop();
+		pq2.pop();
+		pq.pop();
+		pq2.pop();
+		TPIE_OS_OFFSET pop = 61; 
+		for(TPIE_OS_OFFSET i=0;i<pop;i++) {
+			if(!pq.empty()) {
+				if(pq.top() != pq2.top()) {
+					std::cerr << "Pop " << i << " got: " << pq.top() << " expected " << pq2.top() << std::endl;
+					return false;
+				}
+				pq.pop();
+				pq2.pop();
+			}
+		}
+		for(TPIE_OS_OFFSET i=0;i<elements;i++) {
+			int src_int = TPIE_OS_RANDOM()%220;
+			pq.push(src_int);
+			pq2.push(src_int);
+		}
+		while(!pq.empty()) {
+			if(pq.top() != pq2.top()) {
+				std::cerr << "Pop2, got: " << pq.top() << " expected " << pq2.top() << std::endl;
+				return false;
+			}
+			pq.pop();
+			pq2.pop();
+		}
+    }
+	progress.done("Done");
+	return true;
+}
+
+template <bool crash_test>
+bool large_instance(){
+	const double PI = acos(-1.0);
+	MM_manager.set_memory_limit(50*1024*1024);
+	double mem_frac = crash_test ? 1.0 : 0.1;
+	ami::priority_queue<uint64_t, std::greater<uint64_t> > pq(mem_frac);
+	std::priority_queue<uint64_t, vector<uint64_t>,std::less<uint64_t> > pq2;
+	double cycle = crash_test ? 20000000000.0 : 50000000.0;
+	const TPIE_OS_OFFSET iterations=500000000;
+	progress_indicator_arrow progress("Running test","Running test:",0,iterations,1);
+	for (TPIE_OS_OFFSET j=0; j<iterations; j++) {
+		progress.step();
+		double i = static_cast<double>(j);
+		double th = (cos(i*2.0*PI/cycle)+1.0)*(RAND_MAX/2);
+		if (!crash_test && pq.size() != pq2.size()) return false;
+		if (!crash_test && pq.empty() != pq2.empty()) return false;
+
+		if (!crash_test && !pq.empty() && pq.top()!=pq2.top()) {
+			std::cerr << j << " Priority queues differ, got " << pq.top() << " but expected " 
+					  << pq2.top() << std::endl;
+			return false;
+		}
+		if (rand()<th) {
+			uint64_t r = rand();
+			pq.push(r);
+			if (!crash_test) pq2.push(r);
+		} else {
+			if (pq.empty()) continue;
+			pq.pop();
+			if (!crash_test) pq2.pop();
+		}
+		if(j%5000000==0) {
+			std::cout << "\rElements in pq: " << pq.size() << "                             " <<  std::endl;
+			std::cout << "Memory available: " << MM_manager.memory_available()/1024/1024 << " MB" << std::endl;
+		}
+	}
+	progress.done("Done");
+	return true;
+}
+
+
 int main(int argc, char **argv) {
 	if(argc != 2) return 1;
 	std::string test(argv[1]);
 	if (test == "basic")
 		return basic_test()?EXIT_SUCCESS:EXIT_FAILURE;
+	else if (test == "medium")
+		return medium_instance()?EXIT_SUCCESS:EXIT_FAILURE;
+	else if (test == "large")
+		return large_instance<false>()?EXIT_SUCCESS:EXIT_FAILURE;
 	return EXIT_FAILURE;
 }
