61763f6ff6187eb50194f35cf14d30878c6a6f41
Mathias Rav <rav@cs.au.dk>
2011-10-14 15:46:55 +0200

    Update parallel_sort unit test

--- test/unit/test_parallel_sort.cpp
+++ test/unit/test_parallel_sort.cpp
@@ -25,6 +25,11 @@
 #include <tpie/memory.h>
 #include "../speed_regression/testtime.h"
 
+//#define TPIE_TEST_PARALLEL_SORT
+#ifdef TPIE_TEST_PARALLEL_SORT
+	#include <parallel/algorithm>
+#endif
+
 static bool progress;
 static bool stdsort;
 
@@ -66,13 +71,27 @@ bool basic1(const size_t elements = 1024*1024) {
 	}
 	if (progress) gen_p->done();
 
-	parallel_sort_impl<std::vector<int>::iterator, std::less<int>, min_size > s(progress ? par_p.get() : 0);
-	s(v2.begin(), v2.end());
+	//boost::posix_time::time_duration t1;
+	//boost::posix_time::time_duration t2;
+	{
+		//boost::posix_time::ptime start=boost::posix_time::microsec_clock::local_time();
+		parallel_sort_impl<std::vector<int>::iterator, std::less<int>, min_size > s(progress ? par_p.get() : 0);
+		s(v2.begin(), v2.end());
+		//boost::posix_time::ptime end=boost::posix_time::microsec_clock::local_time();
+		//std::cout << end-start << " " << std::endl;
+	}
 
 	if (stdsort) {
+		//boost::posix_time::ptime start=boost::posix_time::microsec_clock::local_time();
 		if (progress) std_p->init(1);
+		#ifdef TPIE_TEST_PARALLEL_SORT
+		__gnu_parallel::sort(v1.begin(), v1.end());
+		#else
 		std::sort(v1.begin(), v1.end());
+		#endif
 		if (progress) std_p->done();
+		//boost::posix_time::ptime end=boost::posix_time::microsec_clock::local_time();
+		//std::cout << end-start << " " << std::endl;
 	}
 
 	if (progress) fp->done();
@@ -131,38 +150,33 @@ bool bad_case() {
 }
 
 void stress_test() {
-	boost::posix_time::time_duration d;
-	{
-		std::vector<int> v;
-		for(int i = 0; i < 4000000; ++i)
-			v.push_back(i);
-		boost::posix_time::ptime t1=boost::posix_time::microsec_clock::local_time();
-		parallel_sort_impl<std::vector<int>::iterator, std::less<int> > s(0);
-		s(v.begin(), v.end());
-		boost::posix_time::ptime t2=boost::posix_time::microsec_clock::local_time();
-		d=t2-t1;
-	}
-
 	boost::rand48 prng(42);
-	while(true) {
-		size_t size = (1 << (prng()%18)) + prng()%100;
-		std::vector<int> v1;
-		std::vector<int> v2;
-		for (size_t i=0; i < size; ++i) {
-			v1.push_back(prng());
-			v2.push_back(v1.back());
-		}
-		std::cout << size << " ";
+	for (size_t size_base = 1024;; size_base *= 2) {
+		for (size_t size = size_base; size < size_base * 2; size += size_base / 4) {
+			std::vector<size_t> v1(size);
+			std::vector<size_t> v2(size);
+			for (size_t i=0; i < size; ++i) {
+				v1[i] = v2[i] = prng();
+			}
+			std::cout << size << " " << std::flush;
 
-		boost::posix_time::ptime t1=boost::posix_time::microsec_clock::local_time();
-		std::sort(v1.begin(), v1.end());
-		boost::posix_time::ptime t2=boost::posix_time::microsec_clock::local_time();
-		parallel_sort_impl<std::vector<int>::iterator, std::less<int> > s(0);
-		s(v2.begin(), v2.end());
-		boost::posix_time::ptime t3=boost::posix_time::microsec_clock::local_time();
-		if(v1 != v2) {std::cerr << "Failed" << std::endl; return;}
-		std::cout << "std: " << (t2-t1) << " ours: " << t3-t2 << std::endl;
-		if( std::max(d,(t2-t1)*3) < (t3-t2)  ) {std::cerr << "Too slow" << std::endl; return;}
+			boost::posix_time::time_duration t1;
+			boost::posix_time::time_duration t2;
+			{
+				boost::posix_time::ptime start=boost::posix_time::microsec_clock::local_time();
+				std::sort(v1.begin(), v1.end());
+				boost::posix_time::ptime end=boost::posix_time::microsec_clock::local_time();
+				std::cout << "std: " << (t1 = end-start) << std::flush;
+			}
+			{
+				boost::posix_time::ptime start=boost::posix_time::microsec_clock::local_time();
+				parallel_sort_impl<std::vector<size_t>::iterator, std::less<size_t>, 524288/8 > s(0);
+				s(v2.begin(), v2.end());
+				boost::posix_time::ptime end=boost::posix_time::microsec_clock::local_time();
+				std::cout << " ours: " << (t2 = end-start) << std::endl;
+			}
+			if( t1*3 < t2  ) {std::cerr << "Too slow" << std::endl; return;}
+		}
 	}
 }
 
