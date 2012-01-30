6436f738ba553cccde3ac6f3699ebe49a025c576
Mathias Rav <rav@cs.au.dk>
2012-01-30 11:49:31 +0100

    Document dummy_progress.h

--- tpie/dummy_progress.h
+++ tpie/dummy_progress.h
@@ -21,32 +21,107 @@
 
 #include <tpie/fractional_progress.h>
 
+///////////////////////////////////////////////////////////////////////////////
+/// \file dummy_progress.h
+///
+/// Implementation of the progress indicator concept in an efficient
+/// non-inheritance way.  For a null object child class of
+/// \ref progress_indicator_base, see \ref progress_indicator_null.
+///////////////////////////////////////////////////////////////////////////////
+
 namespace tpie {
 
 struct dummy_progress_indicator;
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief  A fractional progress indicator that is conceptually compatible
+/// with tpie::fractional_progress.
+///////////////////////////////////////////////////////////////////////////////
+
 struct dummy_fraction_progress {
-	inline dummy_fraction_progress(tpie::progress_indicator_base *) {}
-	inline dummy_fraction_progress(dummy_progress_indicator *) {}
-	inline dummy_fraction_progress & id() {return *this;}
+	/// \copybrief tpie::fractional_progress::fractional_progress(progress_indicator_base *)
+	/// \copydetails tpie::fractional_progress::fractional_progress(progress_indicator_base *)
+	inline dummy_fraction_progress(tpie::progress_indicator_base *) {
+		// Do nothing.
+	}
+
+	/// \copybrief tpie::fractional_progress::fractional_progress()
+	/// \copydetails tpie::fractional_progress::fractional_progress()
+	inline dummy_fraction_progress(dummy_progress_indicator *) {
+		// Do nothing.
+	}
+
+	/// \copybrief tpie::fractional_progress::id()
+	/// \copydetails tpie::fractional_progress::id()
+	inline dummy_fraction_progress & id() {
+		return *this;
+	}
 
 	template <typename T>
-	inline dummy_fraction_progress & operator << (const T &) {return *this;}
-	inline void init() {}
-	inline void done() {}
+	inline dummy_fraction_progress & operator << (const T &) {
+		return *this;
+	}
+
+	/// \copybrief tpie::fractional_progress::init()
+	/// \copydetails tpie::fractional_progress::init()
+	inline void init() {
+	}
+
+	/// \copybrief tpie::fractional_progress::done()
+	/// \copydetails tpie::fractional_progress::done()
+	inline void done() {
+	}
 };
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief  A progress indicator that is conceptually compatible with
+/// tpie::progress_indicator_base.
+///////////////////////////////////////////////////////////////////////////////
 
 struct dummy_progress_indicator {
-	inline dummy_progress_indicator() {}
+	inline dummy_progress_indicator() {
+		// Do nothing.
+	}
+
 	inline dummy_progress_indicator(dummy_fraction_progress &, 
-									const char *, const char *, const char *, TPIE_OS_OFFSET, const char *c=0, bool a=true, bool b=true) { unused(a); unused(b); unused(c);}
-	inline void init(TPIE_OS_OFFSET) {}
-	inline void step(TPIE_OS_OFFSET) {}
-	inline void step() {}
-	inline void done() {}
+									const char *, const char *, const char *,
+									TPIE_OS_OFFSET, const char *c=0,
+									bool a=true, bool b=true) {
+		unused(a);
+		unused(b);
+		unused(c);
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+	/// \copybrief progress_indicator_base::init(TPIE_OS_OFFSET)
+	/// \copydetails progress_indicator_base::init(TPIE_OS_OFFSET)
+	///////////////////////////////////////////////////////////////////////////
+	inline void init(TPIE_OS_OFFSET) { }
+
+	///////////////////////////////////////////////////////////////////////////
+	/// \copybrief progress_indicator_base::step(TPIE_OS_OFFSET)
+	/// \copydetails progress_indicator_base::step(TPIE_OS_OFFSET)
+	///////////////////////////////////////////////////////////////////////////
+	inline void step(TPIE_OS_OFFSET) { }
+
+	///////////////////////////////////////////////////////////////////////////
+	/// \copybrief progress_indicator_base::step(TPIE_OS_OFFSET)
+	/// \copydetails progress_indicator_base::step(TPIE_OS_OFFSET)
+	///////////////////////////////////////////////////////////////////////////
+	inline void step() { }
+
+	///////////////////////////////////////////////////////////////////////////
+	/// \copybrief progress_indicator_base::done()
+	/// \copydetails progress_indicator_base::done()
+	///////////////////////////////////////////////////////////////////////////
+	inline void done() { }
 };
 
-
+///////////////////////////////////////////////////////////////////////////////
+/// For applications where you wish to disable progress indicators via a
+/// template parameter, refer to progress_types<use_progress> members names
+/// sub, fp and base.
+///////////////////////////////////////////////////////////////////////////////
 template <bool use_progress>
 struct progress_types {
 	typedef fractional_subindicator sub;
