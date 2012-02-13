da4a5a271041167b332530096eb80799fb8d61cc
Mathias Rav <rav@cs.au.dk>
2012-02-13 14:05:59 +0100

    Fix doc util.h

--- tpie/util.h
+++ tpie/util.h
@@ -17,6 +17,10 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with TPIE.  If not, see <http://www.gnu.org/licenses/>
 
+///////////////////////////////////////////////////////////////////////////////
+/// \file Miscellaneous utility functions
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef __TPIE_UTIL_H__
 #define __TPIE_UTIL_H__
 
