5a323d16ad60afb9e41288816f679c10b9208356
Mathias Rav <rav@cs.au.dk>
2012-02-13 14:01:05 +0100

    Fix doc types.h

--- tpie/types.h
+++ tpie/types.h
@@ -16,6 +16,11 @@
 //
 // You should have received a copy of the GNU Lesser General Public License
 // along with TPIE.  If not, see <http://www.gnu.org/licenses/>
+
+///////////////////////////////////////////////////////////////////////////////
+/// \file types.h  Standard types.
+///////////////////////////////////////////////////////////////////////////////
+
 #ifndef _TPIE_TYPES_H
 #define _TPIE_TYPES_H
 
