accd7f587ec1ad32ae6fcbedb5d6d2c72031f91d
Mathias Rav <rav@cs.au.dk>
2012-02-27 11:22:34 +0100

    tpie_delete: We don't care if the deleted pointer has a different type from when it was allocated. We only care that it has the same size.

--- tpie/memory.cpp
+++ tpie/memory.cpp
@@ -178,7 +178,7 @@ void memory_manager::__register_pointer(void * p, size_t size, const std::type_i
 	m_pointers[p] = std::make_pair(size, &t);;
 }
 
-void memory_manager::__unregister_pointer(void * p, size_t size, const std::type_info & t) {
+void memory_manager::__unregister_pointer(void * p, size_t size, const std::type_info & /*t*/) {
 	boost::unordered_map<void *, std::pair<size_t, const std::type_info *> >::const_iterator i=m_pointers.find(p);
 	if (i == m_pointers.end()) {
 		log_error() << "Trying to deregister pointer " << p << " of size "
@@ -188,11 +188,6 @@ void memory_manager::__unregister_pointer(void * p, size_t size, const std::type
 		if (i->second.first != size) {
 			log_error() << "Trying to deregister pointer " << p << " of size "
 						<< size << " which was registered with size " << i->second.first << std::endl;
-			segfault();
-		}
-		if (*i->second.second != t) {
-			log_error() << "Trying to deregister pointer " << p << " of type "
-						<< t.name() << " which was registered with size " << i->second.second->name() << std::endl;
 			segfault();
 		}
 		m_pointers.erase(i);
