0b40b9fcb65aa59485855a9e94ea67e3486863c8
Mathias Rav <rav@cs.au.dk>
2012-03-27 12:26:20 +0200

    Switch queue to using the temp_file open method of file

--- tpie/queue.h
+++ tpie/queue.h
@@ -46,7 +46,7 @@ public:
 	////////////////////////////////////////////////////////////////////
 	queue(stream_size_type elements=std::numeric_limits<stream_size_type>::max(), 
 		  double block_factor=1.0): m_size(0), m_file(block_factor), m_back(m_file), m_front(m_file) {
-		m_file.open(m_temp.path(), file_base::read_write, sizeof(stream_size_type) );
+		m_file.open(m_temp, file_base::read_write, sizeof(stream_size_type) );
 		unused(elements);
 	}
 
