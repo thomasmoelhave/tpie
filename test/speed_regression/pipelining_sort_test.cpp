27fb119fcead66274e21470bee46545bceb33a78
Mathias Rav <rav@cs.au.dk>
2012-05-28 10:49:38 +0200

    Enable setting memory limit in pipelining sort test

--- test/speed_regression/pipelining_sort_test.cpp
+++ test/speed_regression/pipelining_sort_test.cpp
@@ -32,68 +32,80 @@ using namespace tpie;
 using namespace tpie::test;
 using namespace tpie::pipelining;
 
-typedef int64_t test_t;
+typedef uint64_t test_t;
 
-void parse_args(int argc, char ** argv, stream_size_type & elements, size_t & times, stream_size_type & runLength, size_t & fanout) {
+struct testparams {
+	stream_size_type elements;
+	size_t times;
+	stream_size_type runLength;
+	size_t fanout;
+	memory_size_type memoryLimit;
+};
+
+void parse_args(int argc, char ** argv, testparams & p) {
 	int i = 1;
 	while (i < argc) {
 		std::string arg(argv[i]);
 		if (arg == "-r") {
 			std::stringstream ss(argv[i+1]);
-			ss >> runLength;
+			ss >> p.runLength;
 			i += 2;
 		} else if (arg == "-f") {
 			std::stringstream ss(argv[i+1]);
-			ss >> fanout;
+			ss >> p.fanout;
+			i += 2;
+		} else if (arg == "-m") {
+			std::stringstream ss(argv[i+1]);
+			ss >> p.memoryLimit;
 			i += 2;
 		} else if (arg == "-h" || arg == "--help") {
-			std::cout << "Usage: " << argv[0] << " [-r runLength] [-f fanout] <elements> <times>" << std::endl;
+			std::cout << "Usage: " << argv[0] << " [-r runLength] [-f fanout] [-m memoryLimit] <elements> <times>" << std::endl;
 			exit(1);
 		} else break;
 	}
 	if (i < argc) {
 		std::stringstream ss(argv[i]);
-		ss >> elements;
+		ss >> p.elements;
 		++i;
 	}
 	if (i < argc) {
 		std::stringstream ss(argv[i]);
-		ss >> times;
+		ss >> p.times;
 		++i;
 	}
 }
 
-void test(test_t elements, size_t times, size_t runLength = 0, size_t fanout = 0) {
+void test(testparams & p) {
 	std::vector<const char *> names(4);
 	names[0] = "Form runs";
 	names[1] = "Merge";
 	names[2] = "Verify";
 	names[3] = "Correct";
 	tpie::test::stat stats(names);
-	for (size_t i = 0; i < times; ++i) {
-		test_t s = tpie::next_prime(elements);
-		test_t y = elements-16;
+	for (size_t i = 0; i < p.times; ++i) {
+		test_t s = tpie::next_prime(p.elements);
+		test_t y = p.elements-16;
 
 		test_realtime_t start;
 		test_realtime_t end;
 
 		getTestRealtime(start);
 
-		merge_sorter<test_t> m(0);
-		if (runLength) {
-			m.set_parameters(runLength, fanout);
+		merge_sorter<test_t> m(p.memoryLimit);
+		if (p.runLength) {
+			m.set_parameters(p.runLength, p.fanout);
 		}
 		m.begin();
 		size_t pushed = 0;
 		for (test_t j = 0; j < s; ++j) {
 			test_t el = (j * y) % s;
-			if (el < elements) {
+			if (el < p.elements) {
 				m.push(el);
 				++pushed;
 			}
 		}
 		m.end();
-		if (pushed != elements) errx(101, "Wut");
+		if (pushed != p.elements) errx(101, "Wut");
 
 		getTestRealtime(end);
 		stats(testRealtimeDiff(start, end));
@@ -105,7 +117,7 @@ void test(test_t elements, size_t times, size_t runLength = 0, size_t fanout = 0
 
 		bool good = true;
 		getTestRealtime(start);
-		for (test_t j = 0; j < elements; ++j) {
+		for (test_t j = 0; j < p.elements; ++j) {
 			tp_assert(m.can_pull(), "Can pull");
 			test_t el = m.pull();
 			if (el != j) good = false;
@@ -117,21 +129,18 @@ void test(test_t elements, size_t times, size_t runLength = 0, size_t fanout = 0
 }
 
 int main(int argc, char ** argv) {
-	stream_size_type elements = 10;
-	size_t times = 1;
-	size_t runLength = 0;
-	size_t fanout = 0;
-	parse_args(argc, argv, elements, times, runLength, fanout);
-	testinfo t("Pipelining sort speed test", 1024, 0, times);
+	testparams p = {10, 1, 0, 0, 50*1024*1024};
+	parse_args(argc, argv, p);
+	testinfo t("Pipelining sort speed test", 1024, 0, p.times);
 	sysinfo s;
-	s.printinfo("Elements", elements);
-	if (runLength) {
-		s.printinfo("Run length", runLength);
-		s.printinfo("Fanout", fanout);
+	s.printinfo("Elements", p.elements);
+	if (p.runLength) {
+		s.printinfo("Run length", p.runLength);
+		s.printinfo("Fanout", p.fanout);
 	} else {
 		s.printinfo("Run length", "(default)");
 		s.printinfo("Fanout", "(default)");
 	}
-	::test(elements, times, runLength, fanout);
+	::test(p);
 	return 0;
 }
