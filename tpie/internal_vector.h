c73c423b11e84219610ae8b82367eea1c3d65bb9
Mathias Rav <rav@cs.au.dk>
2012-10-26 10:34:44 +0200

    Fix documentation of internal_vector and internal_stack

--- tpie/internal_vector.h
+++ tpie/internal_vector.h
@@ -1,6 +1,6 @@
 // -*- mode: c++; tab-width: 4; indent-tabs-mode: t; c-file-style: "stroustrup"; -*-
 // vi:set ts=4 sts=4 sw=4 noet :
-// Copyright 2010, The TPIE development team
+// Copyright 2010, 2012, The TPIE development team
 // 
 // This file is part of TPIE.
 // 
@@ -28,11 +28,11 @@
 #include <tpie/internal_stack_vector_base.h>
 namespace tpie {
 
-/////////////////////////////////////////////////////////
-/// \brief A generic internal vector
+///////////////////////////////////////////////////////////////////////////////
+/// \brief A generic internal vector.
 ///
-/// \tparam T The type of items storred on the vector
-/////////////////////////////////////////////////////////
+/// \tparam T The type of items stored in the structure.
+///////////////////////////////////////////////////////////////////////////////
 template <typename T>
 class internal_vector: public internal_stack_vector_base<T,internal_vector<T> > {
 public:
@@ -41,43 +41,44 @@ public:
 	typedef typename array<T>::const_iterator const_iterator;
 	using parent_t::m_elements;
 	using parent_t::m_size;
+
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief Construct structure with given capacity.
+	/// \copydetails tpie::internal_stack_vector_base::internal_stack_vector_base
+	///////////////////////////////////////////////////////////////////////////
 	internal_vector(size_t size=0): parent_t(size){}
-	
-	/////////////////////////////////////////////////////////
-	/// \brief Acces operator 
-	/////////////////////////////////////////////////////////
+
 	inline T & operator[](size_t s){return m_elements[s];}
 	inline const T & operator[](size_t s)const {return m_elements[s];}
-	
+
 	inline T & front(){return m_elements[0];}
 	inline const T & front()const {return m_elements[0];}
 
 	inline T & back(){return m_elements[m_size-1];}
 	inline const T & back()const {return m_elements[m_size-1];}
-	
-	/////////////////////////////////////////////////////////
-	/// \brief Add an element to the end of the vector
+
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief Add an element to the end of the vector.
 	///
-	/// \param val The element to add
-	/////////////////////////////////////////////////////////
+	/// \param val The element to add.
+	///////////////////////////////////////////////////////////////////////////
 	inline T & push_back(const T & val){m_elements[m_size++] = val; return back();}
 
-	/////////////////////////////////////////////////////////
-	/// \brief Add an element to the end of the vector
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief If an item was previously popped from this point in the
+	/// structure, push it to the structure again; otherwise, push the default
+	/// value.
 	///
-	/////////////////////////////////////////////////////////
+	/// \param val The element to add.
+	///////////////////////////////////////////////////////////////////////////
 	inline T & push_back(){++m_size; return back();}
 
-	/////////////////////////////////////////////////////////
-	/// \brief Remove the last element from the vector
-	/////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief Remove the last element from the vector.
+	///////////////////////////////////////////////////////////////////////////
 	inline void pop_back(){--m_size;}
-	
 
-	/////////////////////////////////////////////////////////
-	/// \brief Iterators 
-	/////////////////////////////////////////////////////////	
-	inline iterator begin(){ return m_elements.begin();}	
+	inline iterator begin(){ return m_elements.begin();}
 	inline const_iterator begin()const {return m_elements.begin();}
 	inline iterator end(){return m_elements.find(m_size);}
 	inline const_iterator end()const {return m_elements.find(m_size);}
