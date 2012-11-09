ff3ad8e02badea49ae8e6ddadcb08ccc2ede5189
Mathias Rav <rav@cs.au.dk>
2012-11-09 15:27:24 +0100

    Refactor access between virtual chunk classes, moving into a friended `access' class

--- tpie/pipelining/virtual.h
+++ tpie/pipelining/virtual.h
@@ -120,6 +120,10 @@ public:
 /// through static methods that return a virt_node::ptr, providing reference
 /// counting so that pipe_segments are only instantiated once each and are
 /// destroyed when the pipeline object goes out of scope.
+///
+/// This class either owns a pipe_segment or two virt_nodes. The first case is
+/// used in the virtual_chunk constructors that accept a single pipe_base. The
+/// second case is used in the virtual_chunk pipe operators.
 ///////////////////////////////////////////////////////////////////////////////
 class virt_node {
 public:
@@ -204,20 +208,52 @@ public:
 } // namespace bits
 
 // Predeclare
+template <typename Input>
+class virtual_chunk_end;
+
+// Predeclare
 template <typename Input, typename Output>
 class virtual_chunk;
+
+// Predeclare
+template <typename Output>
+class virtual_chunk_begin;
+
+namespace bits {
+
+class access {
+	template <typename>
+	friend class pipelining::virtual_chunk_end;
+	template <typename, typename>
+	friend class pipelining::virtual_chunk;
+	template <typename>
+	friend class pipelining::virtual_chunk_begin;
+
+	template <typename Input>
+	static virtsrc<Input> * get_source(const virtual_chunk_end<Input> &);
+	template <typename Input, typename Output>
+	static virtsrc<Input> * get_source(const virtual_chunk<Input, Output> &);
+	template <typename Input, typename Output>
+	static virtrecv<Output> * get_destination(const virtual_chunk<Input, Output> &);
+	template <typename Output>
+	static virtrecv<Output> * get_destination(const virtual_chunk_begin<Output> &);
+};
+
+} // namespace bits
 
 ///////////////////////////////////////////////////////////////////////////////
 /// \brief Virtual chunk that has no output (that is, virtual consumer).
 ///////////////////////////////////////////////////////////////////////////////
 template <typename Input>
 class virtual_chunk_end : public bits::virtual_chunk_base {
+	friend class bits::access;
+	typedef bits::access acc;
 	typedef bits::virtsrc<Input> src_type;
 	src_type * m_src;
 
-public:
 	src_type * get_source() const { return m_src; }
 
+public:
 	///////////////////////////////////////////////////////////////////////////
 	/// \brief Constructor that leaves the virtual chunk unassigned.
 	///////////////////////////////////////////////////////////////////////////
@@ -267,14 +303,16 @@ public:
 ///////////////////////////////////////////////////////////////////////////////
 template <typename Input, typename Output=Input>
 class virtual_chunk : public bits::virtual_chunk_base {
+	friend class bits::access;
+	typedef bits::access acc;
 	typedef bits::virtsrc<Input> src_type;
 	typedef bits::virtrecv<Output> recv_type;
 	src_type * m_src;
 	recv_type * m_recv;
-public:
 	src_type * get_source() const { return m_src; }
 	recv_type * get_destination() const { return m_recv; }
 
+public:
 	///////////////////////////////////////////////////////////////////////////
 	/// \brief Constructor that leaves the virtual chunk unassigned.
 	///////////////////////////////////////////////////////////////////////////
@@ -302,8 +340,8 @@ public:
 				  const virtual_chunk<Mid, Output> & right)
 		: virtual_chunk_base(bits::virt_node::combine(left.get_node(), right.get_node()))
 	{
-		m_src = left.get_source();
-		m_recv = right.get_destination();
+		m_src = acc::get_source(left);
+		m_recv = acc::get_destination(right);
 	}
 
 	///////////////////////////////////////////////////////////////////////////
@@ -333,7 +371,7 @@ public:
 			return *bits::assert_types_equal_and_return<Input, Output, virtual_chunk<Input, NextOutput> *>
 				::go(&dest);
 		}
-		m_recv->set_destination(dest.get_source());
+		m_recv->set_destination(acc::get_source(dest));
 		return virtual_chunk<Input, NextOutput>(*this, dest);
 	}
 
@@ -345,7 +383,7 @@ public:
 			return *bits::assert_types_equal_and_return<Input, Output, virtual_chunk_end<Input> *>
 				::go(&dest);
 		}
-		m_recv->set_destination(dest.get_source());
+		m_recv->set_destination(acc::get_source(dest));
 		return virtual_chunk_end<Input>(*this, dest);
 	}
 };
@@ -357,7 +395,7 @@ virtual_chunk_end<Input>::virtual_chunk_end(const virtual_chunk<Input, Mid> & le
 	: virtual_chunk_base(left.get_segment_map(),
 						 bits::virt_node::combine(left.get_node(), right.get_node()))
 {
-	m_src = left.get_source();
+	m_src = acc::get_source(left);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -365,11 +403,13 @@ virtual_chunk_end<Input>::virtual_chunk_end(const virtual_chunk<Input, Mid> & le
 ///////////////////////////////////////////////////////////////////////////////
 template <typename Output>
 class virtual_chunk_begin : public bits::virtual_chunk_base {
+	friend class bits::access;
+	typedef bits::access acc;
 	typedef bits::virtrecv<Output> recv_type;
 	recv_type * m_recv;
-public:
 	recv_type * get_destination() const { return m_recv; }
 
+public:
 	///////////////////////////////////////////////////////////////////////////
 	/// \brief Constructor that leaves the virtual chunk unassigned.
 	///////////////////////////////////////////////////////////////////////////
@@ -397,7 +437,7 @@ public:
 		: virtual_chunk_base(left.get_segment_map(),
 							 bits::virt_node::combine(left.get_node(), right.get_node()))
 	{
-		m_recv = right.get_destination();
+		m_recv = acc::get_destination(right);
 	}
 
 	///////////////////////////////////////////////////////////////////////////
@@ -426,7 +466,7 @@ public:
 			return *bits::assert_types_equal_and_return<Output, NextOutput, virtual_chunk_begin<NextOutput> *>
 				::go(this);
 		}
-		m_recv->set_destination(dest.get_source());
+		m_recv->set_destination(acc::get_source(dest));
 		return virtual_chunk_begin<NextOutput>(*this, dest);
 	}
 
@@ -436,11 +476,35 @@ public:
 	virtual_chunk_base operator|(virtual_chunk_end<Output> dest) {
 		if (!*this) throw virtual_chunk_missing_begin();
 		if (!dest) throw virtual_chunk_missing_end();
-		m_recv->set_destination(dest.get_source());
+		m_recv->set_destination(acc::get_source(dest));
 		return virtual_chunk_base(this->m_segmap,
 								  bits::virt_node::combine(get_node(), dest.get_node()));
 	}
 };
+
+namespace bits {
+
+template <typename Input>
+virtsrc<Input> * access::get_source(const virtual_chunk_end<Input> & chunk) {
+	return chunk.get_source();
+}
+
+template <typename Input, typename Output>
+virtsrc<Input> * access::get_source(const virtual_chunk<Input, Output> & chunk) {
+	return chunk.get_source();
+}
+
+template <typename Input, typename Output>
+virtrecv<Output> * access::get_destination(const virtual_chunk<Input, Output> & chunk) {
+	return chunk.get_destination();
+}
+
+template <typename Output>
+virtrecv<Output> * access::get_destination(const virtual_chunk_begin<Output> & chunk) {
+	return chunk.get_destination();
+}
+
+} // namespace bits
 
 } // namespace pipelining
 
