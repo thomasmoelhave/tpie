0b8f5dbb63d192ad7f6dc5ef56ef870bf249aee0
Mathias Rav <rav@cs.au.dk>
2013-03-26 10:49:46 +0100

    Add ut-pipelining_serialization sort test.

--- test/unit/test_pipelining_serialization.cpp
+++ test/unit/test_pipelining_serialization.cpp
@@ -124,9 +124,81 @@ bool reverse_test() {
 	return true;
 }
 
+template <typename dest_t>
+class random_strings_type : public node {
+	dest_t dest;
+	stream_size_type n;
+
+public:
+	random_strings_type(const dest_t & dest, stream_size_type n)
+		: dest(dest)
+		, n(n)
+	{
+		add_push_destination(dest);
+		set_name("Random strings");
+	}
+
+	virtual void go() override {
+		using namespace boost::posix_time;
+		long seed = boost::posix_time::microsec_clock::local_time().time_of_day().fractional_seconds();
+		boost::mt19937 rng(seed);
+		for (stream_size_type i = 0; i < n; ++i) {
+			size_t length = rng() % 10;
+			std::string s(length, '\0');
+			for (size_t j = 0; j < length; ++j) s[j] = 'a' + (rng() % 26);
+			dest.push(s);
+		}
+	}
+};
+
+pipe_begin<factory_1<random_strings_type, stream_size_type> >
+random_strings(stream_size_type n) {
+	return factory_1<random_strings_type, stream_size_type>(n);
+}
+
+class sort_verifier_type : public node {
+	bool & res;
+	std::string prev;
+
+public:
+	typedef std::string item_type;
+
+	sort_verifier_type(bool & res)
+		: res(res)
+	{
+		set_name("Sort verifier");
+		res = true;
+	}
+
+	void push(const std::string & s) {
+		if (s < prev) {
+			res = false;
+			log_error() << "Got an out of order string" << std::endl;
+		}
+		prev = s;
+	}
+};
+
+pipe_end<termfactory_1<sort_verifier_type, bool &> >
+sort_verifier(bool & res) {
+	return termfactory_1<sort_verifier_type, bool &>(res);
+}
+
+bool sort_test(stream_size_type n) {
+	bool result = false;
+	pipeline p =
+		random_strings(n)
+		| serialization_pipesort()
+		| sort_verifier(result)
+		;
+	p();
+	return result;
+}
+
 int main(int argc, char ** argv) {
 	return tpie::tests(argc, argv)
 	.test(basic_test, "basic")
 	.test(reverse_test, "reverse")
+	.test(sort_test, "sort", "n", static_cast<stream_size_type>(1000))
 	;
 }
