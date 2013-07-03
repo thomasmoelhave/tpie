874fb44cd966688f1a46850572f44e105e5e1858
Mathias Rav <rav@cs.au.dk>
2013-07-03 12:06:01 +0200

    Some work towards preventing I/O when seeking inside the current block.

--- tpie/compressed/stream.h
+++ tpie/compressed/stream.h
@@ -293,6 +293,7 @@ public:
 		, m_seekState(seek_state::beginning)
 		, m_bufferState(buffer_state::write_only)
 		, m_position(stream_position(0, 0, 0, 0))
+		, m_nextPosition(stream_position(0, 0, 0, 0))
 		, m_nextReadOffset(0)
 		, m_nextBlockSize(0)
 	{
@@ -325,7 +326,11 @@ public:
 				out << ", seeking to end";
 				break;
 			case seek_state::position:
-				out << ", seeking to position";
+				out << ", seeking to position " << m_nextPosition.offset();
+				out << " (block " << m_nextPosition.block_number()
+					<< " @ byte " << m_nextPosition.read_offset()
+					<< ", item " << m_nextPosition.block_item_index()
+					<< ")";
 				break;
 		}
 
@@ -364,13 +369,22 @@ public:
 	void seek(stream_offset_type offset, offset_type whence=beginning) {
 		assert(this->is_open());
 		if (whence == beginning && offset == 0) {
-			m_seekState = seek_state::beginning;
+			if (m_buffer.get() != 0 && m_position.block_number() == 0) {
+				// We are already reading or writing the first block.
+				m_lastItem = m_nextItem;
+				m_nextItem = m_bufferBegin;
+				m_position = stream_position(m_position.read_offset(),
+											 0, 0, 0);
+			} else {
+				// We need to load the first block on the next I/O.
+				m_seekState = seek_state::beginning;
+			}
 			if (size() > 0)
 				m_bufferState = buffer_state::read_only;
 			else
 				m_bufferState = buffer_state::write_only;
-			m_position = stream_position(0, 0, 0, 0);
 		} else if (whence == end && offset == 0) {
+			//if (m_position.block_number() == m_streamBlocks) {
 			m_seekState = seek_state::end;
 			m_bufferState = buffer_state::write_only;
 		} else {
@@ -379,7 +393,17 @@ public:
 	}
 
 	stream_size_type offset() const {
-		return m_position.offset();
+		switch (m_seekState) {
+			case seek_state::none:
+				return m_position.offset();
+			case seek_state::beginning:
+				return 0;
+			case seek_state::end:
+				return size();
+			case seek_state::position:
+				return m_nextPosition.offset();
+		}
+		throw exception("offset: Unreachable statement; m_seekState invalid");
 	}
 
 	stream_size_type size() const {
@@ -404,10 +428,9 @@ public:
 		switch (m_seekState) {
 			case seek_state::position:
 				// We just set_position, so we can just return what we got.
-				return m_position;
+				return m_nextPosition;
 			case seek_state::beginning:
-				// seek(0) has set m_position properly
-				return m_position;
+				return stream_position(0, 0, 0, 0);
 			case seek_state::none:
 				if (m_bufferState == buffer_state::read_only) {
 					if (m_nextItem == m_lastItem) {
@@ -449,18 +472,75 @@ public:
 		if (m_seekState != seek_state::end) {
 			blockItemIndex = m_nextItem - m_bufferBegin;
 		}
+		m_knownFileSize = readOffset;
 		return stream_position(readOffset, blockItemIndex, blockNumber, size());
 	}
 
 	void set_position(const stream_position & pos) {
-		m_position = pos;
+		// If the code is correct, short circuiting is not necessary;
+		// if the code is not correct, short circuiting might mask faults.
+		/*
+		if (pos == m_position) {
+			m_seekState = seek_state::none;
+			return;
+		}
+		*/
+
+		if (m_buffer.get() != 0
+			&& pos.block_number() == m_position.block_number())
+		{
+			if (pos.read_offset() != m_position.read_offset())
+				throw stream_exception("set_position: Invalid position; read_offset is wrong");
+
+			if (pos.offset() - pos.block_item_index()
+				!= m_position.offset() - m_position.block_item_index())
+			{
+				throw stream_exception("set_position: Invalid position; offset is wrong");
+			}
+
+			// Compute the number of items in the current block (m_buffer).
+			// We don't want to add a `default` case to the switch(m_bufferState),
+			// since that masks the compiler warning when an enum value is added
+			// that needs to be handled.
+			// If no `default` case is handled, currentBlockItems must be
+			// initialized to a value here, or the compiler will complain.
+			memory_size_type currentBlockItems = 0;
+
+			switch (m_bufferState) {
+				case buffer_state::read_only:
+					currentBlockItems = m_lastItem - m_bufferBegin;
+					break;
+				case buffer_state::write_only:
+					currentBlockItems = m_nextItem - m_bufferBegin;
+					break;
+			}
+
+			if (currentBlockItems > m_blockSize)
+				throw exception("Something went wrong: number of items in block is greater than the block size");
+
+			if (pos.block_item_index() > currentBlockItems)
+				throw stream_exception("set_position: Invalid position; block item index out of bounds");
+
+			if (pos.block_item_index() == m_blockSize)
+				throw stream_exception("set_position: Invalid position; block item index is equal to block size");
+
+			if (pos.block_item_index() == currentBlockItems)
+				throw stream_exception("set_position: Invalid position; block item index is at block bounds");
+
+			m_position = pos;
+			m_nextItem = m_bufferBegin + pos.block_item_index();
+			m_lastItem = m_bufferBegin + currentBlockItems;
+			m_bufferState = buffer_state::read_only;
+			return;
+		}
+		m_nextPosition = pos;
 		m_seekState = seek_state::position;
 	}
 
 private:
 	const T & read_ref() {
-		if (m_seekState != seek_state::none) perform_seek();
 		if (!can_read()) throw stream_exception("!can_read()");
+		if (m_seekState != seek_state::none) perform_seek();
 		if (m_nextItem == m_lastItem) {
 			compressor_thread_lock l(compressor());
 			read_next_block(l, m_position.block_number() + 1);
@@ -528,6 +608,9 @@ protected:
 		if (m_seekState == seek_state::none)
 			return;
 
+		// For debugging: Verify get_position()
+		stream_position claimedPosition = get_position();
+
 		if (this->m_bufferDirty) {
 			flush_block();
 		}
@@ -537,16 +620,31 @@ protected:
 		finish_requests(l);
 
 		if (m_seekState == seek_state::beginning
-			&& !m_byteStreamAccessor.empty())
+			&& m_byteStreamAccessor.empty())
 		{
+			m_seekState = seek_state::end;
+		}
+
+		// Ensure that seek state `position` is only used
+		// if there is a block to read at the given read offset.
+		if (m_seekState == seek_state::position
+			&& m_nextPosition.block_item_index() == 0
+			&& m_nextPosition.block_number() == m_streamBlocks)
+		{
+			m_seekState = seek_state::end;
+		}
+
+		if (m_seekState == seek_state::beginning) {
 			m_nextReadOffset = 0;
 			read_next_block(l, 0);
 			m_bufferState = buffer_state::read_only;
+			m_position = stream_position(0, 0, 0, 0);
 		} else if (m_seekState == seek_state::position) {
-			m_nextReadOffset = m_position.read_offset();
+			m_nextReadOffset = m_nextPosition.read_offset();
 			m_nextBlockSize = 0;
-			memory_size_type blockItemIndex = m_position.block_item_index();
-			read_next_block(l, m_position.block_number());
+			memory_size_type blockItemIndex = m_nextPosition.block_item_index();
+
+			read_next_block(l, m_nextPosition.block_number());
 
 			memory_size_type blockItems = m_lastItem - m_bufferBegin;
 
@@ -557,15 +655,24 @@ protected:
 			}
 
 			m_nextItem = m_bufferBegin + blockItemIndex;
-			m_position.advance_items(blockItemIndex);
+			m_position = m_nextPosition;
 			m_bufferState = buffer_state::read_only;
-		} else {
-			// seek_state::end
+		} else if (m_seekState == seek_state::end) {
 			get_buffer(l, m_streamBlocks);
 			m_bufferState = buffer_state::write_only;
 			m_nextItem = m_bufferBegin;
+			// We don't care about m_position
+		} else {
+			log_debug() << "Unknown seek state " << m_seekState << std::endl;
+			throw exception("perform_seek: Unknown seek state");
 		}
+
 		m_seekState = seek_state::none;
+
+		l.get_lock().unlock();
+		// For debugging: Verify get_position()
+		if (claimedPosition != get_position())
+			throw exception("get_position() was changed by perform_seek().");
 	}
 
 private:
@@ -639,8 +746,38 @@ private:
 	/** In read mode only: End of readable buffer. */
 	T * m_lastItem;
 
-	/** Invalid if seek state is end. */
+	/** Position relating to the currently loaded buffer.
+	 * Only valid during reading.
+	 * Invariants:
+	 *
+	 * block_number in [0, m_streamBlocks)
+	 * offset in [0, size]
+	 * block_item_index in [0, m_blockSize]
+	 *
+	 * If offset == 0, then read_offset == block_item_index == block_number == 0.
+	 * If block_number != 0, then read_offset, offset != 0.
+	 * block_item_index <= offset.
+	 *
+	 * If block_number == m_streamBlocks, we are in a block that has not yet
+	 * been written to disk.
+	 *
+	 * If block_item_index == m_blockSize, we have to flush the current block
+	 * on the next write, or read the next block on the next read.
+	 */
 	stream_position m_position;
+
+	/** If seekState is `position`, seek to this position before reading/writing.
+	 * Invariants:
+	 *
+	 * block_number in [0, m_streamBlocks] (can point past last written block)
+	 * offset in [0, size]
+	 * block_item_index in [0, m_blockSize) (cannot seek to the end of a block)
+	 *
+	 * If offset == 0, then read_offset == block_item_index == block_number == 0.
+	 * If block_number != 0, then read_offset, offset != 0.
+	 * block_item_index <= offset.
+	 * */
+	stream_position m_nextPosition;
 
 	/** If nextBlockSize is zero,
 	 * the size of the block to read is the first eight bytes,
