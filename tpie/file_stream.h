d275aaa056cba64c2e35575ccd7be4c6bfecd2a2
Mathias Rav <rav@cs.au.dk>
2013-08-27 15:00:53 +0200

    Change read_back to skip_back and peek.
    (cherry picked from commit 7f71fd04a51abba585ea41452e74598045d772e3)

--- tpie/file_stream.h
+++ tpie/file_stream.h
@@ -113,10 +113,8 @@ public:
 	/////////////////////////////////////////////////////////////////////////
 	inline const item_type & read_back() throw(stream_exception) {
 		assert(m_open);
-		seek(-1, current);
-		const item_type & i = read();
-		seek(-1, current);
-		return i;
+		skip_back();
+		return peek();
 	}
 
 	///////////////////////////////////////////////////////////////////////////
