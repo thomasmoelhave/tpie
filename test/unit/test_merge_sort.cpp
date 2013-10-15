a2a8651586be03868959795d9635f1df9204ce06
Mathias Rav <rav@cs.au.dk>
2013-10-15 13:30:52 +0200

    Import ut-merge_sort from master branch.

--- test/unit/test_merge_sort.cpp
+++ test/unit/test_merge_sort.cpp
@@ -26,52 +26,24 @@
 using namespace tpie;
 using namespace tpie::pipelining;
 
-typedef uint64_t test_t;
+#include "merge_sort.h"
 
-struct relative_memory_usage {
-	inline relative_memory_usage(memory_size_type extraMemory)
-		: m_startMemory(actual_used())
-		, m_threshold(0)
-		, m_extraMemory(extraMemory)
-	{
+class use_merge_sort {
+public:
+	typedef uint64_t test_t;
+	typedef merge_sorter<test_t, false> sorter;
+
+	static void merge_runs(sorter & s) {
+		dummy_progress_indicator pi;
+		s.calc(pi);
 	}
-
-	inline memory_size_type used() {
-		return actual_used() - m_startMemory;
-	}
-
-	inline void set_threshold(memory_size_type threshold) {
-		m_threshold = threshold;
-		get_memory_manager().set_limit(m_startMemory + m_threshold + m_extraMemory);
-	}
-
-	inline bool below(memory_size_type threshold) {
-		if (used() > threshold) {
-			report_overusage(threshold);
-			return false;
-		}
-		return true;
-	}
-
-	inline bool below() {
-		return below(m_threshold);
-	}
-
-	void report_overusage(memory_size_type threshold) {
-		log_error() << "Memory usage " << used() << " exceeds threshold " << threshold << std::endl;
-	}
-
-private:
-	inline static memory_size_type actual_used() {
-		return get_memory_manager().used();
-	}
-
-	memory_size_type m_startMemory;
-	memory_size_type m_threshold;
-	memory_size_type m_extraMemory;
 };
 
 bool sort_upper_bound_test() {
+	typedef use_merge_sort Traits;
+	typedef Traits::sorter sorter;
+	typedef Traits::test_t test_t;
+
 	memory_size_type m1 = 100 *1024*1024;
 	memory_size_type m2 = 20  *1024*1024;
 	memory_size_type m3 = 20  *1024*1024;
@@ -83,7 +55,7 @@ bool sort_upper_bound_test() {
 	stream_size_type io = get_bytes_written();
 
 	relative_memory_usage m(0);
-	merge_sorter<test_t, false> s;
+	sorter s;
 	s.set_available_memory(m1, m2, m3);
 	s.set_items(dataUpperBound / sizeof(test_t));
 
@@ -93,156 +65,61 @@ bool sort_upper_bound_test() {
 		s.push(i);
 	}
 	s.end();
-	dummy_progress_indicator pi;
-	s.calc(pi);
+	Traits::merge_runs(s);
 	while (s.can_pull()) s.pull();
 
 	return io == get_bytes_written();
 }
 
-bool sort_test(memory_size_type m1,
-			   memory_size_type m2,
-			   memory_size_type m3,
-			   double mb_data,
-			   memory_size_type extraMemory = 0,
-			   bool evacuateBeforeMerge = false,
-			   bool evacuateBeforeReport = false)
-{
-	m1 *= 1024*1024;
-	m2 *= 1024*1024;
-	m3 *= 1024*1024;
-	extraMemory *= 1024*1024;
-	stream_size_type items = static_cast<stream_size_type>(mb_data*1024/sizeof(test_t)*1024);
-	log_debug() << "sort_test with " << items << " items\n";
-	boost::rand48 rng;
-	relative_memory_usage m(extraMemory);
-	merge_sorter<test_t, false> s;
-	s.set_available_memory(m1, m2, m3);
-
-	log_debug() << "Begin phase 1" << std::endl;
-	m.set_threshold(m1);
-	s.begin();
-	if (!m.below()) return false;
-	for (stream_size_type i = 0; i < items; ++i) {
-		s.push(rng());
-		if (!m.below()) return false;
-	}
-	s.end();
-	if (!m.below()) return false;
-
-	if (evacuateBeforeMerge) {
-		s.evacuate();
-		log_debug() << "MEMORY USED: " << m.used() << '/' << s.evacuated_memory_usage() << std::endl;
-		if (!m.below(s.evacuated_memory_usage())) return false;
-	}
-
-	log_debug() << "Begin phase 2" << std::endl;
-	m.set_threshold(m2);
-	if (!m.below()) return false;
-	dummy_progress_indicator pi;
-	s.calc(pi);
-	if (!m.below()) return false;
-
-	if (evacuateBeforeReport) {
-		s.evacuate();
-		log_debug() << "MEMORY USED: " << m.used() << '/' << s.evacuated_memory_usage() << std::endl;
-		if (!m.below(s.evacuated_memory_usage())) return false;
-	}
-
-	log_debug() << "Begin phase 3" << std::endl;
-	m.set_threshold(m3);
-	if (!m.below()) return false;
-	test_t prev = std::numeric_limits<test_t>::min();
-	memory_size_type itemsRead = 0;
-	while (s.can_pull()) {
-		if (!m.below()) return false;
-		test_t read = s.pull();
-		if (!m.below()) return false;
-		if (read < prev) {
-			log_error() << "Out of order" << std::endl;
-			return false;
-		}
-		prev = read;
-		++itemsRead;
-	}
-	if (itemsRead != items) {
-		log_error() << "Read the wrong number of items. Got " << itemsRead << ", expected " << items << std::endl;
-		return false;
-	}
-
-	log_debug() << "MEMORY USED: " << m.used() << '/' << s.evacuated_memory_usage() << std::endl;
-	if (!m.below(s.evacuated_memory_usage())) return false;
-
-	return true;
-}
-
-bool internal_report_test() {
-	return sort_test(100,100,100,50);
-}
-
-bool internal_report_after_resize_test() {
-	return sort_test(100,80,80,50, 100);
-}
-
-bool one_run_external_report_test() {
-	return sort_test(100,11,11,7);
-}
-
-bool external_report_test() {
-	return sort_test(20,20,20,50);
-}
-
-bool small_final_fanout_test(double mb) {
-	return sort_test(7,20,11,mb);
-}
-
-bool evacuate_before_merge_test() {
-	return sort_test(20,20,20,10, 0, true, false);
-}
-
-bool evacuate_before_report_test() {
-	return sort_test(20,20,20,50, 0, false, true);
-}
-
 bool temp_file_usage_test() {
+	bool result = true;
 	const stream_size_type initialUsage = get_temp_file_usage();
 	log_debug() << "Offsetting get_temp_file_usage by " << initialUsage
 				<< std::endl;
-	const stream_size_type runLength = get_block_size() / sizeof(test_t);
+	const memory_size_type runLength = get_block_size() / sizeof(size_t);
 	const memory_size_type runs = 16;
+	const stream_size_type expectedUsage = runLength * runs * sizeof(size_t);
+	// Presumably, the data is not compressed beyond 1 byte per item.
+	const stream_size_type expectedUsageLowerBound = runLength * runs;
 	const memory_size_type fanout = runs;
 	{
-		merge_sorter<test_t, false> s;
+		merge_sorter<size_t, false> s;
 		s.set_parameters(runLength, fanout);
 		s.begin();
-		for (test_t i = 0; i < runs * runLength; ++i) {
+		for (size_t i = 0; i < runs * runLength; ++i) {
 			s.push(i);
 		}
 		s.end();
-		log_debug() << "After phase 1: " << (get_temp_file_usage()
-											 - initialUsage) << std::endl;
+		const stream_size_type afterPhase1 = get_temp_file_usage()
+											 - initialUsage;
+		log_debug() << "After phase 1: " << afterPhase1 << std::endl;
+		if (afterPhase1 < expectedUsageLowerBound) {
+			log_error() << "Used less space than expected!\nExpected is "
+						<< expectedUsage << ", but " << afterPhase1
+						<< " is way below that (< " << expectedUsageLowerBound
+						<< ")" << std::endl;
+			result = false;
+		}
 		dummy_progress_indicator pi;
 		s.calc(pi);
-		log_debug() << "After phase 2: " << (get_temp_file_usage()
-											 - initialUsage) << std::endl;
+		const stream_size_type afterPhase2 = get_temp_file_usage()
+											 - initialUsage;
+		log_debug() << "After phase 2: " << afterPhase2 << std::endl;
 		while (s.can_pull()) s.pull();
-		log_debug() << "After phase 3: " << (get_temp_file_usage()
-											 - initialUsage) << std::endl;
+		const stream_size_type afterPhase3 = get_temp_file_usage()
+											 - initialUsage;
+		log_debug() << "After phase 3: " << afterPhase3 << std::endl;
 	}
-	log_debug() << "After destroying merge_sorter: "
-				<< (get_temp_file_usage() - initialUsage) << std::endl;
-	return true;
+	const stream_size_type afterDestroy = get_temp_file_usage() - initialUsage;
+	log_debug() << "After destroying merge_sorter: " << afterDestroy
+				<< std::endl;
+	return result;
 }
 
 int main(int argc, char ** argv) {
-	return tests(argc, argv)
-		.test(internal_report_test, "internal_report")
-		.test(internal_report_after_resize_test, "internal_report_after_resize")
-		.test(one_run_external_report_test, "one_run_external_report")
-		.test(external_report_test, "external_report")
-		.test(small_final_fanout_test, "small_final_fanout", "mb", 4.5)
-		.test(evacuate_before_merge_test, "evacuate_before_merge")
-		.test(evacuate_before_report_test, "evacuate_before_report")
+	tests t(argc, argv);
+	return
+		sort_tester<use_merge_sort>::add_all(t)
 		.test(sort_upper_bound_test, "sort_upper_bound")
 		.test(temp_file_usage_test, "temp_file_usage")
 		;
