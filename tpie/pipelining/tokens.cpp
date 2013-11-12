6babc53c69857686d57829c8d9cf739abe2aad08
Mathias Rav <rav@cs.au.dk>
2013-11-12 13:43:24 +0100

    tokens.h: Add node_map::get_successors.

--- tpie/pipelining/tokens.cpp
+++ tpie/pipelining/tokens.cpp
@@ -17,6 +17,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with TPIE.  If not, see <http://www.gnu.org/licenses/>
 
+#include <queue>
 #include <tpie/pipelining/tokens.h>
 #include <tpie/pipelining/node.h>
 
@@ -103,6 +104,45 @@ size_t node_map::out_degree(const relmap_t & map, id_t from, node_relation rel) 
 		++i;
 	}
 	return res;
+}
+
+void node_map::get_successors(id_t from, std::set<id_t> & successors) {
+	std::queue<id_t> q;
+	std::set<id_t> seen;
+	q.push(from);
+	while (!q.empty()) {
+		id_t v = q.front();
+		q.pop();
+		if (seen.count(v)) continue;
+		seen.insert(v);
+		successors.insert(v);
+		{
+			std::pair<relmapit, relmapit> is = m_relations.equal_range(v);
+			for (relmapit i = is.first; i != is.second; ++i) {
+				switch (i->second.second) {
+					case pushes:
+						q.push(i->second.first);
+						break;
+					case pulls:
+					case depends:
+						break;
+				}
+			}
+		}
+		{
+			std::pair<relmapit, relmapit> is = m_relationsInv.equal_range(v);
+			for (relmapit i = is.first; i != is.second; ++i) {
+				switch (i->second.second) {
+					case pushes:
+						break;
+					case pulls:
+					case depends:
+						q.push(i->second.first);
+						break;
+				}
+			}
+		}
+	}
 }
 
 } // namespace bits
