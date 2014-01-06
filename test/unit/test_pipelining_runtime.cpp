57bce64df735b64d78d807072b17de727869880a
Mathias Rav <rav@cs.au.dk>
2014-01-06 13:52:06 +0100

    Add get_phase_graph test

--- test/unit/test_pipelining_runtime.cpp
+++ test/unit/test_pipelining_runtime.cpp
@@ -93,8 +93,77 @@ bool evacuate_test() {
 	return !bad;
 }
 
+bool get_phase_graph_test() {
+	const size_t N = 8;
+	const size_t P = 4;
+	evac_node nodes[N];
+	node_map::id_t ids[N];
+	for (size_t i = 0; i < N; ++i) ids[i] = nodes[i].get_id();
+
+	node_map::ptr nodeMap = nodes[0].get_node_map();
+	for (size_t i = 1; i < N; ++i) nodes[i].get_node_map()->union_set(nodeMap);
+	nodeMap = nodeMap->find_authority();
+
+#define PUSH(i, j) nodeMap->add_relation(ids[i], ids[j], pushes)
+#define DEP(i, j) nodeMap->add_relation(ids[i], ids[j], depends)
+	PUSH(0,1);
+	PUSH(0,2);
+	DEP(3,1);
+	DEP(4,2);
+	DEP(5,3);
+	DEP(6,4);
+	PUSH(5,7);
+	PUSH(6,7);
+
+	std::map<node *, size_t> phaseMap;
+	graph<size_t> phaseGraph;
+	{
+		runtime rt(nodeMap);
+		rt.get_phase_map(phaseMap);
+		rt.get_phase_graph(phaseMap, phaseGraph);
+	}
+	if (phaseMap.size() != N) {
+		log_error() << "phaseMap has wrong size" << std::endl;
+		return false;
+	}
+	size_t n[N];
+	for (size_t i = 0; i < N; ++i) n[i] = phaseMap[&nodes[i]];
+	if (phaseMap.size() != N) {
+		log_error() << "phaseMap has wrong entries" << std::endl;
+		return false;
+	}
+	if (n[0] != 0 || n[1] != 0 || n[2] != 0) {
+		log_error() << "phase 0 is wrong" << std::endl;
+		return false;
+	}
+	if (n[5] != 3 || n[6] != 3 || n[7] != 3) {
+		log_error() << "phase 3 is wrong" << std::endl;
+		return false;
+	}
+	if ((n[3] != 1 && n[3] != 2) || (n[4] != 1 && n[4] != 2)) {
+		log_error() << "node 3 or 4 is wrong" << std::endl;
+		return false;
+	}
+	const bool expectEdge[P][P] = {
+		{ false, true,  true,  false },
+		{ false, false, false, true  },
+		{ false, false, false, true  },
+		{ false, false, false, false }
+	};
+	for (size_t i = 0; i < P; ++i) {
+		for (size_t j = 0; j < P; ++j) {
+			if (phaseGraph.has_edge(i, j) != expectEdge[i][j]) {
+				log_error() << "Edge set is wrong at " << i << "," << j << std::endl;
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
 int main(int argc, char ** argv) {
 	return tpie::tests(argc, argv)
 	.test(evacuate_test, "evacuate")
+	.test(get_phase_graph_test, "get_phase_graph")
 	;
 }
