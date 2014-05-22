2361020a30c69de40e4dd78def21b324c312e5d5
Mathias Rav <rav@cs.au.dk>
2014-05-22 13:15:35 +0200

    Rewrite stream_position documentation

--- tpie/compressed/stream_position.h
+++ tpie/compressed/stream_position.h
@@ -31,15 +31,43 @@ namespace tpie {
 ///////////////////////////////////////////////////////////////////////////////
 /// \brief  POD object indicating the position of an item in a stream.
 ///
-/// A stream position is the tuple `(read_offset, offset)`.
+/// The object returned by stream_position::beginning is equal to any
+/// position object returned by a stream that points to the beginning of the
+/// stream.
 ///
-/// For compressed streams, the stream block begins with a header at byte
-/// position `read_offset`.  After the block header, the items follow.
+/// On the other hand, the object returned by stream_position::end is special:
+/// It is never returned by file_stream::get_position; instead, the object
+/// tells set_position to perform the equivalent of seek(0, end).
+/// The object resulting from the default constructor stream_position()
+/// is also special and should never be passed to set_position.
 ///
-/// For uncompressed streams, `read_offset` is zero and the item position
-/// in the stream is determined solely by `offset`.
+/// The offset() method of an ordinary stream_position object indicates
+/// the offset of the item in the stream that is pointed to.
+/// The values stream_position().offset() and stream_position::end().offset()
+/// are not well-defined and should not be relied on to have any particular
+/// value; instead, check for equality to respectively stream_position() and
+/// stream_position::end().
 ///
-/// Thus, the first item in the stream has the position tuple `(0, 0)`.
+/// The less-than operator defines a total order and is derived from the values
+/// returned by offset() with an implementation-defined tiebreaker in case
+/// offset() are equal.
+///
+/// It is guaranteed that any ordinary position is ordered before
+/// stream_position::end(), but the ordering of stream_position objects
+/// with regards to the special values stream_position() and
+/// stream_position::end() may otherwise be subject to change in the future.
+///////////////////////////////////////////////////////////////////////////////
+// IMPLEMENTATION NOTES
+//
+// A stream position is the tuple `(read_offset, offset)`.
+//
+// For compressed streams, the stream block begins with a header at byte
+// position `read_offset`.  After the block header, the items follow.
+//
+// For uncompressed streams, `read_offset` is zero and the item position
+// in the stream is determined solely by `offset`.
+//
+// Thus, the first item in the stream has the position tuple `(0, 0)`.
 ///////////////////////////////////////////////////////////////////////////////
 class stream_position {
 private:
@@ -51,16 +79,25 @@ private:
 	uint64_t m_offset;
 
 public:
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief  Convenience constructor returning a pointer to the beginning.
+	///////////////////////////////////////////////////////////////////////////
 	static stream_position beginning() {
 		return stream_position(0, 0);
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief  Special-value constructor returning a pointer to the end.
+	///////////////////////////////////////////////////////////////////////////
 	static stream_position end() {
 		return stream_position(
 			std::numeric_limits<uint64_t>::max() - 1,
 			std::numeric_limits<uint64_t>::max() - 1);
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief  Default constructor resulting in a not-a-stream_position.
+	///////////////////////////////////////////////////////////////////////////
 	stream_position()
 		: m_readOffset(std::numeric_limits<uint64_t>::max())
 		, m_offset(std::numeric_limits<uint64_t>::max())
@@ -80,6 +117,9 @@ private:
 	}
 
 public:
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief  The stream offset of the item pointed to.
+	///////////////////////////////////////////////////////////////////////////
 	stream_size_type offset() const {
 		return m_offset;
 	}
@@ -103,6 +143,11 @@ public:
 		return !(*this == other);
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+	/// \brief  Total ordering of stream_position objects.
+	///
+	/// See the class documentation for more information.
+	///////////////////////////////////////////////////////////////////////////
 	bool operator<(const stream_position & other) const {
 		return (m_offset != other.m_offset)
 			? (m_offset < other.m_offset)
