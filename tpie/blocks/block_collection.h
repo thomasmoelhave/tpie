064d273dad2dfcfde912642ee3d3ff171e0ce78a
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2015-01-30 15:55:54 +0100

    Move block collection method definitions into cpp file.

--- tpie/blocks/block_collection.h
+++ tpie/blocks/block_collection.h
@@ -45,72 +45,35 @@ public:
 	 * \param blockSize the size of the blocks
 	 * \param writeable indicates whether the collection is writeable
 	 */
-	block_collection(std::string fileName, memory_size_type blockSize, bool writeable)
-	: m_collection(fileName + ".queue", blockSize)
-	, m_writeable(writeable)
-	{
-		if(writeable) {
-			m_accessor.open_rw_new(fileName);
-			return;
-		}
+	block_collection(std::string fileName, memory_size_type blockSize, bool writeable);
 
-		m_accessor.open_ro(fileName);
-	}
-
-	~block_collection() {
-		m_accessor.close_i();
-	}
+	~block_collection();
 
 	/**
 	 * \brief Allocates a new block
 	 * \return the handle of the new block
 	 */
-	block_handle get_free_block() {
-		tp_assert(m_writeable, "get_free_block(): the block collection is read only");
-
-		return m_collection.alloc();
-	}
+	block_handle get_free_block();
 
 	/**
 	 * \brief frees a block
 	 * \param handle the handle of the block to be freed
 	 */
-	void free_block(block_handle handle) {
-		tp_assert(m_writeable, "free_block(): the block collection is read only");
-
-		m_collection.free(handle);
-
-		if(m_accessor.file_size_i() > m_collection.size()) {
-			m_accessor.truncate_i(m_collection.size());
-		}
-	}
+	void free_block(block_handle handle);
 
 	/**
 	 * \brief Reads the content of a block from disk
 	 * \param handle the handle of the block to read
 	 * \param b the block to store the content in
 	 */
-	void read_block(block_handle handle, block & b) {
-		tp_assert(handle.position + handle.size <= m_collection.size(), "the content of the given handle has not been written to disk");
-
-		b.resize(handle.size);
-
-		m_accessor.seek_i(handle.position);
-		m_accessor.read_i(static_cast<void*>(b.get()), handle.size);
-	}
+	void read_block(block_handle handle, block & b);
 
 	/**
 	 * \brief Writes the content of a block to disk
 	 * \param handle the handle of the block to write
 	 * \param b the block type in which the content is stored
 	 */
-	void write_block(block_handle handle, const block & b) {
-		tp_assert(m_writeable, "write_block(): the block collection is read only.");
-		tp_assert(handle.size >= b.size(), "the given block is not large enough.");
-
-		m_accessor.seek_i(handle.position);
-		m_accessor.write_i(static_cast<const void*>(b.get()), b.size());
-	}
+	void write_block(block_handle handle, const block & b);
 private:
 	bits::freespace_collection m_collection;
 	tpie::file_accessor::raw_file_accessor m_accessor;
