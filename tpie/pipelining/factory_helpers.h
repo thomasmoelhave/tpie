e950aef044372ea65a7fb0daa4618b72af81953b
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2015-07-13 15:31:33 +0200

    Fix split factory helper for c++11x.

--- tpie/pipelining/factory_helpers.h
+++ tpie/pipelining/factory_helpers.h
@@ -219,10 +219,10 @@ public:
 	};
 
 	template <typename dest_t>
-	typename constructed<dest_t>::type construct(const dest_t & dest) const {
+	typename constructed<dest_t>::type construct(TPIE_RREF(dest_t) dest) const {
 		node_token input_token;
 		typedef typename push_type<dest_t>::type item_type;
-		boost::shared_ptr<OB> o(new O<dest_t>(dest, input_token));
+		boost::shared_ptr<OB> o(new O<dest_t>(TPIE_MOVE(dest), input_token));
 		return I<item_type>(input_token, o);
 	};
 };
