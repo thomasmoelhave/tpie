2f5a4a221df3b1e648205e5ec916c3f9a17550d0
Jakob Truelsen <jakob@scalgo.com>
2019-01-21 11:44:58 +0100

    Apparently we need to link publicly against the compression libraries unless we want linker errors for shardlib builds

--- cmake_modules/LibFindSimpleLibrary.cmake
+++ cmake_modules/LibFindSimpleLibrary.cmake
@@ -1,8 +1,8 @@
 macro(find_simple_library FLI_NAME)
-  cmake_parse_arguments(FLI "" "" "HEADER;LIBRARY" ${ARGN})
+  cmake_parse_arguments(FLI "" "" "HEADER;LIBRARY;PATH_SUFFIXES" ${ARGN})
   string(TOUPPER "${FLI_NAME}" FLI_UNAME)
 
-  find_path(${FLI_UNAME}_INCLUDE_DIR NAMES ${FLI_HEADER} )
+  find_path(${FLI_UNAME}_INCLUDE_DIR NAMES ${FLI_HEADER} PATH_SUFFIXES ${FLI_PATH_SUFFIXES})
   find_library(${FLI_UNAME}_LIBRARY NAMES ${FLI_LIBRARY} )
   
   find_package_handle_standard_args(${FLI_UNAME} REQUIRED_VARS ${FLI_UNAME}_LIBRARY ${FLI_UNAME}_INCLUDE_DIR)
@@ -14,7 +14,7 @@ macro(find_simple_library FLI_NAME)
     set_target_properties(${FLI_INAME} PROPERTIES
       INTERFACE_INCLUDE_DIRECTORIES "${${FLI_UNAME}_INCLUDE_DIR}"
       IMPORTED_LOCATION "${${FLI_UNAME}_LIBRARY}")
-   endif()
+  endif()
 endmacro()
   
 
